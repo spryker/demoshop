84481461d5fb84be27fa9cb73e39b0dfee2cba61
René Klatt <rene.klatt@gmx.net>
2013-08-22 14:58:16 +0200

    Some small fixes

--- project/Sao/Shared/Cart/Transfer/Change.php
+++ project/Sao/Shared/Cart/Transfer/Change.php
@@ -7,16 +7,4 @@
  */
 class Sao_Shared_Cart_Transfer_Change extends ProjectA_Shared_Cart_Transfer_Change
 {
-    /**
-     * @param Sao_Shared_Cart_Transfer_Item $cartItem
-     *
-     * @return $this
-     */
-    public function addCartItem(Sao_Shared_Cart_Transfer_Item $cartItem)
-    {
-        // $this->cartItems[] = $cartItem;
-        $this->cartItems->add($cartItem);
-        return $this;
-    }
-
 }
