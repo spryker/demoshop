b9207293bbdd10e8cb36e20ba49cc08eb41b358e
René Klatt <rene.klatt@gmx.net>
2013-08-30 11:53:37 +0200

    Add package to composer.json

--- project/Sao/Zed/Setup/Module/Controller/Index.php
+++ project/Sao/Zed/Setup/Module/Controller/Index.php
@@ -5,6 +5,8 @@ class Sao_Zed_Setup_Module_Controller_Index extends ProjectA_Zed_Setup_Module_Co
 
     public function projectAction()
     {
+        $entity = Generated_Zed_EntityLoader::getPacAclPrivilege();
+        Zend_Debug::dump($entity, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');die();
     }
 
 }
