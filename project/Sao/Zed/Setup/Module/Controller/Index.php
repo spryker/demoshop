bf3916418536fef9c5a031bd195edc3b47c55f67
René Klatt <rene.klatt@gmx.net>
2013-08-30 16:52:37 +0200

    Add test class

--- project/Sao/Zed/Setup/Module/Controller/Index.php
+++ project/Sao/Zed/Setup/Module/Controller/Index.php
@@ -3,9 +3,14 @@
 class Sao_Zed_Setup_Module_Controller_Index extends ProjectA_Zed_Setup_Module_Controller_Index
 {
 
+    const OM_CLASS = 'PacAclPrivilege';
+
     public function projectAction()
     {
         $entity = Generated_Zed_EntityLoader::getPacAclPrivilege();
+        Zend_Debug::dump($entity, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');
+
+        $entity = Generated_Zed_EntityLoader::getSaoAclPrivilege();
         Zend_Debug::dump($entity, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');die();
     }
 
