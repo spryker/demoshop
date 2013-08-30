30934d8ee674419d404f8afd8352b884ab0b9f0e
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-08-30 19:16:56 +0200

    fix initilaize

--- project/Sao/Zed/Setup/Module/Controller/InitializeDatabase.php
+++ project/Sao/Zed/Setup/Module/Controller/InitializeDatabase.php
@@ -1,19 +1,5 @@
 <?php
 
-/**
- * @property Sao_Zed_Installer_Component_Facade $facadeInstaller
- */
-class Sao_Zed_Setup_Module_Controller_InitializeDatabase extends ProjectA_Zed_Library_Controller_Action
-    implements ProjectA_Zed_Installer_Component_Dependency_Facade_Interface
+class Sao_Zed_Setup_Module_Controller_InitializeDatabase extends ProjectA_Zed_Setup_Module_Controller_InitializeDatabase
 {
-
-    use ProjectA_Zed_Installer_Component_Dependency_Facade_Trait;
-
-    public function indexAction()
-    {
-        set_time_limit(0);
-        $queries = $this->facadeInstaller->install();
-        ProjectA_Zed_Library_Setup::renderAndExit(implode('<br />', $queries));
-        echo "2";
-    }
 }
