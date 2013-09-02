141d9d29c10c243eb5d94b0bd50ed48c92fe2e5c
René Klatt <rene.klatt@gmx.net>
2013-09-02 11:50:27 +0200

    update composer

--- project/Sao/Zed/Setup/Module/Controller/Index.php
+++ project/Sao/Zed/Setup/Module/Controller/Index.php
@@ -3,15 +3,13 @@
 class Sao_Zed_Setup_Module_Controller_Index extends ProjectA_Zed_Setup_Module_Controller_Index
 {
 
-    const OM_CLASS = 'PacAclPrivilege';
-
     public function projectAction()
     {
         $customer = (new ProjectA_Zed_Customer_Persistence_PacCustomerQuery())->findOneByIdCustomer(1);
         Zend_Debug::dump($customer, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');
 
         $address = $customer->getAddresses()->getFirst();
-        Zend_Debug::dump($address, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');die();
+        Zend_Debug::dump($address, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');
 
         $addresses = (new ProjectA_Zed_Customer_Persistence_PacCustomerAddressQuery())->findByFkCustomer(1);
         Zend_Debug::dump($addresses, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');
