8b359d2fbe7869897da2ccee1b4d4b3ec99ebe7a
Marco Roßdeutscher <marco.rossdeutscher@project-a.com>
2013-09-03 15:36:35 +0200

    more refactored $factory access without trait

--- project/Sao/Zed/Sales/Component/Model/Orderprocess/StateMachineFactoryHook.php
+++ project/Sao/Zed/Sales/Component/Model/Orderprocess/StateMachineFactoryHook.php
@@ -1,31 +1,26 @@
 <?php
-
+/**
+ * @property Generated_Zed_Sales_Component_Factory $factory
+ */
 class Sao_Zed_Sales_Component_Model_Orderprocess_StateMachineFactoryHook implements
     ProjectA_Zed_Sales_Component_Interface_StatemachineFactoryHook,
     ProjectA_Zed_Stripe_Component_Dependency_Facade_Interface,
     ProjectA_Zed_Adyen_Component_Dependency_Facade_Interface,
     ProjectA_Zed_Library_Dependency_Factory_Interface
 {
-
     use ProjectA_Zed_Stripe_Component_Dependency_Facade_Trait;
     use ProjectA_Zed_Adyen_Component_Dependency_Facade_Trait;
+    use ProjectA_Zed_Library_Dependency_Factory_Trait;
+
     /**
      * @var Generated_Zed_Sales_Component_Factory
      */
     private $factory;
 
     /**
-     * @param ProjectA_Zed_Library_Component_Interface_FactoryInterface $factory
-     */
-    public function setFactory (ProjectA_Zed_Library_Component_Interface_FactoryInterface $factory)
-    {
-        $this->factory = $factory;
-    }
-
-    /**
      * @return ProjectA_Zed_Library_StateMachine_Transition_Guard_Rule_Collection
      */
-    protected function getRules ()
+    protected function getRules()
     {
         $rules = new ProjectA_Zed_Library_StateMachine_Transition_Guard_Rule_Collection();
         $rules->addRule($this->factory->getModelOrderprocessGuardRuleIsNationalShipment());
@@ -57,7 +52,7 @@ class Sao_Zed_Sales_Component_Model_Orderprocess_StateMachineFactoryHook impleme
      * @param ProjectA_Zed_Library_StateMachine $stateMachine
      * @return ProjectA_Zed_Library_StateMachine
      */
-    public function onStateMachineWasCreated (ProjectA_Zed_Library_StateMachine $stateMachine)
+    public function onStateMachineWasCreated(ProjectA_Zed_Library_StateMachine $stateMachine)
     {
         $guards = new ProjectA_Zed_Library_StateMachine_Transition_Guard_Composite();
         $guards->addGuard($this->getRules());
@@ -66,5 +61,4 @@ class Sao_Zed_Sales_Component_Model_Orderprocess_StateMachineFactoryHook impleme
         $stateMachine->attach($this->factory->getModelOrderprocessStateMachineWatchdogObserver());
         return $stateMachine;
     }
-
 }
