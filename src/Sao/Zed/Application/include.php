7828ed8b1f06a2268c01aaf3b9b1eaa71bd39aaf
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-09-05 13:36:18 +0200

    fix src

--- src/Sao/Zed/Application/include.php
+++ src/Sao/Zed/Application/include.php
@@ -3,7 +3,7 @@
 define('APPLICATION', 'ZED');
 defined('APPLICATION_ROOT') or define('APPLICATION_ROOT', realpath(__DIR__ . '/../../../..'));
 
-require_once(APPLICATION_ROOT . '/vendor/project-a/library-package/ProjectA/Shared/Library/Application/Environment.php');
+require_once(APPLICATION_ROOT . '/vendor/project-a/library-package/src/ProjectA/Shared/Library/Application/Environment.php');
 ProjectA\Shared\Library\Application\Environment::initialize();
 ProjectA\Shared\Library\Application\TestEnvironment::initialize();
 
