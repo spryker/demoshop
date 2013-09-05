22027211df65d30d85abe8e858c5f9d480c5d9b9
Jörg Stick <jstick@PAV-NB-C02JN19BF1G4.local>
2013-09-05 10:07:58 +0200

    keys kendo grid

---
+++ project/Sao/Zed/Glossary/Module/Controller/KeysGrid.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class Sao_Zed_Glossary_Module_Controller_KeysGrid
+ */
+class Sao_Zed_Glossary_Module_Controller_KeysGrid extends ProjectA_Zed_Glossary_Module_Controller_KeysGrid
+{
+
+}
