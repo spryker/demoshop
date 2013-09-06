aecc393f7bc3953f1740aa1f75bca702eef1dde8
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-09-06 18:18:05 +0200

    fix deploy

--- src/Sao/Zed/Application/include.php
+++ src/Sao/Zed/Application/include.php
@@ -7,6 +7,8 @@ require_once(APPLICATION_ROOT_DIR . '/vendor/project-a/library-package/src/Proje
 ProjectA\Shared\Library\Application\Environment::initialize();
 ProjectA\Shared\Library\Application\TestEnvironment::initialize();
 
-require_once(APPLICATION_SOURCE_DIR . '/Generated/Zed/DependencyInjectionContainer.php');
+if (file_exists(APPLICATION_SOURCE_DIR . '/Generated/Zed/DependencyInjectionContainer.php')) {
+    require_once(APPLICATION_SOURCE_DIR . '/Generated/Zed/DependencyInjectionContainer.php');
+}
 
 ProjectA_Shared_Library_Context::setDefaultContext(ProjectA_Shared_Library_Context::CONTEXT_ZED);
