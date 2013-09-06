ddedbdb385087b1c3659e81b89098a0b4790d1ea
René Klatt <rene.klatt@gmx.net>
2013-09-06 15:57:51 +0200

    Changed session anabling

--- src/Sao/Zed/Application/Module/Bootstrap.php
+++ src/Sao/Zed/Application/Module/Bootstrap.php
@@ -19,7 +19,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
     protected function _initSession()
     {
-        if (PHP_SAPI === 'cli' && ProjectA_Shared_Library_Environment::isNotTesting()) {
+        if (PHP_SAPI === 'cli' || ProjectA_Shared_Library_Environment::isTesting()) {
             return;
         }
         $config = ProjectA_Shared_Library_Config::get('zed');
