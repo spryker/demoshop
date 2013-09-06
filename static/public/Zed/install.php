0271f053956b83a6447916934c6807421d6ee632
René Klatt <rene.klatt@gmx.net>
2013-09-06 14:55:32 +0200

    Changes in oackage.json

--- static/public/Zed/install.php
+++ static/public/Zed/install.php
@@ -2,10 +2,6 @@
 
 require_once __DIR__ . '/../../../src/Sao/Zed/Application/include.php';
 
-$autoloader = Zend_Loader_Autoloader::getInstance();
-$autoloader->registerNamespace('ProjectA_Zed_Library_');
-$autoloader->registerNamespace('ProjectA_Shared_');
-
 $config = new Zend_Config_Ini(APPLICATION_ROOT_DIR . '/config/Zed/application.ini', APPLICATION_ENV);
 $propelConfig = $config->get('propel');
 
@@ -13,7 +9,7 @@ $list = new ProjectA_Zed_Library_File_Filesystem_List($propelConfig->get('propel
 $buildFile = new ProjectA_Zed_Library_Memory_File('build.properties', '');
 $list->add($buildFile);
 
-// include plain-php schemata-config files
+$schemata = '';
 require APPLICATION_ROOT_DIR . '/config/Zed/schemata.php';
 
 $installer = new ProjectA_Zed_Library_Propel_Install();
