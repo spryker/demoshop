71972420484adcae162e93b6a1b29520a8d06f8a
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-09-10 14:29:44 +0200

    new bootstrap and currencymanager

--- static/public/Yves/index.php
+++ static/public/Yves/index.php
@@ -1,11 +1,9 @@
 <?php
-/**
- * YVES Front Controller
- */
+/* YVES Front Controller */
 
 use ProjectA\Shared\Library\Application\Environment;
 use ProjectA\Shared\Library\Error\ErrorLogger;
-use Sao\Yves\Application\Bootstrap;
+use Sao\Yves\Application\Module\Bootstrap;
 
 define('YVES_START', microtime(true));
 
@@ -15,6 +13,9 @@ defined('APPLICATION_ROOT_DIR') or define('APPLICATION_ROOT_DIR', realpath(__DIR
 require_once(APPLICATION_ROOT_DIR . '/vendor/project-a/library-package/src/ProjectA/Shared/Library/Application/Environment.php');
 
 Environment::initialize();
-
-(new Bootstrap())->bootAndRun();
 ProjectA_Shared_Library_Context::setDefaultContext(ProjectA_Shared_Library_Context::CONTEXT_YVES);
+
+$bootstrap = new Bootstrap();
+$bootstrap
+    ->boot()
+    ->run(Bootstrap::getRequest());
