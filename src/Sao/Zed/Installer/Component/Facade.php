eeefb1abf975b4debf77ecb55f98397c921edf03
René Klatt <rene.klatt@gmx.net>
2013-09-10 11:13:06 +0200

    Cs fixes

--- src/Sao/Zed/Installer/Component/Facade.php
+++ src/Sao/Zed/Installer/Component/Facade.php
@@ -3,11 +3,8 @@
 class Sao_Zed_Installer_Component_Facade extends ProjectA_Zed_Installer_Component_Facade
 {
 
-
-
     public function install()
     {
         return $this->factory->getModelInstaller()->install();
     }
-
 }
