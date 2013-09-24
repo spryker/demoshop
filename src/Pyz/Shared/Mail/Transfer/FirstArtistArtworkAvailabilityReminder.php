db99510ff878e6607e26f821338b0d84aa1dba02
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-09-24 13:57:26 +0200

    remove saatchi mails

--- src/Pyz/Shared/Mail/Transfer/FirstArtistArtworkAvailabilityReminder.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Pyz\Shared\Mail\Transfer;
-
-use ProjectA\Shared\Mail\Transfer\TransferInterface\UniqueInterface;
-
-class FirstArtistArtworkAvailabilityReminder extends Invoice implements UniqueInterface
-{
-}
