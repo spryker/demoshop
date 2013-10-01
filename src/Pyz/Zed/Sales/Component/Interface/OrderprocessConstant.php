5867b99881179de12ad2d5d12aa5475bd40d368b
Michael Kugele <michael.kugele@project-a.com>
2013-10-01 14:49:31 +0200

    Enabled demo state machine

---
+++ src/Pyz/Zed/Sales/Component/Interface/OrderprocessConstant.php
@@ -0,0 +1,8 @@
+<?php
+
+interface Pyz_Zed_Sales_Component_Interface_OrderprocessConstant extends ProjectA_Zed_Sales_Component_Interface_OrderprocessConstant
+{
+
+    // Order processes
+    const ORDER_PROCESS_DEMO = 'demo';
+}
