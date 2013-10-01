8576d0ab6def79c8ac34af2903eff2922d585cf4
Michael Kugele <michael.kugele@project-a.com>
2013-10-01 17:06:48 +0200

    Enabled sales order process installer

--- src/Pyz/Zed/Installer/Component/Model/Installer.php
+++ src/Pyz/Zed/Installer/Component/Model/Installer.php
@@ -8,7 +8,8 @@ class Pyz_Zed_Installer_Component_Model_Installer extends ProjectA_Zed_Installer
     ProjectA_Zed_Misc_Component_Dependency_Facade_Interface,
     ProjectA_Zed_Price_Component_Dependency_Facade_Interface,
     ProjectA_Zed_Stock_Component_Dependency_Facade_Interface,
-    ProjectA_Zed_Glossary_Component_Dependency_Facade_Interface
+    ProjectA_Zed_Glossary_Component_Dependency_Facade_Interface,
+    ProjectA_Zed_Sales_Component_Dependency_Facade_Interface
 {
 
     use ProjectA_Zed_Acl_Component_Dependency_Facade_Trait;
@@ -19,13 +20,14 @@ class Pyz_Zed_Installer_Component_Model_Installer extends ProjectA_Zed_Installer
     use ProjectA_Zed_Price_Component_Dependency_Facade_Trait;
     use ProjectA_Zed_Stock_Component_Dependency_Facade_Trait;
     use ProjectA_Zed_Glossary_Component_Dependency_Facade_Trait;
+    use ProjectA_Zed_Sales_Component_Dependency_Facade_Trait;
 
     /**
      * @return array
      */
     protected function getInstaller()
     {
-        return array(
+        return [
             $this->facadeAcl->getInternalInstall(),
             $this->facadeCatalog->getInternalInstall(),
             $this->facadeCategory->getInternalInstall(),
@@ -33,7 +35,8 @@ class Pyz_Zed_Installer_Component_Model_Installer extends ProjectA_Zed_Installer
             $this->facadeMisc->getInternalInstall(),
             $this->facadePrice->getInternalInstall(),
             $this->facadeStock->getInternalInstall(),
-            $this->facadeGlossary->getInternalInstall()
-        );
+            $this->facadeGlossary->getInternalInstall(),
+            $this->facadeSales->getInternalInstall(),
+        ];
     }
 }
