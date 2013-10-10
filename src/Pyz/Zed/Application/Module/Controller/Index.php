1c34b089ab689f1652096931a8ed29509d63d22b
René Klatt <rene.klatt@gmx.net>
2013-10-10 12:22:09 +0200

    Removed debug stuff

--- src/Pyz/Zed/Application/Module/Controller/Index.php
+++ src/Pyz/Zed/Application/Module/Controller/Index.php
@@ -8,17 +8,6 @@ class Pyz_Zed_Application_Module_Controller_Index extends ProjectA_Zed_Library_C
     public function indexAction()
     {
         $this->_helper->layout->setLayout('modular');
-
-        $path = APPLICATION_VENDOR_DIR . '/project-a/acl-package/src/ProjectA/Shared/Acl/Transfer/User.php';
-        $user = new \ProjectA\Shared\Acl\Transfer\User();
-        $reflection = new \Zend\Code\Reflection\FileReflection($path);
-        $uses = $reflection->getUses();
-        \Zend_Debug::dump($uses, __CLASS__ . ' LINE: ' . __LINE__ . '<br/><br/>');die();
-        foreach ($reflection->getProperties() as $property) {
-            $property->setAccessible(true);
-            echo($property->getValue($user));die();
-        }
-
     }
 }
 
