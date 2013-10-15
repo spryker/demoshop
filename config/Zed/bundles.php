d4ee064288cb281c2f37feeb4fd26043eb87aa9b
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-10-15 15:26:12 +0200

    config

--- config/Zed/bundles.php
+++ config/Zed/bundles.php
@@ -7,10 +7,7 @@ const ACTIVATE_BOOTSTRAP = 'activate bootstrap';
 const ACTIVATE_NAVIGATION = 'activate navigation';
 const ACTIVATE_SCHEMA = 'activate schema';
 
-/**
- * @var $schemata array
- */
-$bundleConfig = [
+return [
     ProjectA_Shared_Library_Config::get(CONFIG_KEY_NAMESPACE_PROJECT) => [],
     NAMESPACE_CORE => [
         'Auth' => [
