2176f6bba0ea352c8fe3293249fe9a896079c5d1
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-10-18 14:56:10 +0200

    add Salesrule calculator

--- src/Pyz/Zed/Calculation/Component/Settings.php
+++ src/Pyz/Zed/Calculation/Component/Settings.php
@@ -2,8 +2,12 @@
 /**
  * @property Generated_Zed_Calculation_Component_Factory $factory
  */
-class Pyz_Zed_Calculation_Component_Settings extends ProjectA_Zed_Calculation_Component_Settings implements ProjectA_Zed_Library_Dependency_Factory_Interface
+class Pyz_Zed_Calculation_Component_Settings extends ProjectA_Zed_Calculation_Component_Settings implements
+    ProjectA_Zed_Library_Dependency_Factory_Interface,
+    ProjectA_Zed_Salesrule_Component_Dependency_Facade_Interface
 {
+
+    use ProjectA_Zed_Salesrule_Component_Dependency_Facade_Trait;
 
     /**
      * @return array
@@ -18,7 +22,7 @@ class Pyz_Zed_Calculation_Component_Settings extends ProjectA_Zed_Calculation_Co
             $this->factory->createModelCalculatorsSubtotal(),
             $this->factory->createModelCalculatorsSubtotalWithoutItemExpenses(),
             $this->factory->createModelCalculatorsGrandTotalWithoutDiscounts(),
-            $this->factory->createModelCalculatorsSalesrule(),
+            $this->facadeSalesrule->createSalesruleCalculator(),
             $this->factory->createModelCalculatorsExpensePriceToPay(),
             $this->factory->createModelCalculatorsItemPriceToPay(),
             $this->factory->createModelCalculatorsOptionPriceToPay(),
