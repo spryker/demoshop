f63b7b009edc2964dab3cee916eef0040a34eb24
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-10-22 16:21:55 +0200

    Yves Controller need controller in name, abstract controllers renamed

--- src/Pyz/Zed/Application/Module/Controller/IndexController.php
+++ src/Pyz/Zed/Application/Module/Controller/IndexController.php
@@ -4,7 +4,7 @@ namespace Pyz\Zed\Application\Module\Controller;
 
 use Symfony\Component\Finder\Finder;
 
-class Index extends \ProjectA_Zed_Library_Controller_Action
+class IndexController extends \ProjectA_Zed_Library_Controller_Action
 {
     public function indexAction()
     {
