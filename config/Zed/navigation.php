c1a51c17b7f434739e80d9bdae2a802d249690ca
Michael Kugele <michael.kugele@project-a.com>
2013-10-24 11:29:46 +0200

    Removed DWH from navigation #261

--- config/Zed/navigation.php
+++ config/Zed/navigation.php
@@ -9,7 +9,7 @@ return array(
 //    APPLICATION_VENDOR_DIR . '/project-a/content-package/src/ProjectA/Zed/Cms/Module/navigation.xml',
     APPLICATION_VENDOR_DIR . '/project-a/crm-package/src/ProjectA/Zed/Customer/Module/navigation.xml',
     APPLICATION_VENDOR_DIR . '/project-a/crm-package/src/ProjectA/Zed/Mail/Module/navigation.xml',
-    APPLICATION_VENDOR_DIR . '/project-a/dwh-package/src/ProjectA/Zed/Dwh/Module/navigation.xml',
+//    APPLICATION_VENDOR_DIR . '/project-a/dwh-package/src/ProjectA/Zed/Dwh/Module/navigation.xml',
     APPLICATION_VENDOR_DIR . '/project-a/frontend-package/src/ProjectA/Zed/Yves/Module/navigation.xml',
     APPLICATION_VENDOR_DIR . '/project-a/glossary-package/src/ProjectA/Zed/Glossary/Module/navigation.xml',
     APPLICATION_VENDOR_DIR . '/project-a/infrastructure-package/src/ProjectA/Zed/Behat/Module/navigation.xml',
