eda5da8737803d56be8795272e0dd91b6ffee021
Jonathan Reisdorf <jonathan.reisdorf@project-a.com>
2013-11-05 12:06:59 +0000

    #227 done

--- static/assets/Zed/scripts/zed.js
+++ static/assets/Zed/scripts/zed.js
@@ -28,7 +28,6 @@
     $.fn.kendoGrid = (function (originalFn) {
         return function () {
             arguments[0] = gridUi.configs.patch($(this).attr('id'), arguments[0]);
-            console.log('initializing grid with config', arguments[0]);
             originalFn.apply(this, arguments);
         };
     })($.fn.kendoGrid);
@@ -39,26 +38,55 @@
                 if (!this[resource]) {
                     this[resource] = {};
                 }
+                if (gridUi.containsModifications(this[resource])) {
+                    gridUi.renderUndoBtn(resource);
+                }
                 if (!this[resource].fields) {
                     this[resource].fields = {};
                 }
                 return this[resource];
             },
-            set : function(resource) {
-                $.get('/kendo/state/save', { grid : resource, state : JSON.stringify(this[resource])}, function(result) {
-                    console.log('config of grid ' + resource + ' changed. server says: success: ', result ? result.success : false);
+            set : function(resource, refreshGrid) {
+                $.get('/kendo/state/save', { grid : resource, state : JSON.stringify(this[resource])}, function() {
+                    gridUi.renderUndoBtn(resource);
+                    if (refreshGrid) {
+                        // @todo: only reload the grid, not the page
+                        document.location.href = document.location.href.split('#')[0];
+                    }
                 });
             },
             load : function(resource, config) {
-                console.log('loading', resource, config);
+                config = gridUi.getConfigFromUrl(resource, config);
                 this[resource] = config;
             },
             patch : function(resource, kendoConfig) {
                 var uiConfig = this.get(resource);
+                var fieldOrder = [];
                 kendoConfig.columns = kendoConfig.columns.map(function(item) {
+                    fieldOrder.push(item.field);
                     if (!uiConfig.fields[item.field]) { return item; }
                     return $.extend(item, uiConfig.fields[item.field]);
                 });
+
+                if (uiConfig.reorders) {
+                    $.each(uiConfig.reorders, function(fieldName, newIndex) {
+                        var oldIndex = $.inArray(fieldName, fieldOrder);
+                        if (oldIndex === -1) { return; }
+                        kendoConfig.columns.splice(newIndex, 0, kendoConfig.columns.splice(oldIndex, 1)[0]);
+                        fieldOrder.splice(newIndex, 0, fieldOrder.splice(oldIndex, 1)[0]);
+                    });
+                }
+
+                if (uiConfig.group) {
+                    kendoConfig.dataSource.group = uiConfig.group;
+                }
+                if (uiConfig.filter) {
+                    kendoConfig.dataSource.filter = uiConfig.filter;
+                }
+                if (uiConfig.sort) {
+                    kendoConfig.dataSource.sort = uiConfig.sort;
+                }
+
                 return kendoConfig;
             }
         },
@@ -78,8 +106,11 @@
             gridUi.configs.set(this);
         },
         columnReorder : function(e) {
-            var field = gridUi.fields.get(this, e.column.field);
-            field.index = e.newIndex;
+            var config = gridUi.configs.get(this);
+            if (!config.reorders) {
+                config.reorders = {};
+            }
+            config.reorders[e.column.field] = e.newIndex;
             gridUi.configs.set(this);
         },
         columnResize : function(e) {
@@ -88,14 +119,17 @@
             gridUi.configs.set(this);
         },
         change : function(e) {
+            var config = gridUi.configs.get(this);
             if ($.inArray(this, gridUi.initials) !== -1) {
                 // prevent initial change call, it's automatically executed
                 gridUi.initials = gridUi.initials.filter(function(item) {
                     return item !== this;
                 }.bind(this));
+                if (gridUi.containsModifications(config)) {
+                    gridUi.renderUndoBtn(this);
+                }
                 return;
             }
-            var config = gridUi.configs.get(this);
             config.group = e.sender._group;
             config.sort = e.sender._sort;
             config.filter = e.sender._filter;
@@ -110,6 +144,40 @@
                 .bind('columnReorder', this.columnReorder.bind(resource))
                 .bind('columnResize', this.columnResize.bind(resource))
                 .dataSource.bind('change', this.change.bind(resource));
+        },
+        renderUndoBtn : function(resource) {
+            var $target = $('#' + resource + '[data-role=grid]');
+            if (!$target.length || $target.find('.undoGridModifications').length) { return; }
+            var $btn = $('<div class="undoGridModifications"></div>').attr('title', 'Reset grid modifications').html('<i class="icon-undo"></i>');
+            $btn.click(function() {
+                gridUi.configs[resource] = {};
+                gridUi.configs.set(resource, true);
+            });
+            $target.prepend($btn);
+            this.renderUrlBtn(resource);
+        },
+        renderUrlBtn : function(resource) {
+            var $target = $('#' + resource + '[data-role=grid]');
+            if (!$target.length || $target.find('.gridModificationsUrl').length) { return; }
+            var $btn = $('<div class="gridModificationsUrl"></div>').attr('title', 'Get persistent URL for this grid configuration').html('<i class="icon-link"></i>');
+            $btn.click(function() {
+                alert(document.location.href.split('#')[0] + '#gridconfig->' + resource + '->' + JSON.stringify(gridUi.configs.get(resource)));
+            });
+            $target.prepend($btn);
+        },
+        containsModifications : function(config) {
+            return (config.fields && Object.keys(config.fields).length)
+                || (config.reorders && Object.keys(config.reorders).length)
+                || (config.group && Object.keys(config.group).length)
+                || (config.sort && Object.keys(config.sort).length)
+                || (config.filter && Object.keys(config.filter).length);
+        },
+        getConfigFromUrl : function(resource, config) {
+            var hash = document.location.hash.split('->');
+            if (hash.length !== 3 || hash[0] !== '#gridconfig' || hash[1] !== resource) {
+                return config;
+            }
+            return $.parseJSON(hash[2]);
         }
     };
 
