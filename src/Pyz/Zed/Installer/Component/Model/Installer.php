7cc0a0651011282c45a8c5018af7ef5c98cabf1f
Jonathan Reisdorf <jonathan.reisdorf@project-a.com>
2013-11-06 16:39:10 +0000

    manual merge conflict fix

--- src/Pyz/Zed/Installer/Component/Model/Installer.php
+++ src/Pyz/Zed/Installer/Component/Model/Installer.php
@@ -1,37 +1,12 @@
 <?php
+
 namespace Pyz\Zed\Installer\Component\Model;
 
-<<<<<<< HEAD
-class Pyz_Zed_Installer_Component_Model_Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implements
-    \Generated\Zed\Acl\Component\Dependency\AclFacadeInterface,
-    \Generated\Zed\Catalog\Component\Dependency\CatalogFacadeInterface,
-    \Generated\Zed\Category\Component\Dependency\CategoryFacadeInterface,
-    \Generated\Zed\Customer\Component\Dependency\CustomerFacadeInterface,
-    \Generated\Zed\Cms\Component\Dependency\CmsFacadeInterface,
-    \Generated\Zed\Misc\Component\Dependency\MiscFacadeInterface,
-    \Generated\Zed\Price\Component\Dependency\PriceFacadeInterface,
-    \Generated\Zed\Stock\Component\Dependency\StockFacadeInterface,
-    \Generated\Zed\Glossary\Component\Dependency\GlossaryFacadeInterface,
-    \Generated\Zed\Sales\Component\Dependency\SalesFacadeInterface,
-    \Generated\Zed\ProductImage\Component\Dependency\ProductImageFacadeInterface
-{
-
-    use \Generated\Zed\Acl\Component\Dependency\AclFacadeTrait;
-    use \Generated\Zed\Catalog\Component\Dependency\CatalogFacadeTrait;
-    use \Generated\Zed\Category\Component\Dependency\CategoryFacadeTrait;
-    use \Generated\Zed\Customer\Component\Dependency\CustomerFacadeTrait;
-    use \Generated\Zed\Cms\Component\Dependency\CmsFacadeTrait;
-    use \Generated\Zed\Misc\Component\Dependency\MiscFacadeTrait;
-    use \Generated\Zed\Price\Component\Dependency\PriceFacadeTrait;
-    use \Generated\Zed\Stock\Component\Dependency\StockFacadeTrait;
-    use \Generated\Zed\Glossary\Component\Dependency\GlossaryFacadeTrait;
-    use \Generated\Zed\Sales\Component\Dependency\SalesFacadeTrait;
-    use \Generated\Zed\ProductImage\Component\Dependency\ProductImageFacadeTrait;
-=======
 use \Generated\Zed\Acl\Component\Dependency\AclFacadeInterface;
 use \Generated\Zed\Catalog\Component\Dependency\CatalogFacadeInterface;
 use \Generated\Zed\Category\Component\Dependency\CategoryFacadeInterface;
 use \Generated\Zed\Cms\Component\Dependency\CmsFacadeInterface;
+use \Generated\Zed\Customer\Component\Dependency\CustomerFacadeInterface;
 use \Generated\Zed\Misc\Component\Dependency\MiscFacadeInterface;
 use \Generated\Zed\Price\Component\Dependency\PriceFacadeInterface;
 use \Generated\Zed\Stock\Component\Dependency\StockFacadeInterface;
@@ -45,6 +20,7 @@ use \Generated\Zed\Acl\Component\Dependency\AclFacadeTrait;
 use \Generated\Zed\Catalog\Component\Dependency\CatalogFacadeTrait;
 use \Generated\Zed\Category\Component\Dependency\CategoryFacadeTrait;
 use \Generated\Zed\Cms\Component\Dependency\CmsFacadeTrait;
+use \Generated\Zed\Customer\Component\Dependency\CustomerFacadeTrait;
 use \Generated\Zed\Misc\Component\Dependency\MiscFacadeTrait;
 use \Generated\Zed\Price\Component\Dependency\PriceFacadeTrait;
 use \Generated\Zed\Stock\Component\Dependency\StockFacadeTrait;
@@ -59,6 +35,7 @@ class Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implem
     CatalogFacadeInterface,
     CategoryFacadeInterface,
     CmsFacadeInterface,
+    CustomerFacadeInterface,
     MiscFacadeInterface,
     PriceFacadeInterface,
     StockFacadeInterface,
@@ -73,6 +50,7 @@ class Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implem
     use CatalogFacadeTrait;
     use CategoryFacadeTrait;
     use CmsFacadeTrait;
+    use CustomerFacadeTrait;
     use MiscFacadeTrait;
     use PriceFacadeTrait;
     use StockFacadeTrait;
@@ -81,7 +59,6 @@ class Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implem
     use ProductImageFacadeTrait;
     use InvoiceFacadeTrait;
     use DocumentFacadeTrait;
->>>>>>> 96feb4970f0000440d40801d3d6b861484786485
 
     /**
      * @return array
