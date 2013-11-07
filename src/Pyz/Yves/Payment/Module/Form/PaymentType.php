329905a3e7ebd94a04f2ee5e25d3e35a9b598f25
Jonathan Reisdorf <jonathan.reisdorf@project-a.com>
2013-11-07 16:33:07 +0000

    paymenttype change

--- src/Pyz/Yves/Payment/Module/Form/PaymentType.php
+++ src/Pyz/Yves/Payment/Module/Form/PaymentType.php
@@ -39,23 +39,24 @@ class PaymentType extends CorePaymentType
         ->add('ccVerification', 'text')
         ->add('ccExpirationMonth', 'choice', [
             'choices' => [
-                '1' => '01',
-                '2' => '02',
-                '3' => '03',
-                '4' => '04',
-                '5' => '05',
-                '6' => '06',
-                '7' => '07',
-                '8' => '08',
-                '9' => '09',
-                '10' => '10',
-                '11' => '11',
-                '12' => '12']
+                1 => '01',
+                2 => '02',
+                3 => '03',
+                4 => '04',
+                5 => '05',
+                6 => '06',
+                7 => '07',
+                8 => '08',
+                9 => '09',
+                10 => '10',
+                11 => '11',
+                12 => '12']
         ]);
 
         $ccExpYears = [];
+        $currentYear = (int)date('Y');
         for ($i = 0; $i < 10; $i++) {
-            $ccExpYears[(int)date('Y') + $i] = (int)date('Y') + $i;
+            $ccExpYears[$currentYear + $i] = $currentYear + $i;
         }
         $builder->add('ccExpirationYear', 'choice', ['choices' => $ccExpYears]);
     }
