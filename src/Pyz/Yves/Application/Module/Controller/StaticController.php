c61acc04261a1af2825540e98282f62f4e42aa47
Daniel Tschinder <daniel.tschinder@project-a.com>
2013-11-08 18:41:15 +0100

    Refactoring: ->request in Controllers removed, use Request injection; Bootstrap:getRequest() removed, as request may change over time; ->app in Controller not there anymore use ->getApplication(); Removed injection of Application in RepsonseMessageHelper (NEVER inject Application or Request), Added some TODOs, Customer and Security needs complete refactoring, sorry

--- src/Pyz/Yves/Application/Module/Controller/StaticController.php
+++ src/Pyz/Yves/Application/Module/Controller/StaticController.php
@@ -2,18 +2,20 @@
 namespace Pyz\Yves\Application\Module\Controller;
 
 use ProjectA\Yves\Library\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class StaticController extends Controller
 {
 
     /**
+     * @param Request $request
      * @return array
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $routeName = $this->request->attributes->get('_route');
+        $routeName = $request->attributes->get('_route');
         $keyName = 'static.pages.' . $routeName;
-        $content = $this->app->trans($keyName);
+        $content = $this->getApplication()->trans($keyName);
 
         return [
             'data' => $content
--- src/Pyz/Yves/Catalog/Module/Controller/CatalogController.php
+++ src/Pyz/Yves/Catalog/Module/Controller/CatalogController.php
@@ -4,6 +4,7 @@ namespace Pyz\Yves\Catalog\Module\Controller;
 use ProjectA\Yves\Catalog\Module\Controller\CatalogController as CoreCatalogController;
 use Pyz\Yves\Catalog\Component\Model\FacetConfig;
 use Pyz\Yves\Catalog\Component\Model\FacetSearch;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @package Pyz\Yves\Catalog\Module\Controller
@@ -12,11 +13,12 @@ class CatalogController extends CoreCatalogController
 {
     /**
      * @param FacetConfig $facetConfig
+     * @param Request $request
      * @return array
      */
-    public function indexAction(FacetConfig $facetConfig)
+    public function indexAction(FacetConfig $facetConfig, Request $request)
     {
-        $search = new FacetSearch($this->request, $facetConfig);
+        $search = $this->getFactory()->createCatalogModelFacetSearch($request, $facetConfig); // TODO Change to RequestStack as of Symfony 2.4
         $result = $search->getResult();
 
         return $result;
