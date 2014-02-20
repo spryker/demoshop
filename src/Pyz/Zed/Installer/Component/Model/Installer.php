41ea327ea875bef5f9211edd7f52306bf67671ba
René Klatt <rene.klatt@project-a.com>
2014-02-20 12:44:30 +0000

    Removed cms update lib to 3.1.0

--- src/Pyz/Zed/Installer/Component/Model/Installer.php
+++ src/Pyz/Zed/Installer/Component/Model/Installer.php
@@ -34,7 +34,6 @@ use \Generated\Zed\Payone\Component\Dependency\PayoneFacadeTrait;
 
 class Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implements
     AclFacadeInterface,
-    CmsFacadeInterface,
     CatalogFacadeInterface,
     CategoryFacadeInterface,
     CustomerFacadeInterface,
@@ -50,7 +49,6 @@ class Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implem
 {
 
     use AclFacadeTrait;
-    use CmsFacadeTrait;
     use CatalogFacadeTrait;
     use CategoryFacadeTrait;
     use CustomerFacadeTrait;
@@ -71,7 +69,6 @@ class Installer extends \ProjectA_Zed_Installer_Component_Model_Installer implem
     {
         return [
             $this->facadeAcl->createInternalInstall(),
-            $this->facadeCms->createInternalInstall(),
             $this->facadeCatalog->createInternalInstall(),
             $this->facadeCategory->createInternalInstall(),
             $this->facadeCustomer->createInternalInstall(),
