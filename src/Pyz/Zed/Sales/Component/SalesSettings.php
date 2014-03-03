2d24c65c1051b4cff04fb46e9e9c7739dfa28208
Jörg Stick <jstick@PAV-NB-C02JN19BF1G4.local>
2014-03-03 10:38:13 +0100

    fixed attributes in twig files cart

--- src/Pyz/Zed/Sales/Component/SalesSettings.php
+++ src/Pyz/Zed/Sales/Component/SalesSettings.php
@@ -43,22 +43,22 @@ class SalesSettings extends ProjectASalesSettings
         switch ($method) {
             case PayoneApiConstants::PAYMENT_METHOD_CREDITCARD:
             case PayoneApiConstants::PAYMENT_METHOD_CREDITCARD_PSEUDO:
-                return Orderprocess::ORDER_PROCESS_PAYONE_CREDIT_CARD;
+                return Orderprocess::ORDER_PROCESS_PAYONE_CREDIT_CARD_PSEUDO_01;
                 break;
             case PayoneApiConstants::PAYMENT_METHOD_PAYPAL:
-                return Orderprocess::ORDER_PROCESS_PAYONE_PAYPAL;
+                return Orderprocess::ORDER_PROCESS_PAYONE_PAYPAL_01;
                 break;
             case PayoneApiConstants::PAYMENT_METHOD_DIRECT_DEBIT:
-                return Orderprocess::ORDER_PROCESS_PAYONE_DIRECT_DEBIT;
+                return Orderprocess::ORDER_PROCESS_PAYONE_DIRECT_DEBIT_01;
                 break;
             case PayoneApiConstants::PAYMENT_METHOD_PREPAYMENT:
-                return Orderprocess::ORDER_PROCESS_PAYONE_PREPAYMENT;
+                return Orderprocess::ORDER_PROCESS_PAYONE_PRE_PAYMENT_01;
                 break;
             case PayoneApiConstants::PAYMENT_METHOD_INVOICE:
-                return Orderprocess::ORDER_PROCESS_PAYONE_INVOICE;
+                return Orderprocess::ORDER_PROCESS_PAYONE_INVOICE_01;
                 break;
             case PayoneApiConstants::PAYMENT_METHOD_SOFORT_UEBERWEISUNG:
-                return Orderprocess::ORDER_PROCESS_PAYONE_SOFORT_UEBERWEISUNG;
+                return Orderprocess::ORDER_PROCESS_PAYONE_SOFORT_UEBERWEISUNG_01;
                 break;
             default:
                 throw new \RuntimeException('Could not find any statemachine process for new order');
