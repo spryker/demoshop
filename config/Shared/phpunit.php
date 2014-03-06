e9d09a02b5b3c44c7b26d209d98e82deb0da55fc
René Klatt <rene.klatt@project-a.com>
2014-03-06 07:46:32 +0000

    Fixed unit text bootstraps

--- config/Shared/phpunit.php
+++ config/Shared/phpunit.php
@@ -1,5 +1,5 @@
 <?php
 
 require_once __DIR__ . '/../../vendor/autoload.php';
-$bootstrap = ProjectA\Shared\Library\SystemUnderTest\Bootstrap::getInstance();
-$bootstrap->bootstrap(\ProjectA\Shared\Library\SystemUnderTest\Bootstrap::APPLICATION_SHARED);
+$bootstrap = ProjectA\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
+$bootstrap->bootstrap(\ProjectA\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::APPLICATION_SHARED);
