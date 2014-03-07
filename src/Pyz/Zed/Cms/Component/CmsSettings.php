dc996a1de74cc9c136d5b81921a704b76c75e121
Christian Burgas <christian.burgas@project-a.com>
2014-03-07 15:16:04 +0000

    added new settings

---
+++ src/Pyz/Zed/Cms/Component/CmsSettings.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Pyz\Zed\Cms\Component;
+
+use ProjectA\Zed\Cms\Component\CmsSettings as CoreCmsSettings;
+use ProjectA\Zed\Cms\Component\Model\Block\BlockText;
+use ProjectA\Zed\Cms\Component\Model\Block\BlockGlossary;
+use ProjectA\Zed\Cms\Component\Model\Block\BlockProduct;
+
+class CmsSettings extends CoreCmsSettings
+{
+    /**
+     * @return array
+     */
+    public function getTemplates()
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function getPartials()
+    {
+        $partials = [];
+
+        $partials[] = [
+            'file' => 'text-width-12.twig',
+            'name' => 'text-width-12',
+            'type' => BlockText::BLOCK_TYPE,
+            'size' => 12,
+            'description' => 'width 4/4',
+        ];
+
+        $partials[] = [
+            'file' => 'text-width-6.twig',
+            'name' => 'text-width-6',
+            'type' => BlockText::BLOCK_TYPE,
+            'size' => 6,
+            'description' => 'width 2/4',
+        ];
+
+        $partials[] = [
+            'file' => 'product-width-6.twig',
+            'name' => 'product-width-6',
+            'type' => BlockProduct::BLOCK_TYPE,
+            'size' => 6,
+            'description' => 'width 2/4',
+        ];
+
+        $partials[] = [
+            'file' => 'product-width-12.twig',
+            'name' => 'product-width-12',
+            'type' => BlockProduct::BLOCK_TYPE,
+            'size' => 12,
+            'description' => 'width 4/4',
+        ];
+
+        $partials[] = [
+            'file' => 'product-width-4.twig',
+            'name' => 'product-width-4',
+            'type' => BlockProduct::BLOCK_TYPE,
+            'size' => 4,
+            'description' => 'width 1/3',
+        ];
+
+        $partials[] = [
+            'file' => 'product-teaser-width-4.twig',
+            'name' => 'product-teaser-width-4',
+            'type' => BlockProduct::BLOCK_TYPE,
+            'size' => 4,
+            'description' => 'width 1/3',
+        ];
+
+        return $partials;
+    }
+
+} 
