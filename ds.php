3bb4bba9a3b47df10ae0777122f22af867b8fe39
René Klatt <rene.klatt@project-a.com>
2015-01-07 12:58:41 +0000

    script wip

--- ds.php
+++ ds.php
@@ -10,8 +10,8 @@ $baseDirs = [
     $projectDirPattern
 ];
 
-$search = 'class\s(.*)(?:Controller)\s(?!extends)';
-$replace = 'use ProjectA\Zed\Application\Business\Communication\Controller\AbstractController;
+$search = '/class\s(.*)(?:Controller)\s(?!extends)/';
+$replace = 'use ProjectA\Zed\Application\Communication\Controller\AbstractController;
 
 class $1Controller extends AbstractController';
 
@@ -20,7 +20,14 @@ $finder = new \Symfony\Component\Finder\Finder();
 /* @var $file \Symfony\Component\Finder\SplFileInfo */
 foreach ($finder->files()->in($baseDirs)->name('*Controller.php') as $file) {
     $content = $file->getContents();
-    if (preg_match($search, $content, $matches)) {
-        echo '<pre>' . PHP_EOL . var_dump($matches) . PHP_EOL . 'Line: ' . __LINE__ . PHP_EOL . 'File: ' . __FILE__ . die();
-    }
+    $callback = function ($matches) use ($file, $search, $replace) {
+        $content = $file->getContents();
+        if (strstr($content, 'extends') === false) {
+            $result = preg_replace($search, $replace, $content);
+            $Result = str_replace('AbstractControllerimplements', 'AbstractController implements', $result);
+            echo $file->getPathname() . PHP_EOL;
+            file_put_contents($file->getPathname(), $result);
+        }
+    };
+    preg_replace_callback($search, $callback, $content);
 }
