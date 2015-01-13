b593febd524761bcc189f4d8f9e1a2f7f41db5a9
Alberto Assmann <alberto.assmann@project-a.com>
2015-01-13 18:56:32 +0100

    - fix renamed interface

--- src/Pyz/Zed/Product/Business/ProductFacade.php
+++ src/Pyz/Zed/Product/Business/ProductFacade.php
@@ -5,13 +5,14 @@ namespace Pyz\Zed\Product\Business;
 
 use ProjectA\Zed\Product\Business\ProductFacade as CoreProductFacade;
 use ProjectA\Zed\ProductExporter\Business\Builder\MultipleProductsBuilderInterface;
+use ProjectA\Zed\ProductExporter\Business\Builder\ProductExporterToProductInterface;
 
 /**
  * Class ProductFacade
  *
  * @package Pyz\Zed\Product\Business
  */
-class ProductFacade extends CoreProductFacade implements MultipleProductsBuilderInterface
+class ProductFacade extends CoreProductFacade implements ProductExporterToProductInterface
 {
     /**
      * @param array $productsData
