260e728aa487fad776201f51c63118f5e7fb7cac
Lucas Manzke <lmanzke@outlook.com>
2015-01-23 18:06:27 +0100

    refactor a bit add new integration tests for synchronizing keys and creating translations

--- tests/unit/Glossary2Test.php
+++ tests/unit/Glossary2Test.php
@@ -32,7 +32,7 @@ class Glossary2Test extends \Codeception\TestCase\Test
 
     public function testCreateKeyInsertsSomething()
     {
-        $keyQuery = $this->glossaryQueryContainer->getKeyQuery();
+        $keyQuery = $this->glossaryQueryContainer->getKeyListQuery();
         $keyCountBeforeCreation = $keyQuery->count();
 
         $this->glossaryFacade->createKey('ATestKey');
@@ -42,8 +42,9 @@ class Glossary2Test extends \Codeception\TestCase\Test
 
     public function testDeleteKeyDeletesSomething()
     {
-        $keyQuery = $this->glossaryQueryContainer->getKeyQuery();
+        $keyQuery = $this->glossaryQueryContainer->getKeyListQuery();
 
+        //Is the usage of another facade method in this test problematic?
         $this->glossaryFacade->createKey('KeyToBeDeleted');
 
         $keyCountBeforeDeletion = $keyQuery->count();
@@ -51,5 +52,40 @@ class Glossary2Test extends \Codeception\TestCase\Test
         $keyCountAfterDeletion = $keyQuery->count();
 
         $this->assertTrue($keyCountAfterDeletion < $keyCountBeforeDeletion);
+    }
+
+    public function testSynchronizeFilesWritesToDatabase()
+    {
+        /*
+         * This test is not yet final.
+         * Current Problem:
+         * It might work, but can also fail, as we know nothing about the preconditions (the database state)
+         * The method tests the sync between a config file with static files and the db
+         * It should not insert new records if these are already synced.
+         * However, this test knows nothing about the state.
+         * Possible Solution: A nice way to use another key file source (or other input) under the hood,
+         * i.e. by providing an overriding implementation of the file parser for the test
+         * Currently one could manually switch the concrete implementation in the glossary dependency container
+         */
+
+        $keyQuery = $this->glossaryQueryContainer->getKeyListQuery();
+
+        $keyCountBeforeSynchronization = $keyQuery->count();
+        $this->glossaryFacade->synchronize();
+        $keyCountAfterSynchronization = $keyQuery->count();
+
+        $this->assertTrue($keyCountAfterSynchronization > $keyCountBeforeSynchronization);
+    }
+
+    public function testCreateTranslationInsertsSomething()
+    {
+        $translationQuery = $this->glossaryQueryContainer->getTranslationListQuery();
+        $this->glossaryFacade->createKey('AKey');
+
+        $translationCountBeforeCreation = $translationQuery->count();
+        $this->glossaryFacade->saveTranslation('AKey', 'Local', 'ATranslation');
+        $translationCountAfterCreation = $translationQuery->count();
+
+        $this->assertTrue($translationCountAfterCreation > $translationCountBeforeCreation);
     }
 }
