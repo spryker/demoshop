31513ba3df4828fa1cb08f860cbd1c37c5b589a1
Alberto Assmann <alberto.assmann@project-a.com>
2015-01-27 11:39:21 +0100

    Revert "- remove product attribute to operations mapping demo installer (this is now possible via settings)"
    This reverts commit 8e01a9f8aa0b0c44df0768c712a1c83df86f5626.

--- src/Pyz/Zed/Product/Business/Internal/DemoData/ProductDataInstall.php
+++ src/Pyz/Zed/Product/Business/Internal/DemoData/ProductDataInstall.php
@@ -98,6 +98,57 @@ class ProductDataInstall implements DemoDataInstallInterface
     public function install(Console $console)
     {
         $this->createProduct();
+        $this->createAttributes();
+    }
+
+    protected function createAttributes()
+    {
+        $attributes = [
+            'theme' => 'string',
+            'color' => 'list',
+            'radius' => 'float',
+            'cable_length' => 'float',
+            'weight' => 'float',
+            'light_bulb' => 'integer',
+            'socket' => 'string',
+            'length' => 'float',
+            'width' => 'float',
+            'height' => 'float',
+            'image_url' => 'string',
+            'thumbnail_url' => 'string',
+            'price' => 'float'
+        ];
+
+        $typeEntities = \ProjectA_Zed_Product_Persistence_Propel_PacProductAttributeTypeQuery::create()
+            ->select(
+                [
+                    \ProjectA_Zed_Product_Persistence_Propel_PacProductAttributeTypePeer::NAME,
+                    \ProjectA_Zed_Product_Persistence_Propel_PacProductAttributeTypePeer::TYPE_ID
+                ]
+            )->setFormatter(
+                new \PropelSimpleArrayFormatter()
+            )->find()->getArrayCopy();
+
+        $types = [];
+        foreach ($typeEntities as $typeEntity) {
+            $typeKey = array_shift($typeEntity);
+            $types[$typeKey] = array_shift($typeEntity);
+        }
+
+        foreach ($attributes as $attribute => $type) {
+            if (\ProjectA_Zed_Product_Persistence_Propel_PacProductAttributesMetadataQuery::create()->findOneByKey($attribute)) {
+                continue;
+            }
+
+            if (array_key_exists($type, $types)) {
+                $attributeEntity = new \ProjectA_Zed_Product_Persistence_Propel_PacProductAttributesMetadata();
+                $attributeEntity->setAttributeId(null);
+                $attributeEntity->setKey($attribute);
+                $attributeEntity->setTypeId($types[$type]);
+                $attributeEntity->setIsEditable(true);
+                $attributeEntity->save();
+            }
+        }
     }
 
     /**
--- src/Pyz/Zed/ProductSearch/Business/ProductSearchSettings.php
+++ src/Pyz/Zed/ProductSearch/Business/ProductSearchSettings.php
@@ -35,55 +35,6 @@ class ProductSearchSettings extends CoreProductSearchSettings
                 'CopyToField' => [
                     'integer-sort'
                 ]
-            ],
-            'theme' => [
-                'CopyToField' => [
-                    'full-text-boosted',
-                    'full-text',
-                    'suggestion-term',
-                    'completion-terms'
-                ]
-            ],
-            'radius' => [
-                'CopyToFacet' => [
-                    'float-facet'
-                ]
-            ],
-            'cable_length'=> [
-                'CopyToFacet' => [
-                    'float-facet'
-                ]
-            ],
-            'weight'=> [
-                'CopyToFacet' => [
-                    'float-facet'
-                ]
-            ],
-            'light_bulb'=> [
-                'CopyToFacet' => [
-                    'integer-facet'
-                ]
-            ],
-            'socket' => [
-                'CopyToField' => [
-                    'full-text',
-                    'completion-terms'
-                ]
-            ],
-            'length' => [
-                'CopyToFacet' => [
-                    'float-facet'
-                ]
-            ],
-            'width'=> [
-                'CopyToFacet' => [
-                    'float-facet'
-                ]
-            ],
-            'height'=> [
-                'CopyToFacet' => [
-                    'float-facet'
-                ]
             ]
         ];
 
