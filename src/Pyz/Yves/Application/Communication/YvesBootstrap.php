24661bca6c556e54d804e6f5cdd0d874c5439a0e
René Klatt <rene.klatt@project-a.com>
2015-02-12 10:55:10 +0000

    Renamed YvesExporter

--- src/Pyz/Yves/Application/Communication/YvesBootstrap.php
+++ src/Pyz/Yves/Application/Communication/YvesBootstrap.php
@@ -163,9 +163,9 @@ class YvesBootstrap extends Bootstrap
         return [
             Factory::getInstance()->createSetupModelRouterMonitoringRouter($app),
             Factory::getInstance()->createCmsModelRouterRedirectRouter($app),
-//            Factory::getInstance()->createYvesExportDependencyContainer()->createKvStorageRouter($app)
-//                ->addResourceCreator($productResourceCreator)
-//                ->addResourceCreator($categoryResourceCreator),
+            Factory::getInstance()->createFrontendExporterDependencyContainer()->createKvStorageRouter($app)
+                ->addResourceCreator($productResourceCreator)
+                ->addResourceCreator($categoryResourceCreator),
             Factory::getInstance()->createCatalogModelRouterSearchRouter($app),
             Factory::getInstance()->createCmsModelRouterCmsRouter($app),
             Factory::getInstance()->createCartModelRouterCartRouter($app),
@@ -184,7 +184,10 @@ class YvesBootstrap extends Bootstrap
     protected function globalTemplateVariables(Application $app)
     {
         return [
-            'categories' => Factory::getInstance()->createCategoryExporterDependencyContainer()->createNavigation()->getCategories($app['locale']),
+            'categories' => Factory::getInstance()
+                ->createCategoryExporterDependencyContainer()
+                ->createNavigation()
+                ->getCategories($app['locale']),
             'cartItemCount' => Factory::getInstance()->createCartModelSessionCartCount($app->getSession())->getCount(),
             'tracking' => Tracking::getInstance(),
             'environment' => \ProjectA_Shared_Library_Environment::getEnvironment(),
