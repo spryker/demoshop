6f0de9ceda2fdfd5beab8efe082e260d42af0760
Lucas Manzke <lmanzke@outlook.com>
2015-03-19 18:14:50 +0100

    refactor demo data installer

--- src/Pyz/Zed/Product/Business/ProductDependencyContainer.php
+++ src/Pyz/Zed/Product/Business/ProductDependencyContainer.php
@@ -24,10 +24,23 @@ class ProductDependencyContainer extends CoreDependencyContainer
      */
     public function getDemoDataInstaller(LoggerInterface $logger = null)
     {
-        $installer = $this->factory->createInternalDemoDataProductDataInstall();
+        $installer = $this->factory->createInternalDemoDataProductDataInstall(
+            $this->getAttributeManager(),
+            $this->getProductManager(),
+            $this->getLocaleFacade(),
+            $this->getCSVReader(),
+            $this->getCSVPath()
+        );
         $installer->setLogger($logger);
 
         return $installer;
     }
 
+    /**
+     * @return string
+     */
+    protected function getCSVPath()
+    {
+        return __DIR__ . '/Internal/DemoData/demo-product-data.csv';
+    }
 }
