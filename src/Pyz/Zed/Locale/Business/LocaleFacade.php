1fb299a961bc5ddde5102f5c6c2ccfd8d46e0e9b
Lucas Manzke <lmanzke@outlook.com>
2015-03-27 10:19:52 +0100

    make locale facade implement additional interfaces

--- src/Pyz/Zed/Locale/Business/LocaleFacade.php
+++ src/Pyz/Zed/Locale/Business/LocaleFacade.php
@@ -3,10 +3,12 @@
 
 namespace Pyz\Zed\Locale\Business;
 
+use ProjectA\Zed\Category\Dependency\Facade\CategoryToLocaleInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToLocaleInterface;
+use ProjectA\Zed\ProductCategory\Dependency\Facade\ProductCategoryToLocaleInterface;
 use SprykerCore\Zed\Locale\Business\LocaleFacade as CoreLocaleFacade;
 
-class LocaleFacade extends CoreLocaleFacade implements ProductToLocaleInterface
+class LocaleFacade extends CoreLocaleFacade implements ProductToLocaleInterface, CategoryToLocaleInterface, ProductCategoryToLocaleInterface
 {
 
 }
