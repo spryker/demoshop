12d8b0311d558c989e2ed4f2ac780e8353781066
Lucas Manzke <lmanzke@outlook.com>
2015-03-30 22:18:54 +0200

    implement project level dependency interfaces

--- src/Pyz/Zed/Touch/Business/TouchFacade.php
+++ src/Pyz/Zed/Touch/Business/TouchFacade.php
@@ -3,13 +3,15 @@
 
 namespace Pyz\Zed\Touch\Business;
 
+use ProjectA\Zed\Category\Dependency\Facade\CategoryToTouchInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToTouchInterface;
 use SprykerCore\Zed\Touch\Business\TouchFacade as CoreTouchFacade;
 use SprykerFeature\Zed\Url\Dependency\UrlToTouchInterface;
 
 class TouchFacade extends CoreTouchFacade implements
     ProductToTouchInterface,
-    UrlToTouchInterface
+    UrlToTouchInterface,
+    CategoryToTouchInterface
 {
 
 }
--- src/Pyz/Zed/Url/Business/UrlFacade.php
+++ src/Pyz/Zed/Url/Business/UrlFacade.php
@@ -3,10 +3,13 @@
 
 namespace Pyz\Zed\Url\Business;
 
+use ProjectA\Zed\Category\Dependency\Facade\CategoryToUrlInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToUrlInterface;
 use SprykerFeature\Zed\Url\Business\UrlFacade as CoreUrlFacade;
 
-class UrlFacade extends CoreUrlFacade implements ProductToUrlInterface
+class UrlFacade extends CoreUrlFacade implements
+    CategoryToUrlInterface,
+    ProductToUrlInterface
 {
 
 }
