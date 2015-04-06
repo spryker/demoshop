ba0d09bed80b4e611fa1ebea5de95fbbc5fcc3ac
Lucas Manzke <lmanzke@outlook.com>
2015-04-06 20:57:00 +0200

    rename corelocalefacade to sprykerlocalefacade

--- src/Pyz/Zed/Locale/Business/LocaleFacade.php
+++ src/Pyz/Zed/Locale/Business/LocaleFacade.php
@@ -6,10 +6,10 @@ namespace Pyz\Zed\Locale\Business;
 use ProjectA\Zed\Category\Dependency\Facade\CategoryToLocaleInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToLocaleInterface;
 use ProjectA\Zed\ProductCategory\Dependency\Facade\ProductCategoryToLocaleInterface;
-use SprykerCore\Zed\Locale\Business\LocaleFacade as CoreLocaleFacade;
+use SprykerCore\Zed\Locale\Business\LocaleFacade as SprykerLocaleFacade;
 use SprykerFeature\Zed\Url\Dependency\UrlToLocaleInterface;
 
-class LocaleFacade extends CoreLocaleFacade implements
+class LocaleFacade extends SprykerLocaleFacade implements
     ProductToLocaleInterface,
     CategoryToLocaleInterface,
     ProductCategoryToLocaleInterface,
