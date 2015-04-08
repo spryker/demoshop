17954f12cd8b85e80e2882c0514bc38b85d90465
Lucas Manzke <lmanzke@outlook.com>
2015-04-08 15:47:43 +0200

    make url facade implement cms interface

--- src/Pyz/Zed/Url/Business/UrlFacade.php
+++ src/Pyz/Zed/Url/Business/UrlFacade.php
@@ -4,10 +4,12 @@ namespace Pyz\Zed\Url\Business;
 
 use ProjectA\Zed\Category\Dependency\Facade\CategoryToUrlInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToUrlInterface;
+use sprykerfeature\Zed\Cms\Dependency\Facade\CmsToUrlInterface;
 use SprykerFeature\Zed\Url\Business\UrlFacade as SprykerUrlFacade;
 
 class UrlFacade extends SprykerUrlFacade implements
     CategoryToUrlInterface,
+    CmsToUrlInterface,
     ProductToUrlInterface
 {
 
