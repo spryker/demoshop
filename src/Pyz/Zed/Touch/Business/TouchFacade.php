ef8f491e07057d2c15193a04f610f78e90099490
Lucas Manzke <lucas.manzke@spryker.com>
2015-04-09 22:46:21 +0000

    implement pricetotouch interface in facade

--- src/Pyz/Zed/Touch/Business/TouchFacade.php
+++ src/Pyz/Zed/Touch/Business/TouchFacade.php
@@ -3,6 +3,7 @@
 namespace Pyz\Zed\Touch\Business;
 
 use ProjectA\Zed\Category\Dependency\Facade\CategoryToTouchInterface;
+use ProjectA\Zed\Price\Dependency\Facade\PriceToTouchInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToTouchInterface;
 use ProjectA\Zed\ProductSearch\Dependency\Facade\ProductSearchToTouchInterface;
 use SprykerCore\Zed\Touch\Business\TouchFacade as SprykerTouchFacade;
@@ -11,6 +12,7 @@ use SprykerFeature\Zed\Url\Dependency\UrlToTouchInterface;
 class TouchFacade extends SprykerTouchFacade implements
     CategoryToTouchInterface,
     ProductSearchToTouchInterface,
+    PriceToTouchInterface,
     ProductToTouchInterface,
     UrlToTouchInterface
 {
