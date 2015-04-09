4892c3d3c7c9a6312fc5485d888213da574219d0
Lucas Manzke <lmanzke@outlook.com>
2015-04-09 19:21:31 +0200

    update composer

--- src/Pyz/Zed/Touch/Business/TouchFacade.php
+++ src/Pyz/Zed/Touch/Business/TouchFacade.php
@@ -7,6 +7,7 @@
 namespace Pyz\Zed\Touch\Business;
 
 use ProjectA\Zed\Category\Dependency\Facade\CategoryToTouchInterface;
+use ProjectA\Zed\Price\Dependency\Facade\PriceToTouchInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToTouchInterface;
 use ProjectA\Zed\ProductSearch\Dependency\Facade\ProductSearchToTouchInterface;
 use SprykerCore\Zed\Touch\Business\TouchFacade as SprykerTouchFacade;
@@ -17,6 +18,7 @@ class TouchFacade extends SprykerTouchFacade implements
     CategoryToTouchInterface,
     GlossaryToTouchInterface,
     ProductSearchToTouchInterface,
+    PriceToTouchInterface,
     ProductToTouchInterface,
     UrlToTouchInterface
 {
--- src/Pyz/Zed/Url/Business/UrlFacade.php
+++ src/Pyz/Zed/Url/Business/UrlFacade.php
@@ -4,7 +4,7 @@ namespace Pyz\Zed\Url\Business;
 
 use ProjectA\Zed\Category\Dependency\Facade\CategoryToUrlInterface;
 use ProjectA\Zed\Product\Dependency\Facade\ProductToUrlInterface;
-use sprykerfeature\Zed\Cms\Dependency\Facade\CmsToUrlInterface;
+use SprykerFeature\Zed\Cms\Dependency\Facade\CmsToUrlInterface;
 use SprykerFeature\Zed\Url\Business\UrlFacade as SprykerUrlFacade;
 
 class UrlFacade extends SprykerUrlFacade implements
