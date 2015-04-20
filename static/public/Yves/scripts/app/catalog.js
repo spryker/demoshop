cccec6da4c20f29d7307667938523adee6a6cabd
René Klatt <rene.klatt@project-a.com>
2015-04-20 09:36:06 +0000

    Readd files

---
+++ static/public/Yves/scripts/app/catalog.js
@@ -0,0 +1,229 @@
+app.catalog = {
+    init : function() {
+      this.rangeSlider.init();
+      //this.zoom.init();
+      this.filters.init();
+      this.options.init();
+    },
+    zoom : {
+        vars : {
+            elClass : 'zoom',
+            currentClass : '#product .pictures .current',
+            originalSize : 1008
+        },
+        init : function() {
+            var vars = this.vars;
+            $(vars.currentClass).mouseenter(function() {
+                $('.' + vars.elClass).remove();
+                var bgImgCss = 'url(' + $(this).children('img').attr('src') + ')';
+                vars.$zoom = $('<div class="' + vars.elClass + '"></div>').css({ backgroundImage : bgImgCss });
+                $(this).append(vars.$zoom);
+            });
+
+            $(vars.currentClass).mousemove(function(e) {
+                var coords = this.getBoundingClientRect();
+                if (coords.left > e.clientX || coords.right < e.clientX || coords.top > e.clientY || coords.bottom < e.clientY) {
+                    vars.$zoom.remove();
+                    return;
+                }
+                var factor = vars.originalSize / (coords.width - 2);
+                var bgpos = (((coords.left - e.clientX) * factor) + 100) + 'px ' + (((coords.top - e.clientY) * factor) + 100) + 'px';
+                vars.$zoom.css({ left : e.clientX - 100, top : e.clientY - 100, backgroundPosition : bgpos });
+            });
+
+            $(vars.currentClass).mouseleave(function() {
+                vars.$zoom.remove();
+            });
+        }
+    },
+    rangeSlider : {
+        init : function() {
+            var $targets = $('.rangeSlider.max');
+            $targets.each(function() {
+                var rangeSlider = app.catalog.rangeSlider;
+                var $container = $('<div class="rangeSliderContainer"></div>')
+                    .data({ min : parseInt($(this).prev('.min').attr('min')), max : parseInt($(this).attr('max')) });
+                var $min = $('<div class="min"></div>');
+                var $max = $('<div class="max"></div>');
+                var $range = $('<div class="range"></div>');
+                $container.append($min).append($max).append($range);
+                $(this).after($container);
+                rangeSlider.adjust($container);
+                $min.mousedown(rangeSlider.down);
+                $max.mousedown(rangeSlider.down);
+            });
+
+            this.initConnectors();
+        },
+        initConnectors : function() {
+            var rangeSlider = this;
+            $('.rangeSliderConnector').bind('input paste', function() {
+                var value = $(this).val().replace(/[^\d]/, '');
+                $(this).val(value);
+                var type = $(this).hasClass('min') ? 'min' : 'max';
+                rangeSlider.tmpConfig.type = type;
+                var $container = (type === 'min' ? $(this).prev() : $(this).prev().prev());
+                var config = rangeSlider.getConfig($container);
+                if (isNaN(parseInt(value)) || parseInt(value) < config.min) {
+                    return;
+                }
+                var $el = rangeSlider['$get' + (type === 'min' ? 'Min' : 'Max')]($container);
+                $el.val(parseInt(value));
+                rangeSlider.adjust($container);
+            }).change(function() {
+                var type = $(this).hasClass('min') ? 'min' : 'max';
+                var $container = (type === 'min' ? $(this).prev() : $(this).prev().prev());
+                rangeSlider.apply($container);
+            });
+        },
+        getConfig : function($container) {
+            var config = {
+                min : $container.data('min'),
+                max : $container.data('max'),
+                minVal : parseInt(this.$getMin($container).val()),
+                maxVal : parseInt(this.$getMax($container).val())
+            };
+            config = this.tweakMinMax(config);
+            config.minPerc = ((config.minVal - config.min) / (config.max - config.min)) * 100;
+            config.maxPerc = ((config.maxVal - config.min) / (config.max - config.min)) * 100;
+
+            return config;
+        },
+        tweakMinMax : function(config) {
+            var currentType = this.tmpConfig.type;
+            var twoPercent = Math.ceil(((config.max - config.min) / 100) * 2);
+            if (currentType) {
+                if (currentType === 'min' && config.minVal + twoPercent > config.maxVal) {
+                    config.maxVal = config.minVal;
+                    if (config.maxVal + twoPercent <= config.max) {
+                        config.maxVal += twoPercent;
+                    } else {
+                        config.maxVal = config.max;
+                    }
+                }
+                if (currentType === 'max' && config.maxVal - twoPercent < config.minVal) {
+                    config.minVal = config.maxVal;
+                    if (config.minVal - twoPercent >= config.min) {
+                        config.minVal -= twoPercent;
+                    } else {
+                        config.minVal = config.min;
+                    }
+                }
+            }
+            return config;
+        },
+        getValueForPerc : function(perc, round, config) {
+            if (!config) {
+                config = this.getConfig(this.tmpConfig.$el.parent());
+            }
+            var value = (((config.max - config.min) / 100) * perc) + config.min;
+            if (round) { value = Math.round(value); }
+            return value;
+        },
+        adjust : function($container, config) {
+            if (!config) {
+                config = this.getConfig($container);
+            }
+            $container.children('.min').css({ left : config.minPerc + '%' });
+            $container.children('.max').css({ left : config.maxPerc + '%' });
+            $container.children('.range').css({ left : config.minPerc + '%', width : (config.maxPerc - config.minPerc) + '%' });
+
+            this.$getMaxConnector($container).val(config.maxVal);
+            this.$getMinConnector($container).val(config.minVal);
+        },
+        tmpConfig : {
+            $el : $(),
+            type : null,
+            cursorRange : [null, null],
+            perc : null
+        },
+        getCursorPosPerc : function(currentX) {
+            var config = this.tmpConfig;
+            var perc = ((currentX - config.cursorRange[0]) / (config.cursorRange[1] - config.cursorRange[0])) * 100;
+            return perc > 100 ? 100 : (perc < 0 ? 0 : perc);
+        },
+        down : function(e) {
+            e.preventDefault();
+            var rangeSlider = app.catalog.rangeSlider;
+            rangeSlider.tmpConfig.$el = $(this);
+            rangeSlider.tmpConfig.type = rangeSlider.tmpConfig.$el.hasClass('min') ? 'min' : 'max';
+            var $container = rangeSlider.tmpConfig.$el.parent();
+            var rect = $container.get(0).getBoundingClientRect();
+            rangeSlider.tmpConfig.cursorRange = [rect.left, rect.right];
+            $(document).mousemove(rangeSlider.move).mouseup(rangeSlider.up);
+        },
+        move : function(e) {
+            var rangeSlider = app.catalog.rangeSlider;
+            var $container = rangeSlider.tmpConfig.$el.parent();
+            var perc = rangeSlider.getCursorPosPerc(e.clientX);
+            var newValue = rangeSlider.getValueForPerc(perc, true);
+            var $target = rangeSlider.tmpConfig.type === 'min' ? rangeSlider.$getMin($container) : rangeSlider.$getMax($container);
+            $target.val(newValue);
+            rangeSlider.adjust($container);
+        },
+        up : function() {
+            var rangeSlider = app.catalog.rangeSlider;
+            $(this).unbind('mousemove', rangeSlider.move)
+                .unbind('mouseup', rangeSlider.up);
+            rangeSlider.apply(rangeSlider.tmpConfig.$el.parent());
+            rangeSlider.tmpConfig = {
+                $el : $(),
+                type : null,
+                cursorRange : [null, null],
+                perc : null
+            };
+        },
+        apply : function($container) {
+            var config = this.getConfig($container);
+            this.$getMin($container).val(config.minVal);
+            this.$getMax($container).val(config.maxVal);
+            if ($container.siblings('.resultUrl').length) {
+                document.location.href = $container.siblings('.resultUrl').val().replace('XX-YY', config.minVal + '-' + config.maxVal);
+            }
+        },
+        $getMin : function($container) {
+            return $container.prev().prev();
+        },
+        $getMax : function($container) {
+            return $container.prev();
+        },
+        $getMinConnector : function($container) {
+            return $container.siblings($container.prev().prev().data('connector'))
+        },
+        $getMaxConnector : function($container) {
+            return $container.siblings($container.prev().data('connector'))
+        }
+    },
+    filters : {
+        init : function() {
+            $('.filters input[type=checkbox][data-on][data-off]').change(function() {
+                var url = $(this).is(':checked') ? $(this).data('on') : $(this).data('off');
+                document.location.href = url;
+            });
+            $('.filters option[data-on][data-off]').parent().change(function() {
+                document.location.href = $(this).children(':selected').eq(0).data('on');
+            });
+            $('.filters .toggleList li[data-on][data-off]').click(function() {
+                $(this).toggleClass('selected');
+                if ($(this).hasClass('selected')) {
+                    document.location.href = $(this).data('on');
+                } else {
+                    document.location.href = $(this).data('off');
+                }
+            });
+        }
+    },
+    options : {
+        init : function() {
+            $('.sortBy, .ipp').change(function() {
+                document.location.href = $(this).val();
+            });
+
+            $('.pagination li').click(function() {
+                document.location.href = $(this).data('page');
+            });
+        }
+    }
+};
+
+app.additionals.push('catalog');
---
+++ static/public/Yves/scripts/app/main.js
@@ -0,0 +1,292 @@
+var app = {
+    vars : {},
+    init : function() {
+        this.slider.init();
+        this.viewport.init();
+        this.initAlerts();
+        for (var i in this.additionals) {
+            if (this[this.additionals[i]]) {
+                this[this.additionals[i]].init();
+            }
+        }
+        $(document).keyup(function(e) {
+            if (e.keyCode !== 160) {
+                return;
+            }
+            $('body').toggleClass('showHardcoded');
+        });
+        this.tooltip.init();
+        this.search.init();
+    },
+    initAlerts: function() {
+        $('*[data-dismiss="alert"]').on('click', function(e) {
+            var parent = $(e.target).parent();
+            parent.slideUp("normal", function() { $(this).remove(); } );
+        });
+    },
+    additionals : [],
+    viewport : {
+        init : function() {
+            var $window = $(window);
+            $window.resize(this.adapt.bind(this));
+            $window.resize(this.footer.bind(this));
+            $window.trigger('resize');
+        },
+        sizeRange : [605, 1150],
+        currentSize : 1150,
+        adapt : function() {
+            var currentWidth = $(window).width();
+            if (currentWidth >= this.sizeRange[1] && this.currentSize === this.sizeRange[1]) {
+                return;
+            }
+            this.currentSize = (currentWidth > this.sizeRange[1]
+                ? this.sizeRange[1]
+                : (currentWidth < this.sizeRange[0]
+                ? this.sizeRange[0]
+                : currentWidth));
+            $('section').css({ width: this.currentSize - 40 });
+            $('section.fullWidth').css({ width: this.currentSize });
+            this.plugins.bind(app)(this.currentSize);
+        },
+        footer : function() {
+            var $main = $('.consumeHeight');
+            if (!$main.length) { return; }
+
+            var paddingHeight = $main.outerHeight(false) - $main.height();
+
+            var consumedHeight = 0;
+            $main.siblings(':visible').each(function() {
+                consumedHeight += $(this).outerHeight(true);
+            });
+
+            $main.css({ minHeight : ($(window).height() - consumedHeight) - paddingHeight});
+        },
+        plugins : function(newSize) {
+            this.slider.adaptWidth(newSize - 40);
+        }
+    },
+    slider : {
+        init : function() {
+            this.$slider = $('.slideshow ul');
+            if (!this.$slider.length) { return; }
+            this.max = this.$slider.children('li').length - 1;
+            this.index = 0;
+            this.adaptWidth(app.viewport.currentSize);
+            setTimeout(this.slide.bind(this), 6000);
+        },
+        adaptWidth : function(newSize) {
+            this.slideWidth = newSize;
+            this.$slider.css({ width : (this.max + 1) * newSize, marginLeft : this.index * newSize * (-1) })
+                .children('li').css({ width : newSize });
+        },
+        slide : function() {
+            this.index = this.index < this.max ? this.index + 1 : 0;
+            this.$slider.animate({ marginLeft : this.index * this.slideWidth * (-1) }, 1000);
+            setTimeout(this.slide.bind(this), 6000);
+        }
+    },
+    settings : {
+        get : function(property) {
+            if (typeof localStorage === 'undefined') {
+                return {};
+            }
+            var settings = localStorage.getItem('settings');
+            if (!settings) {
+                localStorage.setItem('settings', '{}');
+                return this.get();
+            }
+
+            settings = $.parseJSON(settings);
+            if (property) {
+                return settings[property];
+            }
+            return settings;
+        },
+        set : function(property, value) {
+            if (typeof localStorage === 'undefined') {
+                return {};
+            }
+            var settings = this.get();
+            settings[property] = value;
+            localStorage.setItem('settings', JSON.stringify(settings));
+            return settings;
+        }
+    },
+    tooltip : {
+        init : function() {
+            $('[data-hover-tooltip]').mouseenter(function() {
+                if ($(this).next('.tooltip').length) { return; }
+                app.tooltip.attachTo($(this), 'info', $(this).data('hover-tooltip'), true, true);
+                $(this).mouseleave(function() {
+                    $(this).next('.tooltip').mouseleave();
+                });
+            });
+        },
+        attachTo : function($target, type, message, detachOnHover, disableDetachAnimation) {
+            var $tooltip = this.$generate(type, message);
+            $target.after($tooltip.css({
+                left : $target.position().left + ($target.outerWidth(true) / 2),
+                top : $target.position().top + $target.outerHeight(true) + 6
+            }));
+
+            var halfWidth = $tooltip.outerWidth(false) / 2;
+            $tooltip.css({ marginLeft : halfWidth * (-1) });
+
+            if (detachOnHover) {
+                $tooltip.mouseleave(function() {
+                    if (!disableDetachAnimation) {
+                        $(this).fadeOut(500, function() {
+                            $(this).remove();
+                        });
+                    } else {
+                        $(this).remove();
+                    }
+                });
+            }
+
+            return $tooltip;
+        },
+        $generate : function(type, message) {
+            return $('<div class="tooltip"></div>').addClass(type).html(message);
+        }
+    },
+    ensureVisibility : function($target) {
+        var elementOffset = $target.offset().top;
+        var windowScrollTop = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var isVisible = (windowScrollTop + windowHeight > elementOffset + 30 && elementOffset - 30 > $(window).scrollTop());
+        if (!isVisible) {
+            $('html, body').animate({ scrollTop : Math.round(elementOffset - (windowHeight / 2)) }, 500);
+        }
+        return isVisible;
+    },
+    validation : {
+        apply : function ($container, childrenOnly) {
+            var result = this.check($container, childrenOnly);
+            $container[childrenOnly ? 'children' : 'find'](':input[name]')
+                .removeClass('validationError');
+            this.getInvalidItems(result)
+                .addClass('validationError');
+            var $firstInvalid = this.getInvalidItems(result, true).eq(0);
+            if ($firstInvalid.length) {
+                var firstInvalidResult = result[$firstInvalid.attr('name')].check;
+                var message = 'validation-message-' + firstInvalidResult;
+                var $msgEl = $firstInvalid.parent('[data-validation-message]');
+                if ($firstInvalid.data('validation-message')) {
+                    message = $firstInvalid.data('validation-message');
+                } else {
+                    if ($msgEl.length) {
+                        message = $msgEl.data('validationMessage');
+                    } else {
+                        message = $('#validationMessages').data(message);
+                    }
+                }
+                if (firstInvalidResult === 'pattern' && $firstInvalid.data('validation-pattern-message')) {
+                    message = $firstInvalid.data('validation-pattern-message');
+                }
+                $('.tooltip').remove();
+                app.ensureVisibility(app.tooltip.attachTo($msgEl.length ? $msgEl : $firstInvalid, 'error', message, true));
+            }
+            return result;
+        },
+        resultIsValid : function (result) {
+            var valid = true;
+            $.each(result, function (index, el) {
+                if (!el.valid) {
+                    valid = false;
+                }
+            });
+            return valid;
+        },
+        getInvalidItems : function (result, firstOnly) {
+            var names = [];
+            $.each(result, function (index, el) {
+                if (!el.valid && !(firstOnly && names.length)) {
+                    names.push(index);
+                }
+            });
+            var selector =
+                names
+                    .map(function (item) {
+                        return '[name="' + item + '"]';
+                    })
+                    .join(', ');
+            return $(selector);
+        },
+        check : function ($container, childrenOnly) {
+            var result = {};
+            var formValues = {};
+            var serializedForm =
+                $container
+                    .closest('form')
+                    .serializeArray();
+
+            $.each(serializedForm, function (index, el) {
+                formValues[ el.name ] = el.value;
+            });
+            $.each($container.find('[data-validator-precondition]'), function (index, el) {
+                var $el = $(el);
+
+                var preconditionFunction = new Function($el.data('validatorPrecondition'));
+
+                if (!preconditionFunction()) {
+                    $el.attr('data-validator-prevent', true);
+                } else {
+                    $el.removeAttr('data-validator-prevent');
+                }
+            });
+            var $notPreventedInputs =
+                $container[childrenOnly ? 'children' : 'find'](':input[name]')
+                    .not('[data-validator-prevent] :input');
+            $notPreventedInputs.each(function (index, el) {
+                var $el = $(el);
+                var currentName = $el.attr('name');
+                result[ currentName ] = {
+                    valid : true,
+                    check : 'none'
+                };
+                if ($el.is('[required]')) {
+                    result[ currentName ] = {
+                        valid : !!formValues[currentName],
+                        check : 'required'
+                    };
+                }
+                if (!result[currentName].valid) {
+                    // continue
+                    return;
+                }
+                if ($el.is('[type=email]')) {
+                    result[currentName] = {
+                        valid : /[A-Z0-9._%a-z\-\+]+?@(?:[A-Z0-9a-z\-]+\.)+?[A-Za-z]{2,4}/.test(formValues[currentName]),
+                        check : 'email'
+                    };
+                }
+                if (!result[currentName].valid) {
+                    // continue
+                    return;
+                }
+                if ($el.is('[pattern]')) {
+                    result[currentName] = {
+                        valid : RegExp($el.attr('pattern')).test(formValues[currentName]),
+                        check : 'pattern'
+                    };
+                }
+                // implement more checks here if necessary
+            });
+
+            return result;
+        }
+    },
+    search : {
+    init : function() {
+        $('.search-button').on('click', function() {
+            var $form = $('.search-form');
+            if (app.validation.resultIsValid(app.validation.apply($form))) {
+               $form.submit();
+            }
+        })
+    }
+}
+};
+
+$(app.init.bind(app));
