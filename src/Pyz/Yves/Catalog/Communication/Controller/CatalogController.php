1f1d17896e99ae54b4a0accd4507b77a5ee2acb1
tobias.rosenstock <tobias.rosenstock@project-a.com>
2015-04-21 12:34:24 +0200

    replaced usage of locator and dependency-container class-variables with method-calls in controller

--- src/Pyz/Yves/Catalog/Communication/Controller/CatalogController.php
+++ src/Pyz/Yves/Catalog/Communication/Controller/CatalogController.php
@@ -18,8 +18,8 @@ class CatalogController extends AbstractController
      */
     public function indexAction(array $categoryNode, Request $request)
     {
-        $search = $this->locator->catalog()->sdk()->createFacetSearch($request, $categoryNode);
-        $categoryTree = $this->locator->categoryExporter()->sdk()->getTreeFromCategoryNode($categoryNode, $this->getLocale());
+        $search = $this->getLocator()->catalog()->sdk()->createFacetSearch($request, $categoryNode);
+        $categoryTree = $this->getLocator()->categoryExporter()->sdk()->getTreeFromCategoryNode($categoryNode, $this->getLocale());
 
         //TODO check if this should be renamed to categoryNode
         return array_merge($search->getResult(), ['category' => $categoryNode, 'categoryTree' => $categoryTree]);
@@ -32,7 +32,7 @@ class CatalogController extends AbstractController
      */
     public function fulltextSearchAction(Request $request)
     {
-        $search = $this->locator->catalog()->sdk()->createFulltextSearch($request);
+        $search = $this->getLocator()->catalog()->sdk()->createFulltextSearch($request);
 
         return array_merge($search->getResult(), ['searchString' => $request->get('q')]);
     }
