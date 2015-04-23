d3492e09038b101b92f5e5f9a4d130db68238468
René Klatt <rene.klatt@project-a.com>
2015-04-23 13:13:51 +0000

    Fixed index.php from zed (assets)

--- static/assets/Zed/index.php
+++ static/assets/Zed/index.php
@@ -1,22 +1,17 @@
 <?php
 
-use Pyz\Zed\Application\Communication\ZedBootstrap;
 use SprykerFeature\Shared\Library\Application\Environment;
+use SprykerFeature\Shared\Library\Application\TestEnvironment;
+use Pyz\Zed\Application\Communication\ZedBootstrap;
 
 define('ZED_START', microtime(true));
 define('IS_CLI', false);
 
 define('APPLICATION', 'ZED');
 defined('APPLICATION_ROOT_DIR') or define('APPLICATION_ROOT_DIR', realpath(__DIR__ . '/../../..'));
-require_once(APPLICATION_ROOT_DIR . '/vendor/spryker/zed-package/src/SprykerFeature/Shared/Library/Application/Environment.php');
+require_once(APPLICATION_ROOT_DIR . '/vendor/spryker/library/src/SprykerFeature/Shared/Library/Application/Environment.php');
 
 Environment::initialize('Zed');
-
-SprykerFeature_Shared_Library_Context::setDefaultContext(\SprykerFeature_Shared_Library_Context::CONTEXT_ZED);
-
-if (file_exists(APPLICATION_SOURCE_DIR . '/Generated/Zed/DependencyInjectionContainer.php')) {
-    require_once(APPLICATION_SOURCE_DIR . '/Generated/Zed/DependencyInjectionContainer.php');
-}
 
 $bootstrap = new ZedBootstrap();
 $bootstrap
