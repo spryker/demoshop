1c3259f153dacff8bef55a705f42c37a6cbf5963
René Klatt <rene.klatt@project-a.com>
2015-04-23 13:15:32 +0000

    update index.php for yves (assets)

--- static/assets/Yves/index.php
+++ static/assets/Yves/index.php
@@ -1,19 +1,17 @@
 <?php
 /* YVES Front Controller */
 
-
-use Pyz\Yves\Application\Communication\YvesBootstrap;
 use SprykerFeature\Shared\Library\Application\Environment;
+use Pyz\Yves\Application\Communication\YvesBootstrap;
 
 define('YVES_START', microtime(true));
 
 define('APPLICATION', 'YVES');
 defined('APPLICATION_ROOT_DIR') or define('APPLICATION_ROOT_DIR', realpath(__DIR__ . '/../../..'));
 
-require_once(APPLICATION_ROOT_DIR . '/vendor/spryker/zed-package/src/SprykerFeature/Shared/Library/Application/Environment.php');
+require_once(APPLICATION_ROOT_DIR . '/vendor/spryker/library/src/SprykerFeature/Shared/Library/Application/Environment.php');
 
 Environment::initialize('Yves');
-\SprykerFeature_Shared_Library_Context::setDefaultContext(\SprykerFeature_Shared_Library_Context::CONTEXT_YVES);
 
 $bootstrap = new YvesBootstrap();
 $bootstrap
