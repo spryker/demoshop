db4c41926a4371b12a431a8ad793c348b51bfe44
René Klatt <rene.klatt@project-a.com>
2015-04-29 09:52:20 +0000

    Update refactor script

--- refactoring/TransferAccess.php
+++ refactoring/TransferAccess.php
@@ -8,11 +8,26 @@ use Symfony\Component\Finder\SplFileInfo;
 class TransferAccess extends AbstractRefactorer
 {
 
+    const REGEX = '/\$this->(?:locator|getLocator\(\))->(.*?)\(\)->transfer(.*?)\(\)/';
+
     public function refactor()
     {
         $finder = $this->getFinder();
 
         foreach ($finder as $file) {
+            $content = $file->getContents();
+            if (preg_match(self::REGEX, $content, $matches)) {
+                $bundle = ucfirst($matches[1]);
+                $type = ucfirst($matches[2]);
+                if (strstr($type, 'Collection') !== false) {
+                    continue;
+                } else {
+                    $replaceWith = 'new \\Generated\\Shared\\Transfer\\' . $bundle . $type . 'Transfer()';
+                    $content = preg_replace(self::REGEX, $replaceWith, $content);
+                    file_put_contents($file->getPathname(), $content);
+//                    echo '<pre>' . PHP_EOL . var_dump($file) . PHP_EOL . 'Line: ' . __LINE__ . PHP_EOL . 'File: ' . __FILE__ . die();
+                }
+            }
 
         }
 
@@ -25,9 +40,10 @@ class TransferAccess extends AbstractRefactorer
     {
         $finder = new Finder();
         $finder->files()
-            ->in(__DIR__ . '/../src/Pyz/Zed/')
-            ->in(__DIR__ . '/../vendor/spryker/*/src/*/Zed/')
-            ->exclude()
+            ->in(__DIR__ . '/../src/Pyz/')
+            ->in(__DIR__ . '/../tests/')
+            ->in(__DIR__ . '/../vendor/spryker/*/src/*/')
+            ->in(__DIR__ . '/../vendor/spryker/*/tests/*/')
             ->name('*.php')
         ;
 
