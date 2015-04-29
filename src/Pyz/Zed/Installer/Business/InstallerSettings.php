7f056a9647c54e433bd2bce5e8e196ed89237604
tobias.rosenstock <tobias.rosenstock@project-a.com>
2015-04-29 18:25:09 +0200

    added search page bundle

---
+++ src/Pyz/Zed/Installer/Business/InstallerSettings.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Pyz\Zed\Installer\Business;
+
+use SprykerFeature\Zed\Installer\Business\InstallerSettings as SprykerInstallerSettings;
+use SprykerFeature\Zed\Installer\Business\Model\AbstractInstaller;
+
+class InstallerSettings extends SprykerInstallerSettings
+{
+
+    /**
+     * @return AbstractInstaller[]
+     */
+    public function getInstallerStack()
+    {
+        return [
+            $this->locator->frontendExporter()->pluginInstaller(),
+            $this->locator->product()->pluginInstaller(),
+            $this->locator->productSearch()->pluginInstaller(),
+            $this->locator->price()->pluginInstaller(),
+            $this->locator->locale()->pluginInstaller(),
+            $this->locator->country()->pluginInstaller(),
+            $this->locator->user()->pluginInstaller(),
+            $this->locator->acl()->pluginInstaller(),
+
+        ];
+    }
+
+    /**
+     * @return AbstractInstaller[]
+     */
+    public function getDemoDataInstallerStack()
+    {
+        return [
+            $this->locator->category()->pluginDemoDataInstaller(),
+            $this->locator->glossary()->pluginDemoDataInstaller(),
+            $this->locator->product()->pluginDemoDataInstaller(),
+            $this->locator->productCategory()->pluginDemoDataInstaller(),
+            $this->locator->price()->pluginDemoDataInstaller(),
+            $this->locator->productSearch()->pluginDemoDataInstaller(),
+            $this->locator->stock()->pluginDemoDataInstaller(),
+        ];
+    }
+}
