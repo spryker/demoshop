3f4e45cfbcbfe685cf202cc2a05756d68e548e2b
Philipp Wördehoff <philipp.woerdehoff@project-a.com>
2015-05-04 14:58:41 +0200

    submit register / login form via ajax

--- static/assets/Yves/scripts/ui/login-form.js
+++ static/assets/Yves/scripts/ui/login-form.js
@@ -4,30 +4,70 @@ var $ = require('jquery'),
     overlay = require('./overlay'),
     mode = 'login';
 
-var showLoginForm = function() {
-  $('.overlay').addClass('overlay--login');
-  overlay.show();
+var showLoginForm = function () {
+    $('.overlay').addClass('overlay--login');
+    overlay.show();
 };
 
-var toggleMode = function(e) {
-  e.preventDefault();
-  if (mode === 'login') {
-    $('.js-login-form').addClass('login-form--register');
-    $('.js-login-form-switch').html('Ich habe bereits ein Konto');
-    $('.js-login-submit').val('Registrieren');
-    mode = 'register';
-  } else {
-    $('.login-form').removeClass('login-form--register');
-    $('.js-login-form-switch').html('Ich bin Neukunde');
-    $('.js-login-submit').val('Anmelden');
-    mode = 'login';
-  }
+var toggleMode = function (e) {
+    e.preventDefault();
+
+    if (mode === 'login') {
+        setRegisterUrl();
+        $('.js-login-form').addClass('login-form--register');
+        $('.js-login-form-switch').html('Ich habe bereits ein Konto');
+        $('.js-login-submit').val('Registrieren');
+        mode = 'register';
+    } else {
+        setLoginUrl();
+        $('.login-form').removeClass('login-form--register');
+        $('.js-login-form-switch').html('Ich bin Neukunde');
+        $('.js-login-submit').val('Anmelden');
+        mode = 'login';
+    }
 };
+
+var setLoginUrl = function() {
+    var $loginForm = $('.js-login-form');
+    $loginForm.attr('action', $loginForm.attr('data-login-url'));
+}
+
+var setRegisterUrl = function() {
+    var $loginForm = $('.js-login-form');
+    $loginForm.attr('action', $loginForm.attr('data-register-url'));
+}
+
+var postForm = function (e) {
+    e.preventDefault();
+
+    var $form = $(e.target);
+    var actionUrl = $form.attr('action');
+    var formData = $form.serialize();
+
+    console.log('Action');
+    console.log(actionUrl);
+    console.log('FormData');
+    console.log(formData);
+
+    $.ajax({
+        type: "POST",
+        url: actionUrl,
+        data: formData
+    }).done(function(){
+        console.log('ho');
+    }).error(function(error){
+        console.log(error);
+    });
+}
 
 module.exports = {
 
-  init: function() {
-    $('.js-show-login-button').click(showLoginForm);
-    $('.js-login-form-switch').click(toggleMode);
-  }
+    init: function () {
+        setLoginUrl();
+
+        $('.js-show-login-button').click(showLoginForm);
+        $('.js-login-form-switch').click(toggleMode);
+
+        $('.js-login-form').submit(postForm);
+    }
 };
