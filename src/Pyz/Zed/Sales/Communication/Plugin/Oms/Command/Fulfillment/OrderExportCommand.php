53698c4f621f1953600ccb80c925e3c63cad1edc
René Klatt <rene.klatt@project-a.com>
2015-05-06 15:44:38 +0000

    fixed some bugs add transferDefinition file rename scipt

--- src/Pyz/Zed/Sales/Communication/Plugin/Oms/Command/Fulfillment/OrderExportCommand.php
+++ src/Pyz/Zed/Sales/Communication/Plugin/Oms/Command/Fulfillment/OrderExportCommand.php
@@ -2,25 +2,21 @@
 
 namespace Pyz\Zed\Sales\Communication\Plugin\Oms\Command\Fulfillment;
 
-use SprykerFeature\Zed\Oms\Business\Model\Util\ReadOnlyArrayObject;
+use SprykerFeature\Zed\Oms\Business\Util\ReadOnlyArrayObject;
 use SprykerFeature\Zed\Oms\Communication\Plugin\Oms\Command\AbstractCommand;
 use SprykerFeature\Zed\Oms\Communication\Plugin\Oms\Command\CommandByOrderInterface;
+use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder;
 use Zend\Stdlib\ArrayObject;
 
-/**
- * Class OrderExportCommand
- * @package Pyz\Zed\Sales\Business\Model\Orderprocess\Command\Fulfillment
- */
 class OrderExportCommand extends AbstractCommand implements CommandByOrderInterface
 {
 
     /**
      * @param array $orderItems
-     * @param \SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder $orderEntity
+     * @param SpySalesOrder $orderEntity
      * @param ReadOnlyArrayObject $data
-     * @return array|void
      */
-    public function run(array $orderItems, \SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder $orderEntity, ReadOnlyArrayObject $data)
+    public function run(array $orderItems, SpySalesOrder $orderEntity, ReadOnlyArrayObject $data)
     {
         // TODO: needs implementation
     }
