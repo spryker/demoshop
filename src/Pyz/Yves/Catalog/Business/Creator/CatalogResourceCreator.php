2af1970cf2da40f72b706b9217c4ebb415a57161
Lucas Manzke <lucas.manzke@spryker.com>
2015-05-11 17:48:43 +0200

    use right resource creator interface and constant

--- src/Pyz/Yves/Catalog/Business/Creator/CatalogResourceCreator.php
+++ src/Pyz/Yves/Catalog/Business/Creator/CatalogResourceCreator.php
@@ -3,7 +3,8 @@
 namespace Pyz\Yves\Catalog\Business\Creator;
 
 use SprykerFeature\Shared\Application\Communication\ControllerServiceBuilder;
-use SprykerFeature\Yves\FrontendExporter\Business\Creator\ResourceCreatorInterface;
+use SprykerFeature\Shared\Category\CategoryResourceSettings;
+use SprykerFeature\Yves\FrontendExporter\Creator\ResourceCreatorInterface;
 use Pyz\Sdk\Catalog\Model\FacetConfig;
 use Silex\Application;
 use SprykerEngine\Yves\Kernel\Communication\BundleControllerAction;
@@ -31,7 +32,7 @@ class CategoryResourceCreator implements ResourceCreatorInterface
      */
     public function getType()
     {
-        return 'category';
+        return CategoryResourceSettings::RESOURCE_TYPE_CATEGORY_NODE;
     }
 
     /**
