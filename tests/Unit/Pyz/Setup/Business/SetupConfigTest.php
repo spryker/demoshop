e5e3b5abdf38d6b7f57aa08f320bd10137a2a7dc
René Klatt <rene.klatt@project-a.com>
2015-05-29 13:57:48 +0000

    Removed bundles.php Add SetupConfig

---
+++ tests/Unit/Pyz/Setup/Business/SetupConfigTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Unit\Pyz\Setup\Business;
+
+use Pyz\Zed\Setup\Business\SetupConfig;
+use SprykerEngine\Shared\Config;
+use SprykerEngine\Zed\Kernel\Locator;
+
+/**
+ * @group Pyz
+ * @group Zed
+ * @group Setup
+ * @group Business
+ * @group SetupConfig
+ */
+class SetupConfigTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @return SetupConfig
+     */
+    private function getConfig()
+    {
+        return new SetupConfig(Config::getInstance(), Locator::getInstance());
+    }
+
+    public function testGetPropelSchemaPathPatterShouldReturnArrayWithOnePatternToSchemaDirectories()
+    {
+        $pathPatterns = $this->getConfig()->getPropelSchemaPathPattern();
+        $this->assertTrue(is_array($pathPatterns));
+        $this->assertCount(1, $pathPatterns);
+    }
+
+
+}
