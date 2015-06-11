a9041046b1845c0d3e86cfccfa04d3f3860293c8
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-06-11 12:16:00 +0200

    Fixed glossary facade error

--- src/Pyz/Zed/Glossary/Communication/Plugin/YamlInstallerPlugin.php
+++ src/Pyz/Zed/Glossary/Communication/Plugin/YamlInstallerPlugin.php
@@ -46,7 +46,7 @@ class YamlInstallerPlugin extends AbstractPlugin implements GlossaryInstallerPlu
      */
     protected function installKeysAndTranslations(array $translations)
     {
-        $glossaryFacade = $this->getDependencyContainer()->getFacade();
+        $glossaryFacade = $this->getDependencyContainer()->createGlossaryFacade();
         foreach ($translations['keys'] as $keyName => $data) {
             if (!$glossaryFacade->hasKey($keyName)) {
                 $glossaryFacade->createKey($keyName);
