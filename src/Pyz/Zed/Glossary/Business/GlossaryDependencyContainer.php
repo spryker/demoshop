b6721aaec85df4d00ed41ac2af38b8dae1e3403f
René Klatt <rene.klatt@project-a.com>
2015-06-11 06:24:36 +0000

    Renamed getExternalDependency to getInjectedDependency

--- src/Pyz/Zed/Glossary/Business/GlossaryDependencyContainer.php
+++ src/Pyz/Zed/Glossary/Business/GlossaryDependencyContainer.php
@@ -17,7 +17,7 @@ class GlossaryDependencyContainer extends SprykerGlossaryDependencyContainer
     public function createDemoDataInstaller(LoggerInterface $messenger)
     {
         $installers = [
-            $this->getExternalDependency(GlossaryDependencyProvider::PLUGIN_YML_INSTALLER)
+            $this->getDependency(GlossaryDependencyProvider::PLUGIN_YML_INSTALLER)
         ];
         $installer = $this->getFactory()->createInternalDemoDataGlossaryInstall($installers);
         $installer->setMessenger($messenger);
