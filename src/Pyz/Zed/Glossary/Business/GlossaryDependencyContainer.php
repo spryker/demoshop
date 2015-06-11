4088825673943fb1c3f368b783b9c4193ed522f6
René Klatt <rene.klatt@project-a.com>
2015-06-11 08:46:43 +0000

    Fixed remaming

--- src/Pyz/Zed/Glossary/Business/GlossaryDependencyContainer.php
+++ src/Pyz/Zed/Glossary/Business/GlossaryDependencyContainer.php
@@ -17,7 +17,7 @@ class GlossaryDependencyContainer extends SprykerGlossaryDependencyContainer
     public function createDemoDataInstaller(LoggerInterface $messenger)
     {
         $installers = [
-            $this->getDependency(GlossaryDependencyProvider::PLUGIN_YML_INSTALLER)
+            $this->getProvidedDependency(GlossaryDependencyProvider::PLUGIN_YML_INSTALLER)
         ];
         $installer = $this->getFactory()->createInternalDemoDataGlossaryInstall($installers);
         $installer->setMessenger($messenger);
