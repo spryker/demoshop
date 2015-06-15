77b9b14469daa11c545cb0c42b113f2197365e69
Freddie Frantzen <freddie@freddiefrantzen.com>
2015-06-15 14:14:19 +0200

    Fixed test

--- tests/Functional/Pyz/Zed/ProductOption/Business/Internal/DemoData/Importer/ProductOptionDataInstallTest.php
+++ tests/Functional/Pyz/Zed/ProductOption/Business/Internal/DemoData/Importer/ProductOptionDataInstallTest.php
@@ -4,6 +4,10 @@ namespace Functional\Pyz\Zed\ProductOption\Business\Internal\DemoData\Importer;
 
 use Pyz\Zed\ProductOption\Business\ProductOptionFacade;
 use SprykerEngine\Zed\Kernel\AbstractFunctionalTest;
+use SprykerFeature\Zed\Product\Persistence\Propel\SpyProduct;
+use SprykerFeature\Zed\Product\Persistence\Propel\SpyAbstractProduct;
+use SprykerFeature\Zed\Product\Persistence\Propel\SpyProductQuery;
+use SprykerFeature\Zed\Product\Persistence\Propel\SpyAbstractProductQuery;
 
 /**
  * @group Pyz
@@ -22,7 +26,38 @@ class ProductOptionInstallerTest extends AbstractFunctionalTest
             ->disableOriginalConstructor()
             ->getMock();
 
+        $this->ensureProductFixturesExistInDb();
+
         $this->getFacade()->installDemoData($stub);
     }
 
+    private function ensureProductFixturesExistInDb()
+    {
+        $this->loadAbstractProductsIfNotInDb(['136823', '137455']);
+        $this->loadConcreteProductsIfNotInDb(['136823', '137288', '137455']);
+    }
+
+    private function loadAbstractProductsIfNotInDb(array $skus)
+    {
+        foreach ($skus as $sku) {
+            $result = SpyAbstractProductQuery::create()->filterBySku($sku)->findOne();
+            if (null === $result) {
+                $abstractProduct = (new SpyAbstractProduct())
+                    ->setSku($sku);
+                $abstractProduct->save();
+            }
+        }
+    }
+
+    private function loadConcreteProductsIfNotInDb(array $skus)
+    {
+        foreach ($skus as $sku) {
+            $result = SpyProductQuery::create()->filterBySku($sku)->findOne();
+            if (null === $result) {
+                $concreteProduct = (new SpyProduct())
+                    ->setSku($sku);
+                $concreteProduct->save();
+            }
+        }
+    }
 }
