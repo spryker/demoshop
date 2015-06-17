fe9eaf6efb982b6eb9ba874940a0fbc13fe674c5
René Klatt <rene.klatt@project-a.com>
2015-06-17 06:50:26 +0000

    update refactoring script

--- sdkRenaming.php
+++ sdkRenaming.php
@@ -55,7 +55,7 @@ $renameSdkToClient = function (Finder $files) {
     $filesystem = new Filesystem();
     /** @var SplFileInfo $file */
     foreach ($files as $file) {
-        $content = str_replace(['\\Sdk\\', '/Sdk/', '/sdk/'], ['\\Client\\', '/Client/', '/client/'], $file->getContents());
+        $content = str_replace(['\\Sdk\\', '/Sdk/', '/sdk/', '->sdk()->'], ['\\Client\\', '/Client/', '/client/', '->client()->'], $file->getContents());
         file_put_contents($file->getPathname(), $content);
     }
 };
