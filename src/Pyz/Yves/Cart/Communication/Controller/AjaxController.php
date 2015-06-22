9da28aee6367dea853623cd0d28b3469f9aa2a33
Marco Podien <marco@lazystar.de>
2015-06-22 15:18:37 +0200

    getProductsForCartItems() is part of the sdk controller for now

--- src/Pyz/Yves/Cart/Communication/Controller/AjaxController.php
+++ src/Pyz/Yves/Cart/Communication/Controller/AjaxController.php
@@ -46,7 +46,7 @@ class AjaxController extends AbstractController
 
         return $this->viewResponse([
             'cart' => $cart,
-            'products' => $this->getLocator()->cart()->sdk()->getProductsForCartItems($cart->getItems()),
+            'products' => $this->getProductsForCartItems($cart->getItems()),
         ]);
     }
 
@@ -93,6 +93,52 @@ class AjaxController extends AbstractController
         $this->getCart()->decreaseItemQuantity($sku);
 
         return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
+    }
+
+    /**
+     * @param \ArrayObject $cartItems
+     * @return array
+     */
+    public function getProductsForCartItems(\ArrayObject $cartItems)
+    {
+        if (count($cartItems === 0)) {
+            return [];
+        }
+
+        $products = [];
+        /** @var CartItemTransfer $item */
+        foreach ($cartItems as $item) {
+            $product = [
+                'name' => '',
+                'price' => 0,
+                'quantity' => $item->getQuantity(),
+            ];
+
+            $abstractProduct = $this->getLocator()->catalog()->sdk()->createCatalogModel()->getProductDataById($item->getId());
+            if (isset($abstractProduct['abstract_Attributes']) && isset($abstractProduct['abstract_attributes']['thumbnail_url'])) {
+                $product['thumbnail'] = $abstractProduct['abstract_attributes']['thumbnail_url'];
+            }
+
+            if (isset($abstractProduct['concrete_products'])) {
+                foreach ($abstractProduct['concrete_products'] as $concreteProduct) {
+                    if (isset($concreteProduct['sku']) && $concreteProduct['sku'] == $item->getSku()) {
+                        if (isset($concreteProduct['name'])) {
+                            //@todo fall back to abstract name?
+                            $product['name'] = $concreteProduct['name'];
+                        }
+                    }
+                }
+            }
+
+            //@todo price from item?
+            if (isset($abstractProduct['valid_price'])) {
+                $product['price'] =  $abstractProduct['valid_price'];
+            }
+
+            $products[] = $product;
+        }
+
+        return $products;
     }
 
 }
