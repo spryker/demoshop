b556f2cb86073b93ac80e832b453070eddc509e7
Marco Podien <marco@lazystar.de>
2015-06-22 10:50:24 +0200

    Changed template in header.twig

--- src/Pyz/Yves/Cart/Communication/Controller/AjaxController.php
+++ src/Pyz/Yves/Cart/Communication/Controller/AjaxController.php
@@ -2,32 +2,97 @@
 namespace Pyz\Yves\Cart\Communication\Controller;
 
 use Generated\Shared\Transfer\CartItemTransfer;
+use Generated\Shared\Cart\CartInterface as GeneratedCartInterface;
 use SprykerEngine\Shared\Kernel\LocatorLocatorInterface;
-use Pyz\Yves\Cart\Communication\Helper\CartControllerTrait;
-use SprykerFeature\Yves\Library\Communication\MessageParser;
+use SprykerEngine\Yves\Application\Business\Application;
+use SprykerEngine\Yves\Kernel\Communication\Factory;
 use SprykerEngine\Yves\Application\Communication\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use SprykerFeature\Sdk\Cart\Model\CartInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class AjaxController extends AbstractController
 {
-    use CartControllerTrait;
+    /**
+     * @param Application $app
+     * @param Factory $factory
+     * @param LocatorLocatorInterface $locator
+     */
+    public function __construct(Application $app, Factory $factory, LocatorLocatorInterface $locator)
+    {
+        parent::__construct($app, $factory, $locator);
+        $this->cart = $this
+            ->getLocator()
+            ->cart()
+            ->pluginCartService()
+            ->createCartServiceProvider($app['session'])
+        ;
+    }
 
     /**
-     * @param CartItemTransfer $cartItem
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     * @return CartInterface
      */
-    public function addAction(CartItemTransfer $cartItem, Request $request)
+    protected function getCart()
     {
-        $transferResponse = $this->getCart($request)->addItem($cartItem);
-        $parser = new MessageParser($this->getTranslator());
+        return $this->cart;
+    }
 
-        return $this->jsonResponse([
-            'success' => $transferResponse->isSuccess(),
-            'messages' => $parser->parseAndTranslateMessages($transferResponse->getMessages()),
-            'errorMessages' => $parser->parseAndTranslateMessages($transferResponse->getErrorMessages()),
-            'count' => $this->getCart($request)->getCount()
+    /**
+     * @return array
+     */
+    public function indexAction()
+    {
+        /** @var GeneratedCartInterface $cart */
+        $cart = $this->getCart()->getCart();
+
+        return $this->viewResponse([
+            'cart' => $cart,
+            'products' => $this->getLocator()->cart()->sdk()->getProductsForCartItems($cart->getItems()),
         ]);
+    }
+
+    /**
+     * @param string $sku
+     * @param int $quantity
+     * @return RedirectResponse
+     */
+    public function addAction($sku, $quantity)
+    {
+        $this->getCart()->addToCart($sku, $quantity);
+
+        return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
+    }
+
+    /**
+     * @param string $sku
+     * @return RedirectResponse
+     */
+    public function removeAction($sku)
+    {
+        $this->getCart()->removeFromCart($sku);
+
+        return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
+    }
+
+    /**
+     * @param string $sku
+     * @return RedirectResponse
+     */
+    public function increaseAction($sku)
+    {
+        $this->getCart()->increaseItemQuantity($sku);
+
+        return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
+    }
+
+    /**
+     * @param string $sku
+     * @return RedirectResponse
+     */
+    public function decreaseAction($sku)
+    {
+        $this->getCart()->decreaseItemQuantity($sku);
+
+        return $this->redirectResponseInternal(CartControllerProvider::ROUTE_CART_OVERLAY);
     }
 
 }
