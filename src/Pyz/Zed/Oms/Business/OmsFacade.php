e6b86035f082f9945ea39daae0273356cda68fba
Lucas Manzke <lucas.manzke@spryker.com>
2015-06-24 09:34:25 +0200

    DEM-10 Adapt Facades for Country/Customer/Oms

---
+++ src/Pyz/Zed/Oms/Business/OmsFacade.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pyz\Zed\Oms\Business;
+
+use SprykerFeature\Zed\Checkout\Dependency\Facade\CheckoutToOmsInterface;
+use SprykerFeature\Zed\Oms\Business\OmsFacade as SprykerOmsFacade;
+use SprykerFeature\Zed\Sales\Dependency\Facade\SalesToOmsInterface;
+
+class OmsFacade extends SprykerOmsFacade implements
+    CheckoutToOmsInterface,
+    SalesToOmsInterface
+{
+
+}
