2400cdebf563976f9b96c8f6f72ac0ef78cb6947
Lucas Manzke <lucas.manzke@spryker.com>
2015-06-24 19:12:54 +0200

    DEM-10 Add project level checkout dependency provider

---
+++ src/Pyz/Zed/Checkout/CheckoutDependencyProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Pyz\Zed\Checkout;
+
+use SprykerEngine\Zed\Kernel\Container;
+use SprykerFeature\Zed\Checkout\CheckoutDependencyProvider as SprykerCheckoutDependencyProvider;
+use SprykerFeature\Zed\Checkout\Dependency\Plugin\CheckoutOrderHydrationInterface;
+use SprykerFeature\Zed\Checkout\Dependency\Plugin\CheckoutPostSaveHookInterface;
+use SprykerFeature\Zed\Checkout\Dependency\Plugin\CheckoutPreconditionInterface;
+use SprykerFeature\Zed\Checkout\Dependency\Plugin\CheckoutSaveOrderInterface;
+
+class CheckoutDependencyProvider extends SprykerCheckoutDependencyProvider
+{
+
+    /**
+     * @param Container $container
+     *
+     * @return CheckoutPreconditionInterface[]
+     */
+    protected function getCheckoutPreconditions(Container $container)
+    {
+        return [
+            $container->getLocator()->customerCheckoutConnector()->pluginCustomerPreconditionCheckerPlugin(),
+            $container->getLocator()->availabilityCheckoutConnector()->pluginProductsAvailablePreconditionPlugin(),
+        ];
+    }
+
+    /**
+     * @param Container $container
+     *
+     * @return CheckoutOrderHydrationInterface[]
+     */
+    protected function getCheckoutOrderHydrators(Container $container)
+    {
+        return [
+            $container->getLocator()->customerCheckoutConnector()->pluginOrderCustomerHydrationPlugin(),
+            $container->getLocator()->cartCheckoutConnector()->pluginOrderCartHydrationPlugin(),
+            $container->getLocator()->omsCheckoutConnector()->pluginOrderOmsHydrationPlugin()
+        ];
+    }
+
+    /**
+     * @param Container $container
+     *
+     * @return CheckoutSaveOrderInterface[]
+     */
+    protected function getCheckoutOrderSavers(Container $container)
+    {
+        return [
+            $container->getLocator()->salesCheckoutConnector()->pluginSalesOrderSaverPlugin(),
+            $container->getLocator()->customerCheckoutConnector()->pluginOrderCustomerSavePlugin(),
+        ];
+    }
+
+    /**
+     * @param Container $container
+     *
+     * @return CheckoutPostSaveHookInterface[]
+     */
+    protected function getCheckoutPostHooks(Container $container)
+    {
+        return parent::getCheckoutPostHooks($container);
+    }
+
+}
