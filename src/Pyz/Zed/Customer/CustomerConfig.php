adabc32397c5c2c776536916c572aa12f57ba0ed
danielsveller <daniel.sveller@spryker.com>
2015-07-07 17:09:16 +0200

    moving sender plugins to the CustomerDependencyProvider

--- src/Pyz/Zed/Customer/CustomerConfig.php
+++ src/Pyz/Zed/Customer/CustomerConfig.php
@@ -1,41 +1,13 @@
 <?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
 
 namespace Pyz\Zed\Customer;
 
 use SprykerFeature\Zed\Customer\CustomerConfig as SprykerCustomerConfig;
-use SprykerFeature\Zed\Customer\CustomerDependencyProvider;
+use SprykerFeature\Zed\Customer\CustomerDependencyProvider as SprykerCustomerDependencyProvider;
 
 class CustomerConfig extends SprykerCustomerConfig
 {
-
-    /**
-     * @return array
-     */
-    public function getPasswordRestoredConfirmationSenders()
-    {
-        return [
-            CustomerDependencyProvider::PASSWORD_RESTORED_CONFIRMATION_SENDER
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    public function getPasswordRestoreTokenSenders()
-    {
-        return [
-            CustomerDependencyProvider::PASSWORD_RESTORE_TOKEN_SENDER
-        ];
-    }
-
-    /**
-     * @return array
-     */
-    public function getRegistrationTokenSenders()
-    {
-        return [
-            CustomerDependencyProvider::REGISTRATION_TOKEN_SENDER
-        ];
-    }
-
 }
--- src/Pyz/Zed/User/Business/UserFacade.php
+++ src/Pyz/Zed/User/Business/UserFacade.php
@@ -1,12 +1,14 @@
 <?php
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
 
 namespace Pyz\Zed\User\Business;
 
 use SprykerFeature\Zed\Acl\Dependency\Facade\AclToUserInterface;
 use SprykerFeature\Zed\User\Business\UserFacade as SprykerUserFacade;
 
-class UserFacade extends SprykerUserFacade implements
-    AclToUserInterface
+class UserFacade extends SprykerUserFacade implements AclToUserInterface
 {
 
 }
