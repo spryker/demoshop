e126a5ead8fe045e9c3e7912d40615fb5c1b63de
Marco Podien <marco@lazystar.de>
2015-07-09 17:45:05 +0200

    Changed indentation to 4 spaces, stripped obsolete code

--- static/assets/Yves/scripts/ui/catalog/filters.js
+++ static/assets/Yves/scripts/ui/catalog/filters.js
@@ -1,54 +1,49 @@
 'use strict';
 
-require ('jquery-ui/slider');
+require('jquery-ui/slider');
 var $ = require('jquery'),
     _ = require('underscore'),
     catalog = require('./index'),
-    URLUtils = require('../../utils/URLUtils'),
     URLManager = require('./URLManager'),
     Filter = require('./Filter');
 
 var filters = [];
 
-var updatePriceValueDisplay = function(filter) {
-  $('.filter__price').data('min', filter.min).data('max', filter.max);
+var updatePriceValueDisplay = function (filter) {
+    $('.filter__price').data('min', filter.min).data('max', filter.max);
 
-  $('.filter__price-min').html(filter.min);
-  $('.filter__price-max').html(filter.max);
+    $('.filter__price-min').html(filter.min);
+    $('.filter__price-max').html(filter.max);
 };
 
-var triggerPriceChange = function(event, ui) {
-  $(ui.handle).parents('.js-filter').trigger('change');
-}
+var initActiveFilterList = function () {
+    $('.js-filter').on('change', function (e) {
+        var changedFilterName = $(e.currentTarget).data('filter-name');
+        updateActiveFilterList(getFilter(changedFilterName));
 
-var initActiveFilterList = function() {
-  $('.js-filter').on('change', function(e) {
-    var changedFilterName = $(e.currentTarget).data('filter-name');
-    updateActiveFilterList(getFilter(changedFilterName));
+        $('.js-products-prev').remove();
+        $('.js-products-next').remove();
 
-    $('.js-products-prev').remove();
-    $('.js-products-next').remove();
+        var $filtered = $(catalog.template);
+        $filtered.addClass('js-products-current').replaceAll('.js-products-current');
+        catalog.loadProducts(
+            URLManager.getPath(),
+            URLManager.paramsToString(getUpdatedParams()),
+            $filtered,
+            updateFilters
+        );
+    });
 
-    var $filtered = $(catalog.template);
-    $filtered.addClass('js-products-current').replaceAll('.js-products-current');
-    catalog.loadProducts(
-        URLManager.getPath(),
-        URLManager.paramsToString(getUpdatedParams()),
-        $($filtered),
-        updateFilters
-    );
-  });
-
-  $('.js-filter-remove').on('click', function(e) {
-    var filterName = $(e.currentTarget).parent().data('filter-name');
-    getFilter(filterName).clear();
-  });
+    $('.js-filter-remove').on('click', function (e) {
+        var filterName = $(e.currentTarget).parent().data('filter-name');
+        getFilter(filterName).clear();
+    });
 };
 
-var updateFilters = function(data) {
+var updateFilters = function (data) {
     var params = URLManager.getParams();
 
-    _.each(filters, function(filter) {
+    _.each(filters, function (filter) {
         switch (filter.name) {
             case "price":
                 filter.max = Math.ceil(data.price.rangeValues.max / 100);
@@ -77,90 +72,92 @@ var updateFilters = function(data) {
         }
     });
 
-    $('.js-filter').each(function() {
+    $('.js-filter').each(function () {
         filters.push(new Filter($(this)));
     });
 };
 
 
 // select filters based on URL params
-var selectRequiredFilters = function() {
-  var paramName, filter;
-  var params = URLManager.getParams();
-  for (paramName in params) {
-    if (filter = getFilter(paramName)) {
-      filter.setSelectedValue(params[paramName]);
-      updateActiveFilterList(filter, true);
+var selectRequiredFilters = function () {
+    var paramName, filter;
+    var params = URLManager.getParams();
+    for (paramName in params) {
+        if (filter = getFilter(paramName)) {
+            filter.setSelectedValue(params[paramName]);
+            updateActiveFilterList(filter, true);
+        }
     }
-  }
 };
 
-var updateActiveFilterList = function(changedFilter, silent) {
-  var $activeEl = $('.active-filter[data-filter-name="'+changedFilter.name+'"');
-  var filterValue = changedFilter.getSelectedValue(true);
+var updateActiveFilterList = function (changedFilter, silent) {
+    var $activeEl = $('.active-filter[data-filter-name="' + changedFilter.name + '"');
+    var filterValue = changedFilter.getSelectedValue(true);
 
-  $activeEl.attr('data-filter-value', filterValue);
-  $activeEl.find('.js-filter-value').text(filterValue);
+    $activeEl.attr('data-filter-value', filterValue);
+    $activeEl.find('.js-filter-value').text(filterValue);
 
-  if (!silent) {
-    updateURL();
-  }
-};
-
-var getFilter = function(filterName) {
-  for (var i = 0; i < filters.length; i++) {
-    if (filters[i].name == filterName) {
-      return filters[i];
+    if (!silent) {
+        updateURL();
     }
-  };
 };
 
-var updateURL = function() {
-  var params = getUpdatedParams();
-  URLManager.setParams(params);
+var getFilter = function (filterName) {
+    for (var i = 0; i < filters.length; i++) {
+        if (filters[i].name == filterName) {
+            return filters[i];
+        }
+    }
+    ;
 };
 
-var getUpdatedParams = function() {
-  var paramName;
-  var paramString = '';
-  var params = URLManager.getParams();
-  var filterParams = {};
-  for (var i = 0; i < filters.length; i++) {
-    params[filters[i].getURLName()] = filters[i].getURLValue();
-  };
-
-  for (paramName in filterParams) {
-    params[paramName] = filterParams[paramName];
-  }
-
-  params.page = 1;
-
-  return params;
+var updateURL = function () {
+    var params = getUpdatedParams();
+    URLManager.setParams(params);
 };
 
-var initMobileFilterHiding = function() {
-  var $filtersEl;
-  var filtersHidden = false;
+var getUpdatedParams = function () {
+    var paramName;
+    var paramString = '';
+    var params = URLManager.getParams();
+    var filterParams = {};
+    for (var i = 0; i < filters.length; i++) {
+        params[filters[i].getURLName()] = filters[i].getURLValue();
+    }
+    ;
 
-  $filtersEl = $('.js-filters');
-  $filtersEl.css('height', $filtersEl.innerHeight())
-  $filtersEl.addClass('js-filters-hidden');
-  filtersHidden = true;
+    for (paramName in filterParams) {
+        params[paramName] = filterParams[paramName];
+    }
 
-  $('.js-toggle-filters').click(function(e) {
-    $filtersEl.toggleClass('js-filters-hidden');
-  });
+    params.page = 1;
+
+    return params;
+};
+
+var initMobileFilterHiding = function () {
+    var $filtersEl;
+    var filtersHidden = false;
+
+    $filtersEl = $('.js-filters');
+    $filtersEl.css('height', $filtersEl.innerHeight())
+    $filtersEl.addClass('js-filters-hidden');
+    filtersHidden = true;
+
+    $('.js-toggle-filters').click(function (e) {
+        $filtersEl.toggleClass('js-filters-hidden');
+    });
 };
 
 module.exports = {
-  init: function() {
-    $('.js-filter').each(function() {
-      filters.push(new Filter($(this)));
-    });
+    init: function () {
+        $('.js-filter').each(function () {
+            filters.push(new Filter($(this)));
+        });
 
-    initActiveFilterList();
-    selectRequiredFilters();
+        initActiveFilterList();
+        selectRequiredFilters();
 
-    initMobileFilterHiding();
-  }
-}
+        initMobileFilterHiding();
+    }
+};
