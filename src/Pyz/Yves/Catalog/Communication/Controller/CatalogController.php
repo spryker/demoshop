d5e5bd007b4b4f12ba316f5db61b2163bf4d7519
Marco Podien <marco@lazystar.de>
2015-07-09 15:04:31 +0200

    Formatting and translations

--- src/Pyz/Yves/Catalog/Communication/Controller/CatalogController.php
+++ src/Pyz/Yves/Catalog/Communication/Controller/CatalogController.php
@@ -17,7 +17,6 @@ class CatalogController extends AbstractController
     public function indexAction(array $categoryNode, Request $request)
     {
         $search = $this->getLocator()->catalog()->client()->createFacetSearch($request, $categoryNode);
-        $search->setItemsPerPage(6);
 
         $categoryTree = $this->getLocator()->categoryExporter()->client()->getTreeFromCategoryNode($categoryNode, $this->getLocale());
         $searchResults = array_merge($search->getResult(), ['category' => $categoryNode, 'categoryTree' => $categoryTree]);
