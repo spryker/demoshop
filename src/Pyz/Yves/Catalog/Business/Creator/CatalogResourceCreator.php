239051479be1b67da25c44085a588987c7ab51e4
Marco Podien <marco@lazystar.de>
2015-07-13 11:49:12 +0200

    Corrected Use statements

--- src/Pyz/Yves/Catalog/Business/Creator/CatalogResourceCreator.php
+++ src/Pyz/Yves/Catalog/Business/Creator/CatalogResourceCreator.php
@@ -5,7 +5,7 @@ namespace Pyz\Yves\Catalog\Business\Creator;
 use SprykerFeature\Shared\Application\Communication\ControllerServiceBuilder;
 use SprykerFeature\Shared\Category\CategoryResourceSettings;
 use SprykerFeature\Yves\FrontendExporter\Creator\ResourceCreatorInterface;
-use Pyz\Client\Catalog\Model\FacetConfig;
+use Pyz\Client\Catalog\Service\Model\FacetConfig;
 use Silex\Application;
 use SprykerEngine\Yves\Kernel\Communication\BundleControllerAction;
 use SprykerEngine\Yves\Kernel\Communication\Controller\BundleControllerActionRouteNameResolver;
