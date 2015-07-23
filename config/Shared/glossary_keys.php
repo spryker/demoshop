b62654f5b0ecf0ade42ebc7d970eab01445a0c58
Benjamin Nadland <me@benjaminnadland.de>
2015-07-23 12:04:13 +0200

    DEM-62 Readd glossary keys file.

---
+++ config/Shared/glossary_keys.php
@@ -0,0 +1,13 @@
+<?php
+
+return
+    //TODO think of a scheme how to name the keys
+    [
+        'FileKey1' => '',
+        'FileKey2' => '',
+        'FileKey3' => '',
+        'FileKey4' => '',
+        'FileKey5' => '',
+        'FileKey6' => '',
+        'FileKey7' => '',
+    ];
