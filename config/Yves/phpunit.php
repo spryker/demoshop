06b942fda1a675fa6c60d2c55a94a5b452622a03
Benjamin Nadland <me@benjaminnadland.de>
2015-07-23 11:15:29 +0200

    DEM-62 Remove legacy config files.

--- config/Yves/phpunit.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-$bootstrap = SprykerFeature\Shared\Library\SystemUnderTest\SystemUnderTestBootstrap::getInstance();
-$bootstrap->bootstrap('Yves');
