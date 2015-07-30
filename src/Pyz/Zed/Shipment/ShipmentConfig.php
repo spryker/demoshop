08ddf6fb7c3789fccbb2ca1cb4777c4e2caf292c
Dmytro Savin <savin.dmitry@gmail.com>
2015-07-30 20:07:44 +0200

    СD-272 Add shipment config

--- src/Pyz/Zed/Shipment/ShipmentConfig.php
+++ src/Pyz/Zed/Shipment/ShipmentConfig.php
@@ -9,43 +9,4 @@ class ShipmentConfig extends SprykerShipmentConfig
 
     const DHL_PAKET = 'DHL Paket';
     const DHL_EXPRESS = 'DHL Express';
-
-    /**
-     * @return array
-     */
-    public function getAvailabilityPlugins()
-    {
-        $plugins = [
-            self::DHL_EXPRESS => $this->getLocator()->shipment()->pluginAvailabilityDHLExpressPlugin(),
-            self::DHL_PAKET => $this->getLocator()->shipment()->pluginAvailabilityDHLPaketPlugin()
-        ];
-
-        return $plugins;
-    }
-
-    /**
-     * @return array
-     */
-    public function getPriceCalculationPlugins()
-    {
-        $plugins = [
-            self::DHL_EXPRESS => $this->getLocator()->shipment()->pluginPriceCalculationDHLExpressPlugin(),
-            self::DHL_PAKET => $this->getLocator()->shipment()->pluginPriceCalculationDHLPaketPlugin()
-        ];
-
-        return $plugins;
-    }
-
-    /**
-     * @return array
-     */
-    public function getDeliveryTimePlugins()
-    {
-        $plugins = [
-            self::DHL_EXPRESS => $this->getLocator()->shipment()->pluginDeliveryTimeDHLExpressPlugin(),
-            self::DHL_PAKET => $this->getLocator()->shipment()->pluginDeliveryTimeDHLPaketPlugin()
-        ];
-
-        return $plugins;
-    }
 }
