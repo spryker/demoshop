54443e52cfa1a0e7fad33ca69954be131211992f
Dmytro Savin <savin.dmitry@gmail.com>
2015-07-31 07:01:47 +0200

    СD-272 Fix plugins

--- src/Pyz/Zed/Shipment/Communication/Plugin/DeliveryTime/DHLExpressPlugin.php
+++ src/Pyz/Zed/Shipment/Communication/Plugin/DeliveryTime/DHLExpressPlugin.php
@@ -6,19 +6,26 @@
 
 namespace Pyz\Zed\Shipment\Communication\Plugin\DeliveryTime;
 
-use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Cart\CartInterface;
 use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
 use SprykerFeature\Zed\Shipment\Communication\Plugin\ShipmentMethodDeliveryTimePluginInterface;
 
 class DHLExpressPlugin extends AbstractPlugin implements ShipmentMethodDeliveryTimePluginInterface
 {
+    const HALF_DAY_IN_SECONDS = 43200;
+
     /**
-     * @param OrderTransfer $orderTransfer
+     * @param CartInterface $cartTransfer
      *
      * @return int
      */
-    public function getTime(OrderTransfer $orderTransfer)
+    public function getTime(CartInterface $cartTransfer)
     {
-        return 10000;
+        $count = 0;
+        foreach ($cartTransfer->getItems() as $item) {
+            $count += $item->getQuantity();
+        }
+
+        return ($count < 4) ? self::HALF_DAY_IN_SECONDS : self::HALF_DAY_IN_SECONDS * 2;
     }
 }
--- src/Pyz/Zed/Shipment/Communication/Plugin/DeliveryTime/DHLPaketPlugin.php
+++ src/Pyz/Zed/Shipment/Communication/Plugin/DeliveryTime/DHLPaketPlugin.php
@@ -6,19 +6,24 @@
 
 namespace Pyz\Zed\Shipment\Communication\Plugin\DeliveryTime;
 
-use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Cart\CartInterface;
 use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
 use SprykerFeature\Zed\Shipment\Communication\Plugin\ShipmentMethodDeliveryTimePluginInterface;
 
 class DHLPaketPlugin extends AbstractPlugin implements ShipmentMethodDeliveryTimePluginInterface
 {
+
+    const DAY_IN_SECONDS = 86400;
+
     /**
-     * @param OrderTransfer $orderTransfer
+     * @param CartInterface $cartTransfer
      *
      * @return int
      */
-    public function getTime(OrderTransfer $orderTransfer)
+    public function getTime(CartInterface $cartTransfer)
     {
-        return 10000000;
+        $time = self::DAY_IN_SECONDS * ((date('H') < 11) ? 2 : 3);
+
+        return $time;
     }
 }
