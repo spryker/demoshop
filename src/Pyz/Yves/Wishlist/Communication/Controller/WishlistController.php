bf6c532d282eb8e90976f0236da42f59758fea09
Aurimas Lickus <aurimas.lickus@spryker.com>
2015-08-04 17:23:48 +0200

    CD-275: added minimal ui and grouping plugins

--- src/Pyz/Yves/Wishlist/Communication/Controller/WishlistController.php
+++ src/Pyz/Yves/Wishlist/Communication/Controller/WishlistController.php
@@ -5,8 +5,12 @@
  */
 namespace Pyz\Yves\Wishlist\Communication\Controller;
 
+use Generated\Shared\Transfer\ItemTransfer;
+use Pyz\Yves\Wishlist\Communication\Plugin\WishlistControllerProvider;
 use SprykerEngine\Yves\Application\Communication\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 class WishlistController extends AbstractController
 {
@@ -22,5 +26,76 @@ class WishlistController extends AbstractController
         return [
           'customerWishlist' => $wishlistClient->getCustomerWishlist()
         ];
+    }
+
+    /**
+     * @param string $sku
+     * @param int    $quantity
+     *
+     * @return RedirectResponse
+     */
+    public function addAction($sku, $quantity = 1)
+    {
+        $wishlistClient = $this->getLocator()->wishlist()->client();
+
+        $itemTransfer = new ItemTransfer();
+        $itemTransfer->setSku($sku)->setQuantity($quantity);
+
+        $wishlistClient->addItem($itemTransfer);
+
+        return $this->redirectResponseInternal(WishlistControllerProvider::ROUTE_WISHLIST);
+    }
+
+    /**
+     * @param string $sku
+     *
+     * @return RedirectResponse
+     */
+    public function removeAction($sku)
+    {
+        $wishlistClient = $this->getLocator()->wishlist()->client();
+
+        $itemTransfer = new ItemTransfer();
+        $itemTransfer->setSku($sku)->setQuantity(-1);
+
+        $wishlistClient->removeItem($itemTransfer);
+
+        return $this->redirectResponseInternal(WishlistControllerProvider::ROUTE_WISHLIST);
+    }
+
+    /**
+     * @param     $sku
+     * @param int $quantity
+     *
+     * @return RedirectResponse
+     */
+    public function increaseAction($sku, $quantity = 1)
+    {
+        $wishlistClient = $this->getLocator()->wishlist()->client();
+
+        $itemTransfer = new ItemTransfer();
+        $itemTransfer->setSku($sku)->setQuantity($quantity);
+
+        $wishlistClient->increaseItemQuantity($itemTransfer);
+
+        return $this->redirectResponseInternal(WishlistControllerProvider::ROUTE_WISHLIST);
+    }
+
+    /**
+     * @param     $sku
+     * @param int $quantity
+     *
+     * @return RedirectResponse
+     */
+    public function reduceAction($sku, $quantity = 1)
+    {
+        $wishlistClient = $this->getLocator()->wishlist()->client();
+
+        $itemTransfer = new ItemTransfer();
+        $itemTransfer->setSku($sku)->setQuantity($quantity);
+
+        $wishlistClient->decreaseItemQuantity($itemTransfer);
+
+        return $this->redirectResponseInternal(WishlistControllerProvider::ROUTE_WISHLIST);
     }
 }
