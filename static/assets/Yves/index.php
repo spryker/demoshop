ec96b0efa1a304b15cea5e93ed1f36b038f8e667
René Klatt <rene.klatt@spryker.com>
2015-09-02 12:47:48 +0000

    CD-225 removed error message from index.php files this is added in Environment class

--- static/assets/Yves/index.php
+++ static/assets/Yves/index.php
@@ -12,10 +12,6 @@ defined('APPLICATION_ROOT_DIR') or define('APPLICATION_ROOT_DIR', realpath(__DIR
 
 require_once(APPLICATION_ROOT_DIR . '/vendor/spryker/spryker/Bundles/Library/src/SprykerFeature/Shared/Library/Application/Environment.php');
 
-if (!file_exists(APPLICATION_ROOT_DIR . DIRECTORY_SEPARATOR . '.class_map')) {
-    echo 'Can\'t find a class map file. Please run $ vendor/bin/class-map-builder to create a class map.';exit;
-}
-
 Environment::initialize('Yves');
 
 $bootstrap = new YvesBootstrap();
