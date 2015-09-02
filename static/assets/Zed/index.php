639018618f11bb62ec38b6694b144545f78262e3
René Klatt <rene.klatt@spryker.com>
2015-09-02 05:37:18 +0000

    CD-225 move builder to Kernel

--- static/assets/Zed/index.php
+++ static/assets/Zed/index.php
@@ -11,6 +11,10 @@ define('APPLICATION', 'ZED');
 defined('APPLICATION_ROOT_DIR') or define('APPLICATION_ROOT_DIR', realpath(__DIR__ . '/../../..'));
 require_once(APPLICATION_ROOT_DIR . '/vendor/spryker/spryker/Bundles/Library/src/SprykerFeature/Shared/Library/Application/Environment.php');
 
+if (!file_exists(APPLICATION_ROOT_DIR . DIRECTORY_SEPARATOR . '.class_map')) {
+    echo 'Can\'t find a class map file. Please run $ vendor/bin/class-map-builder to create a class map.';exit;
+}
+
 Environment::initialize('Zed');
 
 $bootstrap = new ZedBootstrap();
