bc171b0378e045bfd653c10241b779721fe9fe1a
Stefan Langwald <stefanlangwald+github.com@gmail.com>
2015-09-08 15:17:15 +0200

    CD-375 Config files for Lumberjack

--- config/Shared/config_default-development_DE.php
+++ config/Shared/config_default-development_DE.php
@@ -5,6 +5,7 @@ use SprykerFeature\Shared\Payone\PayoneConfig;
 use SprykerFeature\Shared\Setup\SetupConfig;
 use SprykerFeature\Shared\System\SystemConfig;
 use SprykerFeature\Shared\Yves\YvesConfig;
+use SprykerEngine\Shared\Lumberjack\LumberjackConfig;
 
 $config[SystemConfig::ZED_MYSQL_USERNAME] = 'development';
 $config[SystemConfig::ZED_MYSQL_PASSWORD] = 'mate20mg';
@@ -51,3 +52,7 @@ $config[SystemConfig::ZED_RABBITMQ_PORT] =                 '5672';
 $config[SystemConfig::ZED_RABBITMQ_USERNAME] =             'DE_development';
 $config[SystemConfig::ZED_RABBITMQ_PASSWORD] =             'mate20mg';
 $config[SystemConfig::ZED_RABBITMQ_VHOST] =                '/DE_development_zed';
+
+$config[LumberjackConfig::WRITER_OPTIONS] = [
+    '\SprykerEngine\Shared\Lumberjack\Model\Writer\File' => ['log_path' => '/data/logs/development/DE/'],
+];
