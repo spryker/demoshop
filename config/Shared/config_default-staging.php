f2838a7d3e83cd0c23df8b2faa5da7987446d75e
Stephan Schulze <stephan.schulze@project-a.com>
2015-10-08 13:49:34 +0200

    added default deploy directory and updated staging configuration

--- config/Shared/config_default-staging.php
+++ config/Shared/config_default-staging.php
@@ -5,8 +5,49 @@
 
 
 use SprykerFeature\Shared\System\SystemConfig;
+use SprykerFeature\Shared\Yves\YvesConfig;
+use SprykerFeature\Shared\Application\ApplicationConfig;
+use SprykerFeature\Shared\Acl\AclConfig;
+use SprykerFeature\Shared\Session\SessionConfig;
 
-$config[SystemConfig::CLOUD_ENABLED] = true;
-$config[SystemConfig::CLOUD_OBJECT_STORAGE_ENABLED] = true;
-$config[SystemConfig::CLOUD_CDN_ENABLED] = true;
+$config[YvesConfig::YVES_SESSION_SAVE_HANDLER] = SessionConfig::SESSION_HANDLER_REDIS;
+$config[SystemConfig::ZED_SESSION_SAVE_HANDLER] = SessionConfig::SESSION_HANDLER_FILE;
 
+$config[SystemConfig::ZED_DB_USERNAME] = 'staging';
+$config[SystemConfig::ZED_DB_PASSWORD] = 'mate20mg';
+$config[SystemConfig::ZED_DB_HOST]     = '127.0.0.1';
+$config[SystemConfig::ZED_DB_PORT]     = 5432;
+$config[SystemConfig::ZED_DB_DATABASE] = null;
+
+$config[ApplicationConfig::ZED_TWIG_OPTIONS] = [
+    'cache' => true,
+];
+$config[ApplicationConfig::YVES_TWIG_OPTIONS] = [
+    'cache' => true,
+];
+
+$config[SystemConfig::YVES_STORAGE_SESSION_REDIS_PROTOCOL] = 'tcp';
+$config[SystemConfig::YVES_STORAGE_SESSION_REDIS_HOST] = '127.0.0.1';
+$config[SystemConfig::YVES_STORAGE_SESSION_REDIS_PORT] = '13009';
+
+$config[SystemConfig::ZED_STORAGE_SESSION_REDIS_PROTOCOL] = $config[SystemConfig::YVES_STORAGE_SESSION_REDIS_PROTOCOL];
+$config[SystemConfig::ZED_STORAGE_SESSION_REDIS_HOST] = $config[SystemConfig::YVES_STORAGE_SESSION_REDIS_HOST];
+$config[SystemConfig::ZED_STORAGE_SESSION_REDIS_PORT] = $config[SystemConfig::YVES_STORAGE_SESSION_REDIS_PORT];
+
+$config[YvesConfig::YVES_SESSION_COOKIE_DOMAIN] = $config[SystemConfig::HOST_YVES];
+
+$config[SystemConfig::JENKINS_BASE_URL] = 'http://' . $config[SystemConfig::HOST_ZED_GUI] . ':13007/jenkins';
+$config[SystemConfig::JENKINS_DIRECTORY] = '/data/shop/development/shared/data/common/jenkins';
+$config[YvesConfig::TRANSFER_DEBUG_SESSION_FORWARD_ENABLED] = false;
+
+$config[ApplicationConfig::NAVIGATION_CACHE_ENABLED] = true;
+
+$config[AclConfig::ACL_USER_RULE_WHITELIST][] = [
+    'bundle' => 'wdt',
+    'controller' => '*',
+    'action' => '*',
+    'type' => 'allow',
+];
+
+$config[SystemConfig::PROPEL_DEBUG] = false;
+$config[ApplicationConfig::SHOW_SYMFONY_TOOLBAR] = false;
