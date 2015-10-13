c8a42323da45ff3a84880320094f663f3aacb440
René Klatt <rene.klatt@spryker.com>
2015-10-13 09:41:57 +0000

    CD-519 refactored boostrap

--- src/Pyz/Zed/Application/Communication/ZedBootstrap.php
+++ src/Pyz/Zed/Application/Communication/ZedBootstrap.php
@@ -2,57 +2,34 @@
 
 namespace Pyz\Zed\Application\Communication;
 
-use Generated\Zed\Ide\AutoCompletion;
 use Silex\Provider\FormServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\SessionServiceProvider as SilexSessionServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\WebProfilerServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\ServiceProviderInterface;
 use SprykerEngine\Shared\Kernel\Store;
-use SprykerEngine\Zed\Kernel\Locator;
-use SprykerEngine\Zed\Translation\Communication\Plugin\TranslationServiceProvider;
+use SprykerEngine\Zed\Application\Communication\ZedBootstrap as SprykerZedBootstrap;
 use SprykerFeature\Shared\Application\ApplicationConfig;
-use SprykerFeature\Shared\Application\Business\Application;
-use SprykerFeature\Shared\Application\Business\Bootstrap;
+use SprykerEngine\Shared\Application\Communication\Application;
 use SprykerFeature\Shared\Application\Business\Routing\SilexRouter;
-use SprykerFeature\Shared\Application\Communication\Plugin\ServiceProvider\RoutingServiceProvider;
-use SprykerFeature\Shared\Application\Communication\Plugin\ServiceProvider\UrlGeneratorServiceProvider;
 use SprykerFeature\Shared\Library\Config;
 use SprykerFeature\Shared\Library\Context;
+use SprykerFeature\Shared\Library\DataDirectory;
 use SprykerFeature\Shared\Library\DateFormatter;
+use SprykerFeature\Shared\Library\Environment;
 use SprykerFeature\Shared\Library\Twig\DateFormatterTwigExtension;
 use SprykerFeature\Shared\System\SystemConfig;
 use SprykerFeature\Zed\Application\Business\Model\Router\MvcRouter;
 use SprykerFeature\Zed\Application\Business\Model\Twig\ZedExtension;
-use SprykerFeature\Zed\Application\Communication\Plugin\Pimple;
-use SprykerFeature\Zed\Application\Communication\Plugin\ServiceProvider\EnvironmentInformationServiceProvider;
-use SprykerFeature\Zed\Application\Communication\Plugin\ServiceProvider\NewRelicServiceProvider;
-use SprykerEngine\Zed\Propel\Communication\Plugin\ServiceProvider\PropelServiceProvider;
-use SprykerFeature\Zed\Application\Communication\Plugin\ServiceProvider\RequestServiceProvider;
-use SprykerFeature\Zed\Application\Communication\Plugin\ServiceProvider\SslServiceProvider;
-use SprykerFeature\Zed\Application\Communication\Plugin\ServiceProvider\TwigServiceProvider;
-use SprykerFeature\Zed\Kernel\Communication\Plugin\GatewayServiceProviderPlugin;
 use SprykerFeature\Zed\Price\Communication\Plugin\Twig\PriceTwigExtensions;
 use Symfony\Bridge\Twig\Extension\TranslationExtension;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
 
-class ZedBootstrap extends Bootstrap
+class ZedBootstrap extends SprykerZedBootstrap
 {
-
-    /**
-     * @return Application
-     */
-    protected function getBaseApplication()
-    {
-        $application = new Application();
-
-        Pimple::setApplication($application);
-
-        return $application;
-    }
 
     /**
      * @param Application $app
@@ -75,9 +52,8 @@ class ZedBootstrap extends Bootstrap
     protected function beforeBoot(Application $app)
     {
         $app['locale'] = Store::getInstance()->getCurrentLocale();
-        if (\SprykerFeature\Shared\Library\Environment::isDevelopment()) {
-            $app['profiler.cache_dir'] =
-                \SprykerFeature\Shared\Library\DataDirectory::getLocalStoreSpecificPath('cache/profiler');
+        if (Environment::isDevelopment()) {
+            $app['profiler.cache_dir'] = DataDirectory::getLocalStoreSpecificPath('cache/profiler');
         }
     }
 
@@ -96,31 +72,26 @@ class ZedBootstrap extends Bootstrap
      */
     protected function getServiceProviders(Application $app)
     {
-        $locator = $this->getLocator();
-        $sessionServiceProvider = $locator->session()->pluginServiceProviderSessionServiceProvider();
-        $sessionServiceProvider->setClient(
-            $locator->session()->client()
-        );
-
         $providers = [
-            new SilexSessionServiceProvider(),
-            $sessionServiceProvider,
-            new PropelServiceProvider(),
-            $locator->auth()->pluginServiceProviderRedirectAfterLoginProvider(),
-            $locator->auth()->pluginBootstrapAuthBootstrapProvider(),
-            new RequestServiceProvider(),
-            new SslServiceProvider(),
+            new SessionServiceProvider(),
+            $this->getSessionServiceProvider(),
+            $this->getLocator()->propel()->pluginServiceProviderPropelServiceProvider(),
+            $this->getLocator()->auth()->pluginServiceProviderRedirectAfterLoginProvider(),
+            $this->getLocator()->auth()->pluginBootstrapAuthBootstrapProvider(),
+            $this->getLocator()->application()->pluginServiceProviderRequestServiceProvider(),
+            $this->getLocator()->application()->pluginServiceProviderSslServiceProvider(),
             new ServiceControllerServiceProvider(),
-            new RoutingServiceProvider(),
-            $locator->acl()->pluginBootstrapAclBootstrapProvider(),
+            $this->getLocator()->application()->pluginServiceProviderRoutingServiceProvider(),
+            $this->getLocator()->acl()->pluginBootstrapAclBootstrapProvider(),
             new ValidatorServiceProvider(),
             new FormServiceProvider(),
             new TwigServiceProvider(),
-            new EnvironmentInformationServiceProvider(),
-            new TranslationServiceProvider(),
+            $this->getLocator()->application()->pluginServiceProviderTwigServiceProvider(),
+            $this->getLocator()->application()->pluginServiceProviderEnvironmentInformationServiceProvider(),
+            $this->getLocator()->translation()->pluginTranslationServiceProvider(),
             $this->getGatewayServiceProvider(),
-            new UrlGeneratorServiceProvider(),
-            new NewRelicServiceProvider(),
+            $this->getLocator()->application()->pluginServiceProviderUrlGeneratorServiceProvider(),
+            $this->getLocator()->application()->pluginServiceProviderNewRelicServiceProvider(),
             new HttpFragmentServiceProvider(),
         ];
 
@@ -154,57 +125,15 @@ class ZedBootstrap extends Bootstrap
         $navigation = $this->getNavigation();
         $breadcrumbs = $navigation['path'];
 
-        $username = '';
-
-        $userFacade = $this->getLocator()->user()->facade();
-        if ($userFacade->hasCurrentUser()) {
-            $user = $userFacade->getCurrentUser();
-            $username = sprintf('%s %s', $user->getFirstName(), $user->getLastName());
-        }
-
         return [
             'environment' => APPLICATION_ENV,
             'store' => Store::getInstance()->getStoreName(),
             'title' => Config::get(SystemConfig::PROJECT_NAMESPACE) . ' | Zed | ' . ucfirst(APPLICATION_ENV),
             'currentController' => get_class($this),
-            'navigation' => $this->getNavigation(),
+            'navigation' => $navigation,
             'breadcrumbs' => $breadcrumbs,
-            'username' => $username,
+            'username' => $this->getUsername(),
         ];
-    }
-
-    /**
-     * @return AutoCompletion
-     */
-    public function getLocator()
-    {
-        return Locator::getInstance();
-    }
-
-    /**
-     * @return GatewayServiceProviderPlugin
-     */
-    protected function getGatewayServiceProvider()
-    {
-        $locator = $this->getLocator();
-        $controllerListener = $locator->kernel()->pluginGatewayControllerListenerPlugin();
-        $serviceProvider = $locator->kernel()->pluginGatewayServiceProviderPlugin();
-        $serviceProvider->setControllerListener($controllerListener);
-
-        return $serviceProvider;
-    }
-
-    /**
-     * @return string
-     */
-    protected function getNavigation()
-    {
-        $request = Request::createFromGlobals();
-
-        return $this->getLocator()
-            ->application()
-            ->pluginNavigation()
-            ->buildNavigation($request->getPathInfo());
     }
 
 }
