33f41b7ef7777c83f350506dbcc5082e54d10eac
René Klatt <rene.klatt@spryker.com>
2015-10-13 12:54:39 +0000

    CD-519 add test environment config

--- config/Shared/config_default-development.php
+++ config/Shared/config_default-development.php
@@ -9,6 +9,7 @@ use SprykerFeature\Shared\Yves\YvesConfig;
 use SprykerFeature\Shared\Application\ApplicationConfig;
 use SprykerFeature\Shared\Acl\AclConfig;
 use SprykerFeature\Shared\Session\SessionConfig;
+use SprykerFeature\Shared\Payone\PayoneConfigConstants;
 
 $config[YvesConfig::YVES_SESSION_SAVE_HANDLER] = SessionConfig::SESSION_HANDLER_REDIS;
 $config[SystemConfig::ZED_SESSION_SAVE_HANDLER] = SessionConfig::SESSION_HANDLER_FILE;
@@ -26,6 +27,19 @@ $config[YvesConfig::YVES_SESSION_COOKIE_DOMAIN] = $config[SystemConfig::HOST_YVE
 $config[SystemConfig::JENKINS_BASE_URL] = 'http://' . $config[SystemConfig::HOST_ZED_GUI] . ':10007/jenkins';
 $config[SystemConfig::JENKINS_DIRECTORY] = '/data/shop/development/shared/data/common/jenkins';
 $config[YvesConfig::TRANSFER_DEBUG_SESSION_FORWARD_ENABLED] = true;
+
+$config[PayoneConfigConstants::PAYONE] = [
+    PayoneConfigConstants::PAYONE_CREDENTIALS_ENCODING => 'UTF-8',
+    PayoneConfigConstants::PAYONE_CREDENTIALS_KEY => 'oZZfMY6L224Q51jq',
+    PayoneConfigConstants::PAYONE_CREDENTIALS_MID => '29250',
+    PayoneConfigConstants::PAYONE_CREDENTIALS_AID => '29499',
+    PayoneConfigConstants::PAYONE_CREDENTIALS_PORTAL_ID => '2020679',
+    PayoneConfigConstants::PAYONE_PAYMENT_GATEWAY_URL => 'https://api.pay1.de/post-gateway/',
+    PayoneConfigConstants::PAYONE_REDIRECT_SUCCESS_URL => $config[SystemConfig::HOST_YVES] . '/checkout/success/',
+    PayoneConfigConstants::PAYONE_REDIRECT_ERROR_URL => $config[SystemConfig::HOST_YVES] . '/checkout/index/',
+    PayoneConfigConstants::PAYONE_REDIRECT_BACK_URL => $config[SystemConfig::HOST_YVES] . '/checkout/regular-redirect-payment-cancellation/',
+    PayoneConfigConstants::PAYONE_MODE => '',
+];
 
 $config[ApplicationConfig::NAVIGATION_CACHE_ENABLED] = false;
 
