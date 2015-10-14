98d2e56ac71d3c9c4b608e348a47b253139b6e87
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-14 17:57:41 +0200

    create afterbuy constant interface

---
+++ src/Pyz/Shared/OrderExporter/AfterbuyExportConstantInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Pyz\Shared\OrderExporter;
+
+interface AfterbuyExportConstantInterface
+{
+    const AFTERBUY_PARTNER_ID = 'partnerId';
+    const AFTERBUY_PARTNER_PASS = 'partnerPass';
+    const AFTERBUY_USER_ID = 'userId';
+}
