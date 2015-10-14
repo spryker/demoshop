ee7a6d7f9e3e9e8c0f8aad373300509f3b346bd0
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-14 17:58:08 +0200

    create afterbuy command file

---
+++ src/Pyz/Zed/OrderExporter/Communication/Console/ExportToAfterBuyConsole.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Pyz\Zed\OrderExporter\Communication\Console;
+
+use Pyz\Zed\OrderExporter\Business\OrderExporterFacade;
+use SprykerFeature\Zed\Console\Business\Model\Console;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * @method OrderExporterFacade getFacade()
+ */
+class ExportToAfterBuyConsole extends Console
+{
+    const COMMAND_NAME = 'order-exporter:export';
+    const COMMAND_DESCRIPTION = 'Export Orders to AfterBuy';
+
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_NAME);
+        $this->setDescription(self::COMMAND_DESCRIPTION);
+        $this->addArgument(
+            'orderId',
+            InputArgument::REQUIRED
+        );
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Export Order');
+        $orderId = $input->getArgument('orderId');
+
+        $this->getFacade()->exportOrder($orderId);
+    }
+
+}
