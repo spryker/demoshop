4e63cbb1d5d6e56982c5e843d9122369b90ebca0
René Klatt <rene.klatt@spryker.com>
2015-10-16 10:17:54 +0000

    CD-544 refactor script

--- refactoring/refactorPropel.php
+++ refactoring/refactorPropel.php
@@ -27,13 +27,92 @@ function getFiles(array $directories)
 }
 
 $renameNamespaceAndPackage = function () {
+    $directories = [
+        __DIR__ . '/../src/Pyz/Zed/*/Persistence/Propel/Schema/',
+        __DIR__ . '/../vendor/spryker/spryker/Bundles/*/src/*/Zed/*/Persistence/Propel/Schema/',
+    ];
 
+    $filesystem = new Filesystem();
+    $schemas = getFiles($directories)->name('*.schema.xml');
+    /** @var SplFileInfo $schema */
+    foreach ($schemas as $schema) {
+        $content = $schema->getContents();
+        $content = preg_replace('/namespace="(.*?)"/', 'namespace="Propel"', $content);
+        $content = preg_replace('/\spackage="(.*?)"/', ' package="src.Propel"', $content);
+
+        $filesystem->dumpFile($schema->getPathname(), $content);
+    }
 };
 
 $addBaseClassToTables = function () {
+    $directories = [
+        __DIR__ . '/../src/Pyz/Zed/*/Persistence/Propel/Schema/',
+        __DIR__ . '/../vendor/spryker/spryker/Bundles/*/src/*/Zed/*/Persistence/Propel/Schema/',
+    ];
 
+    $filesystem = new Filesystem();
+    $schemas = getFiles($directories)->name('*.schema.xml');
+    /** @var SplFileInfo $schema */
+    foreach ($schemas as $schema) {
+
+        $content = $schema->getContents();
+        $nameParts = explode('/', $schema->getPathname());
+        array_pop($nameParts);
+        $currentNamespace = implode('\\', array_slice($nameParts, -6));
+
+        $callback = function ($match) use ($currentNamespace) {
+            if (!preg_match('/phpName="(.*?)"/', $match[0], $phpNameMatches)) {
+                preg_match('/name="(.*?)"/', $match[0], $tableNameMatches);
+                $baseClassName = $tableNameMatches[1];
+                $filter = new \Zend\Filter\Word\UnderscoreToCamelCase();
+                $baseClassName = $filter->filter($baseClassName);
+            } else {
+                preg_match('/phpName="(.*?)"/', $match[0], $phpNameMatches);
+                $baseClassName = $phpNameMatches[1];
+            }
+            $fullBaseClassName = $currentNamespace . '\\' . $baseClassName;
+            $newTableDefinition = str_replace('>', ' baseClass="' . $fullBaseClassName . '">', $match[0]);
+
+            return $newTableDefinition;
+        };
+
+        $content = preg_replace_callback('/<table(.*?)>/', $callback, $content);
+
+        $filesystem->dumpFile($schema->getPathname(), $content);
+    }
 };
 
-$makeVendorClassesAbstract = function () {
+$removeMapAndBaseClasses = function () {
+    $directories = [
+        __DIR__ . '/../src/Pyz/Zed/*/Persistence/Propel/Base/',
+        __DIR__ . '/../src/Pyz/Zed/*/Persistence/Propel/Map/',
+        __DIR__ . '/../vendor/spryker/spryker/Bundles/*/src/*/Zed/*/Persistence/Propel/Base/',
+        __DIR__ . '/../vendor/spryker/spryker/Bundles/*/src/*/Zed/*/Persistence/Propel/Map/',
+    ];
 
+    $filesystem = new Filesystem();
+    $filesystem->remove(getFiles($directories));
 };
+
+$updateVendorClasses = function () {
+    $directories = [
+        __DIR__ . '/../src/Pyz/Zed/*/Persistence/Propel/',
+        __DIR__ . '/../vendor/spryker/spryker/Bundles/*/src/*/Zed/*/Persistence/Propel/',
+    ];
+
+    $files = getFiles($directories)->name('*.php');
+    $filesystem = new Filesystem();
+    /** @var SplFileInfo $file */
+    foreach ($files as $file) {
+        $content = $file->getContents();
+        $content = preg_replace('/class (.*) extends/', 'abstract class $1 extends', $content);
+        $content = preg_replace('/use (.*)\\\\Propel\\\\Base/', 'use Propel\\\\Base', $content);
+
+        $filesystem->dumpFile($file->getPathname(), $content);
+    }
+};
+
+$renameNamespaceAndPackage();
+$addBaseClassToTables();
+$removeMapAndBaseClasses();
+$updateVendorClasses();
