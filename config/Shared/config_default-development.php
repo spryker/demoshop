2c7c43874e4a90eaf1f2b5ff5003bb1717043d7f
René <rene.klatt@spryker.com>
2015-10-19 13:03:38 +0000

    CD-547 enable integration checks in development

--- config/Shared/config_default-development.php
+++ config/Shared/config_default-development.php
@@ -56,3 +56,4 @@ $config[ApplicationConfig::SHOW_SYMFONY_TOOLBAR] = true;
 
 $config[ApplicationConfig::STORE_PREFIX] = 'DEV';
 $config[ApplicationConfig::SET_REPEAT_DATA] = true;
+$config[ApplicationConfig::ALLOW_INTEGRATION_CHECKS] = true;
