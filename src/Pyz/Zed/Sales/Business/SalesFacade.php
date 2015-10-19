4f2a25fec3039cda6ef96ee8828ea5f63e812ee6
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-19 19:06:27 +0200

    add get order in QC from project

--- src/Pyz/Zed/Sales/Business/SalesFacade.php
+++ src/Pyz/Zed/Sales/Business/SalesFacade.php
@@ -2,10 +2,25 @@
 
 namespace Pyz\Zed\Sales\Business;
 
+use Pyz\Zed\OrderExporter\Dependency\Facade\OrderExporterToSalesInterface;
 use SprykerFeature\Zed\Sales\Business\SalesFacade as SprykerSalesFacade;
 use SprykerFeature\Zed\SalesCheckoutConnector\Dependency\Facade\SalesCheckoutConnectorToSalesInterface;
+use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrder;
 
-class SalesFacade extends SprykerSalesFacade implements SalesCheckoutConnectorToSalesInterface
+/**
+ * @method SalesDependencyContainer getDependencyContainer()
+ */
+class SalesFacade extends SprykerSalesFacade implements SalesCheckoutConnectorToSalesInterface, OrderExporterToSalesInterface
 {
+    /**
+     * @param int $orderId
+     * @return SpySalesOrder
+     */
+    public function getOrderBySalesOrderId($orderId)
+    {
+        return $this->getDependencyContainer()
+            ->createSalesManager()
+            ->getOrderDetailsBySalesId($orderId);
+    }
 
 }
