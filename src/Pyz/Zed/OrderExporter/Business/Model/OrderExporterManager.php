2e63ece180aa51a32832d37b0efcd7acf1e93e77
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-20 18:42:49 +0200

    separate afterbuy constants

--- src/Pyz/Zed/OrderExporter/Business/Model/OrderExporterManager.php
+++ src/Pyz/Zed/OrderExporter/Business/Model/OrderExporterManager.php
@@ -2,6 +2,7 @@
 
 namespace Pyz\Zed\OrderExporter\Business\Model;
 
+use Pyz\Zed\OrderExporter\AfterbuyConstants;
 use Pyz\Zed\OrderExporter\OrderExporterConfig;
 use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrderAddress;
 use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrderItem;
@@ -12,66 +13,6 @@ use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder;
  */
 class OrderExporterManager
 {
-    const AFTERBUY_ACTION = 'Action';
-    const AFTERBUY_PARTNER_ID = 'PartnerID';
-    const AFTERBUY_PARTNER_PASS = 'PartnerPass';
-    const AFTERBUY_USER_ID = 'UserID';
-
-    const SALES_ORDER_ID = 'Kbenutzername';
-    const CUSTOMER_EMAIL = 'Kemail';
-
-    const ORDER_ID = 'VID';
-    const ORDER_COMMENT = 'Kommentar';
-    const IS_DOUBLE_ORDER = 'CheckVID';
-
-    const SEND_FEEDBACK = 'NoFeeback';
-    const IS_SHIPPING_BILLING_DIFFERENT = 'Lieferanschrift';
-
-    const STOCK_TYPE = 'Bestandart';
-
-    const SHIPPING_COMPANY_NAME = 'KLFirma';
-    const SHIPPING_SALUTATION = 'KLAnrede';
-    const SHIPPING_FIRST_NAME = 'KLVorname';
-    const SHIPPING_LAST_NAME = 'KLNachname';
-    const SHIPPING_STREET = 'KLStrasse';
-    const SHIPPING_ZIP_CODE = 'KLPLZ';
-    const SHIPPING_CITY = 'KLOrt';
-    const SHIPPING_METHOD = '';
-    const SHIPPING_PHONE = 'KLTelefon';
-    const SHIPPING_COUNTRY_ID = 'KLLand';
-    const SHIPPING_COST = 'Versandkosten';
-    const SHIPPING_SERVICE = 'Versandart';
-
-    const BILLING_COMPANY_NAME = 'KFirma';
-    const BILLING_SALUTATION = 'KAnrede';
-    const BILLING_FIRST_NAME = 'KVorname';
-    const BILLING_LAST_NAME = 'KNachname';
-    const BILLING_STREET = 'KStrasse';
-    const BILLING_ZIP_CODE = 'KPLZ';
-    const BILLING_CITY = 'KOrt';
-    const BILLING_METHOD = '';
-    const BILLING_PHONE = 'KTelefon';
-    const BILLING_COUNTRY_ID = 'KLand';
-
-    const ITEM_SKU = 'ArtikelStammID_';
-    const ITEM_WEIGHT = 'ArtikelGewicht_';
-    const ITEM_NAME = 'Artikelname_';
-    const ITEM_QUANTITY_ORDERED = 'ArtikelMenge_';
-    const ITEM_TAX_PERCENTAGE = 'ArtikelMwSt_';
-    const ITEMS_NUMBER = 'PosAnz';
-    const ITEM_PRICE = 'ArtikelEPreis_';
-    const ITEM_NUMBER = 'Artikelnr_';
-    const ITEM_ATTRIBUTE = 'Attribute_';
-    const ITEM_LINK = 'ArtikelLink_';
-
-    const PAYMENT_CHARGE = 'ZahlartenAufschlag';
-    const PAYMENT_BANK_ACCOUNT_NUMBER = 'Kontonummer';
-    const PAYMENT_BANK_ACCOUNT_OWNER = 'Kontoinhaber';
-    const PAYMENT_BANK_CODE = 'BLZ';
-    const PAYMENT_METHOD = 'Zahlart';
-    const PAYMENT_ID = 'ZFunktionsID';
-    const PAYMENT_STATUS = 'SetPay';
-
     /** @var string */
     protected $userId;
 
@@ -94,7 +35,6 @@ class OrderExporterManager
      */
     public function __construct(OrderExporterConfig $orderExporterConfig, AfterbuyConnectorInterface $connector)
     {
-        //@TODO add email error => ds@petsdeli.de
         $this->userId = $orderExporterConfig->getAfterbuyUserId();
         $this->partnerId = $orderExporterConfig->getAfterbuyPartnerId();
         $this->partnerPass = $orderExporterConfig->getAfterbuyPartnerPass();
@@ -109,7 +49,7 @@ class OrderExporterManager
         $afterBuyInfo = $this->configureAfterbuy();
         $afterBuyInfo = $this->getOrderInfo($order, $afterBuyInfo);
         $postString = $this->buildPostString($afterBuyInfo);
-        $sendingResult = $this->sendOrderInfoToAfterBuy($postString);
+        $this->sendOrderInfoToAfterBuy($postString);
     }
 
     /**
@@ -118,9 +58,9 @@ class OrderExporterManager
     protected function configureAfterbuy()
     {
         $postData = array();
-        $postData[self::AFTERBUY_PARTNER_ID] = $this->partnerId;
-        $postData[self::AFTERBUY_PARTNER_PASS] = $this->partnerPass;
-        $postData[self::AFTERBUY_USER_ID] = $this->userId;
+        $postData[AfterbuyConstants::AFTERBUY_PARTNER_ID] = $this->partnerId;
+        $postData[AfterbuyConstants::AFTERBUY_PARTNER_PASS] = $this->partnerPass;
+        $postData[AfterbuyConstants::AFTERBUY_USER_ID] = $this->userId;
 
         return $postData;
     }
@@ -134,11 +74,11 @@ class OrderExporterManager
     protected function getOrderInfo(SpySalesOrder $order, array $postData)
     {
         $items = $order->getItems()->getData();
-        $postData[self::CUSTOMER_EMAIL] = $order->getEmail();
-        $postData[self::SALES_ORDER_ID] = $order->getIdSalesOrder();
+        $postData[AfterbuyConstants::CUSTOMER_EMAIL] = $order->getEmail();
+        $postData[AfterbuyConstants::SALES_ORDER_ID] = $order->getIdSalesOrder();
 
         if (!null == $order->getShipmentMethod()) {
-            $postData[self::SHIPPING_METHOD] = $order->getShipmentMethod()->getName();
+            $postData[AfterbuyConstants::SHIPPING_METHOD] = $order->getShipmentMethod()->getName();
         }
 
         $shippingAddress = $order->getShippingAddress();
@@ -146,9 +86,9 @@ class OrderExporterManager
         $postData = $this->addShippingAddressInfo($shippingAddress, $postData);
 
         if ($billingAddress == $shippingAddress) {
-            $postData[self::IS_SHIPPING_BILLING_DIFFERENT] = 0;
+            $postData[AfterbuyConstants::IS_SHIPPING_BILLING_DIFFERENT] = 0;
         } else {
-            $postData[self::IS_SHIPPING_BILLING_DIFFERENT] = 1;
+            $postData[AfterbuyConstants::IS_SHIPPING_BILLING_DIFFERENT] = 1;
             $postData = $this->addBillingAddressInfo($billingAddress, $postData);
         }
 
@@ -180,15 +120,15 @@ class OrderExporterManager
         /** @var SpySalesOrderItem $item */
         foreach ($items as $item) {
             $numberOfItems ++;
-            $postData[self::ITEM_SKU . $numberOfItems] = $item->getSku();
-            $postData[self::ITEM_NUMBER . $numberOfItems] = $item->getSku();
-            $postData[self::ITEM_QUANTITY_ORDERED . $numberOfItems] = $item->getQuantity();
-            $postData[self::ITEM_NAME . $numberOfItems] = $item->getName();
-            $postData[self::ITEM_TAX_PERCENTAGE . $numberOfItems] = $item->getTaxPercentage();
+            $postData[AfterbuyConstants::ITEM_SKU . $numberOfItems] = $item->getSku();
+            $postData[AfterbuyConstants::ITEM_NUMBER . $numberOfItems] = $item->getSku();
+            $postData[AfterbuyConstants::ITEM_QUANTITY_ORDERED . $numberOfItems] = $item->getQuantity();
+            $postData[AfterbuyConstants::ITEM_NAME . $numberOfItems] = $item->getName();
+            $postData[AfterbuyConstants::ITEM_TAX_PERCENTAGE . $numberOfItems] = $item->getTaxPercentage();
             $postData = $this->addItemDiscountInfo($item, $numberOfItems, $postData);
             $postData = $this->addProductAttributesInfo($item, $numberOfItems, $postData);
         }
-        $postData[self::ITEMS_NUMBER] = count($items);
+        $postData[AfterbuyConstants::ITEMS_NUMBER] = count($items);
 
         return $postData;
     }
@@ -218,7 +158,7 @@ class OrderExporterManager
         if (!null == $item->getDiscounts()->getData()) {
             // @TODO calculate Discounts information (const PAYMENT_CHARGE or ITEM_PRICE)
         } else {
-            $postData[self::ITEM_PRICE . $numberOfItems] = 0;
+            $postData[AfterbuyConstants::ITEM_PRICE . $numberOfItems] = 0;
         }
 
         return $postData;
@@ -232,15 +172,15 @@ class OrderExporterManager
      */
     protected function addShippingAddressInfo(SpySalesOrderAddress $shippingAddress, array $postData)
     {
-        $postData[self::SHIPPING_COMPANY_NAME] = $shippingAddress->getCompany();
-        $postData[self::SHIPPING_SALUTATION] = $shippingAddress->getSalutation();
-        $postData[self::SHIPPING_FIRST_NAME] = $shippingAddress->getFirstName();
-        $postData[self::SHIPPING_LAST_NAME] = $shippingAddress->getLastName();
-        $postData[self::SHIPPING_STREET] = $shippingAddress->getAddress1();
-        $postData[self::SHIPPING_ZIP_CODE] = $shippingAddress->getZipCode();
-        $postData[self::SHIPPING_CITY] = $shippingAddress->getCity();
-        $postData[self::SHIPPING_PHONE] = $shippingAddress->getPhone();
-        $postData[self::SHIPPING_COUNTRY_ID] = $shippingAddress->getFkCountry();
+        $postData[AfterbuyConstants::SHIPPING_COMPANY_NAME] = $shippingAddress->getCompany();
+        $postData[AfterbuyConstants::SHIPPING_SALUTATION] = $shippingAddress->getSalutation();
+        $postData[AfterbuyConstants::SHIPPING_FIRST_NAME] = $shippingAddress->getFirstName();
+        $postData[AfterbuyConstants::SHIPPING_LAST_NAME] = $shippingAddress->getLastName();
+        $postData[AfterbuyConstants::SHIPPING_STREET] = $shippingAddress->getAddress1();
+        $postData[AfterbuyConstants::SHIPPING_ZIP_CODE] = $shippingAddress->getZipCode();
+        $postData[AfterbuyConstants::SHIPPING_CITY] = $shippingAddress->getCity();
+        $postData[AfterbuyConstants::SHIPPING_PHONE] = $shippingAddress->getPhone();
+        $postData[AfterbuyConstants::SHIPPING_COUNTRY_ID] = $shippingAddress->getFkCountry();
 
         return $postData;
     }
@@ -253,15 +193,15 @@ class OrderExporterManager
      */
     protected function addBillingAddressInfo(SpySalesOrderAddress $billingAddress, array $postData)
     {
-        $postData[self::BILLING_COMPANY_NAME] = $billingAddress->getCompany();
-        $postData[self::BILLING_SALUTATION] = $billingAddress->getSalutation();
-        $postData[self::BILLING_FIRST_NAME] = $billingAddress->getFirstName();
-        $postData[self::BILLING_LAST_NAME] = $billingAddress->getLastName();
-        $postData[self::BILLING_STREET] = $billingAddress->getAddress1();
-        $postData[self::BILLING_ZIP_CODE] = $billingAddress->getZipCode();
-        $postData[self::BILLING_CITY] = $billingAddress->getCity();
-        $postData[self::BILLING_PHONE] = $billingAddress->getPhone();
-        $postData[self::BILLING_COUNTRY_ID] = $billingAddress->getFkCountry();
+        $postData[AfterbuyConstants::BILLING_COMPANY_NAME] = $billingAddress->getCompany();
+        $postData[AfterbuyConstants::BILLING_SALUTATION] = $billingAddress->getSalutation();
+        $postData[AfterbuyConstants::BILLING_FIRST_NAME] = $billingAddress->getFirstName();
+        $postData[AfterbuyConstants::BILLING_LAST_NAME] = $billingAddress->getLastName();
+        $postData[AfterbuyConstants::BILLING_STREET] = $billingAddress->getAddress1();
+        $postData[AfterbuyConstants::BILLING_ZIP_CODE] = $billingAddress->getZipCode();
+        $postData[AfterbuyConstants::BILLING_CITY] = $billingAddress->getCity();
+        $postData[AfterbuyConstants::BILLING_PHONE] = $billingAddress->getPhone();
+        $postData[AfterbuyConstants::BILLING_COUNTRY_ID] = $billingAddress->getFkCountry();
 
         return $postData;
     }
@@ -272,7 +212,7 @@ class OrderExporterManager
      */
     protected function buildPostString(array $afterBuyInfo)
     {
-        $postString = self::AFTERBUY_ACTION .'=new';
+        $postString = AfterbuyConstants::AFTERBUY_ACTION .'=new';
 
         foreach ($afterBuyInfo as $afterbuyField => $value) {
             $postString .= '&' . $afterbuyField . '=' . $value;
@@ -290,12 +230,6 @@ class OrderExporterManager
         $sendingResult = $this->afterbuyConnector->sendToAfterBuy($postVariables);
 
         return $sendingResult;
-    }
-
-    /** @TODO: do we still need to send the email???? */
-    protected function sendEmail()
-    {
-
     }
 
 }
