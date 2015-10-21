cc326556f3e8be6c4b1a1fa0cd464ebd6a6b4c7b
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-21 13:56:06 +0200

    create test files

---
+++ tests/Unit/Pyz/Zed/OrderExporter/AfterbuyConnectorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unit\Pyz\Zed\OrderExporter;
+
+class AfterbuyConnectorTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+    }
+
+}
