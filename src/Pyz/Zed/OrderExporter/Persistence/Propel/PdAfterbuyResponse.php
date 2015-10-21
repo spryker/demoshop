864c43e003873be9595682851b48e85e70c79d66
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-21 12:24:35 +0200

    afterbuy response table

---
+++ src/Pyz/Zed/OrderExporter/Persistence/Propel/PdAfterbuyResponse.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pyz\Zed\OrderExporter\Persistence\Propel;
+
+use Pyz\Zed\OrderExporter\Persistence\Propel\Base\PdAfterbuyResponse as BasePdAfterbuyResponse;
+
+/**
+ * Skeleton subclass for representing a row from the 'pd_afterbuy_response' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class PdAfterbuyResponse extends BasePdAfterbuyResponse
+{
+
+}
---
+++ src/Pyz/Zed/OrderExporter/Persistence/Propel/PdAfterbuyResponseQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pyz\Zed\OrderExporter\Persistence\Propel;
+
+use Pyz\Zed\OrderExporter\Persistence\Propel\Base\PdAfterbuyResponseQuery as BasePdAfterbuyResponseQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'pd_afterbuy_response' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class PdAfterbuyResponseQuery extends BasePdAfterbuyResponseQuery
+{
+
+}
