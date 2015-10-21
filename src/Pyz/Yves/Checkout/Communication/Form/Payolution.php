214fc2dba58594d5669469d337d58ee56b681a1e
Hussam Hebbo <hussam.hebbo@spryker.com>
2015-10-21 12:27:18 +0000

    CD-408 fixed payolution form

--- src/Pyz/Yves/Checkout/Communication/Form/Payolution.php
+++ src/Pyz/Yves/Checkout/Communication/Form/Payolution.php
@@ -14,6 +14,16 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class Payolution extends AbstractType
 {
 
+    const FIELD_ADDRESS = 'address';
+    const FIELD_GENDER = 'gender';
+    const FIELD_DATE_OF_BIRTH = 'date_of_birth';
+    const FIELD_EMAIL = 'email';
+    const FIELD_PHONE = 'address_phone';
+    const FIELD_LANGUAGE_CODE = 'language_iso2_code';
+    const FIELD_CURRENCY_CODE = 'currency_iso3_code';
+    const FIELD_ACCOUNT_BRAND = 'account_brand';
+    const FIELD_CLIENT_IP = 'client_ip';
+
     /**
      * @var int
      */
@@ -42,13 +52,13 @@ class Payolution extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('address', new Address($this->tabIndexOffset + 100), [
+            ->add(self::FIELD_ADDRESS, new Address($this->tabIndexOffset + 100), [
                 'data_class' => 'Generated\Shared\Transfer\AddressTransfer',
                 'error_bubbling' => true,
             ])
-            ->add('gender', 'choice', [
+            ->add(self::FIELD_GENDER, 'choice', [
                 'choices' => [
-                    'Mr' => 'Herr',
+                    'Mr' => 'general.customer.salutation.mr',
                     'Mrs' => 'Frau',
                 ],
                 'property_path' => 'address.salutation',
@@ -61,7 +71,7 @@ class Payolution extends AbstractType
                     'style' => 'display: block;',
                 ],
             ])
-            ->add('date_of_birth', 'birthday', [
+            ->add(self::FIELD_DATE_OF_BIRTH, 'birthday', [
                 'label' => false,
                 'required' => true,
                 'widget' => 'single_text',
@@ -73,7 +83,7 @@ class Payolution extends AbstractType
                     'tabindex' => 160 + $this->tabIndexOffset,
                 ],
             ])
-            ->add('email', 'text', [
+            ->add(self::FIELD_EMAIL, 'text', [
                 'label' => false,
                 'required' => true,
                 'property_path' => 'address.email',
@@ -83,7 +93,7 @@ class Payolution extends AbstractType
                     'tabindex' => 170 + $this->tabIndexOffset,
                 ],
             ])
-            ->add('address_phone', 'text', [
+            ->add(self::FIELD_PHONE, 'text', [
                 'label' => false,
                 'required' => true,
                 'property_path' => 'address.phone',
@@ -93,16 +103,16 @@ class Payolution extends AbstractType
                     'tabindex' => 180 + $this->tabIndexOffset,
                 ],
             ])
-            ->add('language_iso2_code', 'hidden', [
+            ->add(self::FIELD_LANGUAGE_CODE, 'hidden', [
                 'data' => 'DE',
             ])
-            ->add('currency_iso3_code', 'hidden', [
+            ->add(self::FIELD_CURRENCY_CODE, 'hidden', [
                 'data' => 'EUR',
             ])
-            ->add('account_brand', 'hidden', [
+            ->add(self::FIELD_ACCOUNT_BRAND, 'hidden', [
                 'data' => PayolutionApiConstants::BRAND_INVOICE
             ])
-            ->add('client_ip', 'hidden', [
+            ->add(self::FIELD_CLIENT_IP, 'hidden', [
                 'data' => '127.0.0.1'
             ]);
     }
