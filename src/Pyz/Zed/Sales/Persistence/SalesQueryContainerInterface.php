bd2d1aecaab830054aedcdacbbfbbb5b231da87e
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-23 11:22:52 +0200

    add get orderItem function

--- src/Pyz/Zed/Sales/Persistence/SalesQueryContainerInterface.php
+++ src/Pyz/Zed/Sales/Persistence/SalesQueryContainerInterface.php
@@ -3,6 +3,7 @@
 namespace Pyz\Zed\Sales\Persistence;
 
 use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrderQuery;
+use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrderItemQuery;
 
 interface SalesQueryContainerInterface
 {
@@ -11,4 +12,10 @@ interface SalesQueryContainerInterface
      * @return SpySalesOrderQuery
      */
     public function querySalesOrderDetailsById($idSalesOrder);
+
+    /**
+     * @param $orderItemId
+     * @return SpySalesOrderItemQuery
+     */
+    public function querySalesOrderItemById($orderItemId);
 }
