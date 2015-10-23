962cc8cfea1da1db7e83848f3f0b34729781a80f
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-23 13:07:25 +0200

    add email addresses to config

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -327,4 +327,9 @@ $config[AfterbuyExportConstantInterface::AFTERBUY_PARTNER_ID] = 107450;
 $config[AfterbuyExportConstantInterface::AFTERBUY_PARTNER_PASS] = '6/UMU16Qn/=hCFhKZbqpr)8T0';
 $config[AfterbuyExportConstantInterface::AFTERBUY_USER_ID] = 'petsdelideutschland';
 $config[AfterbuyExportConstantInterface::AFTERBUY_URL] = 'https://api.afterbuy.de/afterbuy/ShopInterface.aspx?';
+$config[AfterbuyExportConstantInterface::AFTERBUY_EMAILS] = [
+    'ds@petsdeli.de',
+    'raed.marji@project-a.com'
+];
+
 
