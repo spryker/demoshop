28b6ad26fe89eaa013bf74084bae014196c97118
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-23 13:08:39 +0200

    add new functions to facade

--- src/Pyz/Zed/OrderExporter/Business/OrderExporterFacade.php
+++ src/Pyz/Zed/OrderExporter/Business/OrderExporterFacade.php
@@ -2,38 +2,61 @@
 
 namespace Pyz\Zed\OrderExporter\Business;
 
-use Generated\Shared\Transfer\OrderTransfer;
 use SprykerEngine\Zed\Kernel\Business\AbstractFacade;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Generated\Shared\Sales\OrderListInterface;
-use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrder;
+use SprykerFeature\Zed\Sales\Persistence\Propel\SpySalesOrder;
 
 /**
  * @method OrderExporterDependencyContainer getDependencyContainer()
  */
 class OrderExporterFacade extends AbstractFacade
 {
-
     /**
-     * @param SpySalesOrder $order
-     */
-    public function exportOrder(SpySalesOrder $order)
-    {
-        $this->getDependencyContainer()
-            ->createOrderExporterManager()
-            ->exportOrder($order);
-
-    }
-
-    /**
-     * @param int $saleOrderId
+     * @param $salesOrderId
      * @return SpySalesOrder
      */
-    public function getOrderBySalesOrderId($saleOrderId)
+    public function getSalesOrderById($salesOrderId)
     {
         return $this->getDependencyContainer()
             ->getSalesFacade()
-            ->getOrderBySalesOrderId($saleOrderId);
+            ->getSalesOrderById($salesOrderId);
+    }
+
+    /**
+     * @param SpySalesOrder[] $orderItems
+     */
+    public function exportOrderItems(array $orderItems)
+    {
+        $this->getDependencyContainer()
+            ->createOrderExporterManager()
+            ->exportOrderItems(
+                $orderItems,
+                $this->getSalesOrderById(
+                    $this->getOrderIdFromOrderItems($orderItems)
+                ));
+    }
+
+    /**
+     * @param $orderItemId
+     * @return SpySalesOrder
+     */
+    public function getOrderItemById($orderItemId)
+    {
+        return $this->getDependencyContainer()
+            ->getSalesFacade()
+            ->getOrderItemById($orderItemId);
+    }
+
+    /**
+     * @param array $orderItems
+     * @return int|null
+     * @throws \Exception
+     */
+    public function getOrderIdFromOrderItems(array $orderItems)
+    {
+        return $this->getDependencyContainer()
+            ->createOrderExporterManager()
+            ->getOrderIdFromOrderItems($orderItems);
     }
 }
