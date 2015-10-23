c59463a8fce0e280abd24b066674dd909ec62bc6
Stephan Schulze <stephan.schulze@project-a.com>
2015-10-23 16:33:28 +0200

    the sequence numbers are not correctly initialized in the spryker core
    the random number generator relies on the min/max settings in the $sequenceNumberSettingsTransfer object
    which are not initialized correctly
    
    we set them to increase by 1 for customers AND orders

--- src/Pyz/Zed/Sales/SalesConfig.php
+++ src/Pyz/Zed/Sales/SalesConfig.php
@@ -3,6 +3,9 @@
 namespace Pyz\Zed\Sales;
 
 use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Transfer\SequenceNumberSettingsTransfer;
+use SprykerEngine\Shared\Kernel\Store;
+use SprykerFeature\Shared\SequenceNumber\SequenceNumberConstants;
 use SprykerFeature\Zed\Sales\SalesConfig as SprykerFeatureSalesConfig;
 use Pyz\Zed\Sales\Business\ConstantsInterface\Orderprocess;
 use Generated\Shared\Transfer\ItemTransfer;
@@ -59,6 +62,29 @@ class SalesConfig extends SprykerFeatureSalesConfig
             default:
                 throw new \RuntimeException('Could not find any statemachine process for new order');
         }
+    }
+
+    /**
+     * @return SequenceNumberSettingsInterface
+     */
+    public function getOrderReferenceDefaults()
+    {
+        $sequenceNumberSettingsTransfer = new SequenceNumberSettingsTransfer();
+
+        $sequenceNumberSettingsTransfer->setName(self::NAME_ORDER_REFERENCE);
+
+        $sequenceNumberSettingsTransfer->setIncrementMinimum(1);
+        $sequenceNumberSettingsTransfer->setIncrementMaximum(1);
+        $sequenceNumberSettingsTransfer->setMinimumNumber(1);
+
+
+        $sequenceNumberPrefixParts = [];
+        $sequenceNumberPrefixParts[] = Store::getInstance()->getStoreName();
+        $sequenceNumberPrefixParts[] = $this->get(SequenceNumberConstants::ENVIRONMENT_PREFIX);
+        $prefix = implode($this->getUniqueIdentifierSeparator(), $sequenceNumberPrefixParts) . $this->getUniqueIdentifierSeparator();
+        $sequenceNumberSettingsTransfer->setPrefix($prefix);
+
+        return $sequenceNumberSettingsTransfer;
     }
 
 }
