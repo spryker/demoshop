87159a1c1fe556ebceb8568b046a343a46f1cd47
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-23 13:07:54 +0200

    update constuct

--- src/Pyz/Zed/OrderExporter/Business/Model/AfterbuyResponseWriter.php
+++ src/Pyz/Zed/OrderExporter/Business/Model/AfterbuyResponseWriter.php
@@ -2,21 +2,40 @@
 
 namespace Pyz\Zed\OrderExporter\Business\Model;
 
+use Generated\Shared\Transfer\AfterbuyResponseTransfer;
 use Pyz\Zed\OrderExporter\Persistence\Propel\PdAfterbuyResponse;
 
 class AfterbuyResponseWriter implements AfterbuyResponseWriterInterface
 {
     /**
+     * @var MailSenderInterface
+     */
+    protected $mailSender;
+
+    /**
+     * @param MailSenderInterface $mailSender
+     */
+    public function __construct(MailSenderInterface $mailSender)
+    {
+        $this->mailSender = $mailSender;
+    }
+
+
+    /**
+     * @param AfterbuyResponseTransfer $afterbuyTransfer
      * @param $afterbuyResponse
      * @param $orderId
      * @return PdAfterbuyResponse
      * @throws \Propel\Runtime\Exception\PropelException
      */
-    public function createAfterbuyResponse($afterbuyResponse, $orderId)
+    public function createAfterbuyResponse(AfterbuyResponseTransfer $afterbuyTransfer, $afterbuyResponse, $orderId)
     {
         $afterbuyResponseEntity = new PdAfterbuyResponse();
-        $afterbuyResponseEntity->setFullResponse(json_encode($afterbuyResponse));
-        $afterbuyResponseEntity->setFkOrder($orderId);
+        $afterbuyResponseEntity
+            ->setFullResponse(json_encode($afterbuyResponse))
+            ->setFkOrder($orderId)
+            ->setRequest($afterbuyTransfer->getRequest())
+            ->setHttpStatusCode($afterbuyTransfer->getHttpStatusCode());
 
         if (array_key_exists('success', $afterbuyResponse)) {
             $afterbuyResponseEntity->setSuccess($afterbuyResponse['success']);
@@ -26,6 +45,8 @@ class AfterbuyResponseWriter implements AfterbuyResponseWriterInterface
             $afterbuyResponseEntity->setErrorsList(json_encode($afterbuyResponse['errorlist']));
         }
         $afterbuyResponseEntity->save();
+
+        $this->mailSender->sendAfterbuyResultMail($afterbuyResponseEntity);
 
         return $afterbuyResponseEntity;
     }
