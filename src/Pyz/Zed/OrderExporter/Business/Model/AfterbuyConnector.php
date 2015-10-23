ba4a19e1116280ceaa94bfe823fbc6485197b5a9
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-23 13:11:22 +0200

    cosmetic changes

--- src/Pyz/Zed/OrderExporter/Business/Model/AfterbuyConnector.php
+++ src/Pyz/Zed/OrderExporter/Business/Model/AfterbuyConnector.php
@@ -2,6 +2,7 @@
 
 namespace Pyz\Zed\OrderExporter\Business\Model;
 
+use Generated\Shared\Transfer\AfterbuyResponseTransfer;
 use Pyz\Zed\OrderExporter\OrderExporterConfig;
 
 class AfterbuyConnector implements AfterbuyConnectorInterface
@@ -10,22 +11,34 @@ class AfterbuyConnector implements AfterbuyConnectorInterface
      * @var string
      */
     protected $afterbuyUrl;
+
+    /**
+     * @var AfterbuyResponseWriterInterface
+     */
+    protected $afterbuyResponseWriter;
+
     /**
      * @param OrderExporterConfig $orderExporterConfig
+     * @param AfterbuyResponseWriterInterface $afterbuyResponseWriter
      */
-    public function __construct(OrderExporterConfig $orderExporterConfig)
-    {
+    public function __construct(
+        OrderExporterConfig $orderExporterConfig,
+        AfterbuyResponseWriterInterface $afterbuyResponseWriter
+    ){
         $this->afterbuyUrl = $orderExporterConfig->getAfterbuyUrl();
+        $this->afterbuyResponseWriter = $afterbuyResponseWriter;
     }
 
     /**
-     * @param array $postVariables
-     * @return mixed
+     * @param $postVariables
+     * @param $orderId
      */
-    public function sendToAfterBuy(array $postVariables)
+    public function sendToAfterBuy($postVariables, $orderId)
     {
-        die('do not send for test');
+        $afterbuyTransfer = new AfterbuyResponseTransfer();
         $connexion = curl_init();
+
+        curl_setopt($connexion, CURLOPT_TIMEOUT, 120);
         curl_setopt($connexion, CURLOPT_URL, "$this->afterbuyUrl");
         curl_setopt($connexion, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($connexion, CURLOPT_SSL_VERIFYHOST, 0);
@@ -34,17 +47,24 @@ class AfterbuyConnector implements AfterbuyConnectorInterface
         curl_setopt($connexion, CURLOPT_POSTFIELDS, $postVariables);
 
         $sendingResponse = curl_exec($connexion);
+        $afterbuyTransfer->setHttpStatusCode(curl_getinfo($connexion, CURLINFO_HTTP_CODE));
         curl_close($connexion);
 
-        return $resultConnexion;
+        $afterbuyTransfer->setRequest($postVariables);
+
+        $sendingResponse = $this->xmlParser($sendingResponse);
+        $this->afterbuyResponseWriter->createAfterbuyResponse($afterbuyTransfer, $sendingResponse, $orderId);
     }
 
-
+    /**
+     * @param $sendingResponse
+     * @return array
+     */
     public function xmlParser($sendingResponse)
     {
         $sendingResponse = simplexml_load_string($sendingResponse);
-        $sendingResponse = (array) $sendingResponse;
 
-
+        return (array) $sendingResponse;
     }
+
 }
