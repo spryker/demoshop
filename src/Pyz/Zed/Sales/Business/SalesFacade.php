c764266bad80d891473e6f4cd5081801bcbdcfc8
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-10-29 11:23:07 +0100

    create salesToCheckout connector

--- src/Pyz/Zed/Sales/Business/SalesFacade.php
+++ src/Pyz/Zed/Sales/Business/SalesFacade.php
@@ -4,13 +4,14 @@ namespace Pyz\Zed\Sales\Business;
 
 use Pyz\Zed\OrderExporter\Dependency\Facade\OrderExporterToSalesInterface;
 use SprykerFeature\Zed\Sales\Business\SalesFacade as SprykerSalesFacade;
-use SprykerFeature\Zed\SalesCheckoutConnector\Dependency\Facade\SalesCheckoutConnectorToSalesInterface;
+use SprykerFeature\Zed\SalesCheckoutConnector\Dependency\Facade\SalesCheckoutConnectorToSalesInterface as SpySalesCheckoutConnectorToSalesInterface;
+use Pyz\Zed\SalesCheckoutConnector\Dependency\Facade\SalesCheckoutConnectorToSalesInterface;
 use SprykerFeature\Zed\Sales\Persistence\Propel\Base\SpySalesOrderItem;
 
 /**
  * @method SalesDependencyContainer getDependencyContainer()
  */
-class SalesFacade extends SprykerSalesFacade implements SalesCheckoutConnectorToSalesInterface, OrderExporterToSalesInterface
+class SalesFacade extends SprykerSalesFacade implements SpySalesCheckoutConnectorToSalesInterface, OrderExporterToSalesInterface, SalesCheckoutConnectorToSalesInterface
 {
     /**
      * @param int $orderItemId
