4fc3883fd43932dfc5ef9bcdc48ebeecfdd906f1
Christian Höpfner <christian.hoepfner@project-a.com>
2015-10-30 18:11:21 +0100

    Add localstorage backup of form data, remove duplicate line in Yves gulpfile

--- static/assets/Yves/styleguide/assets/js/sg-main.js
+++ static/assets/Yves/styleguide/assets/js/sg-main.js
@@ -1,6 +1,7 @@
 import './modules/faqCard';
 //import './modules/faqSearch';
 import './modules/faqSwitch';
+import './modules/formBackup';
 import './modules/headerMenu';
 import './modules/navBar';
 import './modules/productFilter';
