6448344f90688afa3d18e172dec52c2ec79c2bfd
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-03 11:20:05 +0100

    divide afterbuy in two tables

---
+++ src/Pyz/Zed/OrderExporter/Persistence/Propel/PdSalesOrderItemAfterbuyExport.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pyz\Zed\OrderExporter\Persistence\Propel;
+
+use Pyz\Zed\OrderExporter\Persistence\Propel\Base\PdSalesOrderItemAfterbuyExport as BasePdSalesOrderItemAfterbuyExport;
+
+/**
+ * Skeleton subclass for representing a row from the 'pd_sales_order_item_afterbuy_export' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class PdSalesOrderItemAfterbuyExport extends BasePdSalesOrderItemAfterbuyExport
+{
+
+}
---
+++ src/Pyz/Zed/OrderExporter/Persistence/Propel/PdSalesOrderItemAfterbuyExportQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Pyz\Zed\OrderExporter\Persistence\Propel;
+
+use Pyz\Zed\OrderExporter\Persistence\Propel\Base\PdSalesOrderItemAfterbuyExportQuery as BasePdSalesOrderItemAfterbuyExportQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'pd_sales_order_item_afterbuy_export' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class PdSalesOrderItemAfterbuyExportQuery extends BasePdSalesOrderItemAfterbuyExportQuery
+{
+
+}
