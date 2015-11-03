e0b65011750e759a0d4a5f8fc2b4d23d6d8423f6
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-03 18:09:52 +0100

    cosmetic change

--- src/Pyz/Zed/OrderExporter/Business/OrderExporterDependencyContainer.php
+++ src/Pyz/Zed/OrderExporter/Business/OrderExporterDependencyContainer.php
@@ -35,6 +35,7 @@ class OrderExporterDependencyContainer extends AbstractBusinessDependencyContain
             $this->getQueryContainer()
         );
     }
+
     /**
      * @return Model\AfterbuyExportManager
      */
