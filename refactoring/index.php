2e36f89b56afbac49ffafc4ee4a655e7e13a9186
René Klatt <rene.klatt@spryker.com>
2015-11-11 12:37:46 +0000

    TD-104 add id-method-parameter indentation fixer for schemas

--- refactoring/index.php
+++ refactoring/index.php
@@ -3,12 +3,14 @@
 namespace Spryker\Refactor;
 
 use Spryker\Refactor\Propel\AddNameToSchemaProperties;
+use Spryker\Refactor\Propel\FixIndentationOfIdMethodParameter;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
 $refactorer = new Refactor();
 
-$refactorer->addRefactorer(new AddNameToSchemaProperties());
+//$refactorer->addRefactorer(new AddNameToSchemaProperties());
+$refactorer->addRefactorer(new FixIndentationOfIdMethodParameter());
 
 $refactorer->run();
 
