16e5fa5bb16d6caac9885f788cf6c8c53fe7a932
Marius Bogdan <marius.rizac@spryker.com>
2015-11-11 12:45:39 +0100

    updated commands to run

--- setup-vm.sh
+++ setup-vm.sh
@@ -1,27 +1,51 @@
 #!/bin/bash
 
-echo 'Installing Composer Dependencies'
+PHP=`which php`
+CURL=`which curl`
+NPM=`which npm`
 
-[ ! -f composer.phar ] && curl -sS https://getcomposer.org/installer | php
-php composer.phar selfupdate
-php composer.phar install
+if [[ ! -f "composer.phar" ]]; then
+    echo "-> Download composer.phar"
+    $CURL -sS https://getcomposer.org/installer | $PHP
+fi
 
-echo 'Running build-class-map'
+echo "-> Install Composer Dependencies (a github token will be required. Read screen instructions)"
+$PHP composer.phar selfupdate
+$PHP composer.phar install
 
+NODEJS_VERSION=`node -v`
+
+if [[ ! "$NODEJS_VERSION" =~ "[^5]" ]]; then
+    echo "+ Upgrade node js"
+    sudo $NPM cache clean -f
+    sudo $NPM install -g n
+    sudo n stable
+
+    echo "=== NodeJS updated to version `node -v` ==="
+    echo "=== NPM updated to version `$NPM -v`    ==="
+
+    echo "-> Install Gulp Globally"
+    sudo $NPM install -g gulp
+
+    if [[ -d "node_modules" ]]; then
+        echo "-> Remove node_modules and reinstall npm packages"
+        rm -rf ./node_modules/
+        $NPM install
+    fi
+fi
+
+
+echo "-> Build-class-map"
 vendor/bin/build-class-map
 
-echo 'Running setup:install'
-
+echo "-> setup:install"
 vendor/bin/console setup:install -vvv
 
-echo 'Running setup:install-demo-data'
-
+echo "-> setup:install-demo-data"
 vendor/bin/console setup:install-demo-data -vvv
 
-echo 'Running collector:search:export'
-
+echo "-> collector:search:export"
 vendor/bin/console collector:search:export -vvv
 
-echo 'Running collector:storage:export'
-
+echo "-> collector:storage:export"
 vendor/bin/console collector:storage:export -vvv
