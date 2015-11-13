549db941170f2087bfa1d65167c182c101645ea3
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-13 16:41:54 +0100

    divide get expense and get shipment method

--- src/Pyz/Zed/Shipment/Business/ShipmentFacade.php
+++ src/Pyz/Zed/Shipment/Business/ShipmentFacade.php
@@ -2,13 +2,16 @@
 
 namespace Pyz\Zed\Shipment\Business;
 
+use Orm\Zed\Shipment\Persistence\SpyShipmentMethod;
+use Pyz\Zed\ShipmentCheckoutConnector\Dependency\Facade\ShipmentCheckoutConnectorToShipmentInterface;
 use SprykerEngine\Shared\Kernel\Messenger\MessengerInterface;
 use SprykerFeature\Zed\Shipment\Business\ShipmentFacade as SprykerShipmentFacade;
+use Generated\Shared\Transfer\ExpenseTransfer;
 
 /**
  * @method ShipmentDependencyContainer getDependencyContainer()
  */
-class ShipmentFacade extends SprykerShipmentFacade
+class ShipmentFacade extends SprykerShipmentFacade implements ShipmentCheckoutConnectorToShipmentInterface
 {
 
     /**
@@ -21,12 +24,23 @@ class ShipmentFacade extends SprykerShipmentFacade
 
     /**
      * @param $countryId
-     * @return \Generated\Shared\Transfer\ExpenseTransfer
+     * @return SpyShipmentMethod
      */
     public function getShipmentMethodByCountryId($countryId)
     {
         return $this->getDependencyContainer()
             ->createShipmentMethodManager()
             ->getShipmentMethod($countryId);
+    }
+
+    /**
+     * @param SpyShipmentMethod $shipmentMethod
+     * @return ExpenseTransfer
+     */
+    public function getShipmentMethodAsCartExpense(SpyShipmentMethod $shipmentMethod)
+    {
+        return $this->getDependencyContainer()
+            ->createShipmentMethodManager()
+            ->getShipmentMethodAsCartExpenseTransfer($shipmentMethod);
     }
 }
