5c74f477ccdb63add0d8300ab23470eb344f2eec
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-13 16:44:00 +0100

    add docbloc

--- src/Pyz/Zed/Cart/Dependency/Plugin/OrderShipmentMethodInterface.php
+++ src/Pyz/Zed/Cart/Dependency/Plugin/OrderShipmentMethodInterface.php
@@ -2,7 +2,13 @@
 
 namespace Pyz\Zed\Cart\Dependency\Plugin;
 
+use Generated\Shared\Transfer\ExpenseTransfer;
+
 interface OrderShipmentMethodInterface
 {
+    /**
+     * @param int $countryId
+     * @return ExpenseTransfer
+     */
     public function getShipmentMethodByCountry($countryId);
 }
