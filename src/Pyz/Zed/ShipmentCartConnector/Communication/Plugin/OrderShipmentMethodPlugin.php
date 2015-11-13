1edc3f99078c9fc69b11e09408161bdc32aecccc
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-13 16:43:53 +0100

    add docbloc

--- src/Pyz/Zed/ShipmentCartConnector/Communication/Plugin/OrderShipmentMethodPlugin.php
+++ src/Pyz/Zed/ShipmentCartConnector/Communication/Plugin/OrderShipmentMethodPlugin.php
@@ -5,15 +5,21 @@ namespace Pyz\Zed\ShipmentCartConnector\Communication\Plugin;
 use Pyz\Zed\Cart\Dependency\Plugin\OrderShipmentMethodInterface;
 use SprykerEngine\Zed\Kernel\Communication\AbstractPlugin;
 use Pyz\Zed\ShipmentCartConnector\Business\ShipmentCartConnectorFacade;
+use Generated\Shared\Transfer\ExpenseTransfer;
 
 /**
  * @method ShipmentCartConnectorFacade getFacade()
  */
 class OrderShipmentMethodPlugin extends AbstractPlugin implements OrderShipmentMethodInterface
 {
-
+    /**
+     * @param int $countryId
+     * @return ExpenseTransfer
+     */
     public function getShipmentMethodByCountry($countryId)
     {
-        return $this->getFacade()->getShipmentMethodByCountryId($countryId);
+        $shipmentMethod =  $this->getFacade()->getShipmentMethodByCountryId($countryId);
+
+        return $this->getFacade()->getShipmentMethodAsCartExpense($shipmentMethod);
     }
 }
