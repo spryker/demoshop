d19a6e69863fc2d93cc79d5728b6ba982e013d51
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-17 15:52:45 +0100

    add concrete writer

--- src/Pyz/Zed/PetsDeliImporterWriter/Business/WriterProvider/PetsDeliWriterProvider.php
+++ src/Pyz/Zed/PetsDeliImporterWriter/Business/WriterProvider/PetsDeliWriterProvider.php
@@ -6,24 +6,39 @@ use PavFeature\Zed\ProductDynamicImporter\Business\Writer\ProductWriterInterface
 use PavFeature\Zed\ProductDynamicImporter\Business\Writer\ProductWriterProviderInterface;
 use Pyz\Zed\PetsDeliImporterWriter\Business\Writer\AbstractProductDynamicWriter;
 use Pyz\Zed\PetsDeliImporterWriter\Business\Writer\AbstractProductWriter;
+use Pyz\Zed\PetsDeliImporterWriter\Business\Writer\ConcreteBundleProductWriter;
 use Pyz\Zed\PetsDeliImporterWriter\Business\Writer\ConcreteProductWriter;
 
 class PetsDeliWriterProvider implements ProductWriterProviderInterface
 {
+    /**
+     * @var AbstractProductWriter
+     */
     protected $abstractProductWriter;
+    /**
+     * @var ConcreteProductWriter
+     */
     protected $concreteProductWriter;
+    /**
+     * @var AbstractProductDynamicWriter
+     */
     protected $abstractProductDynamicWriter;
+    /**
+     * @var ConcreteBundleProductWriter
+     */
+    protected $concreteBundleProductWriter;
 
 
     public function __construct(
         AbstractProductWriter $abstractProductWriter,
         ConcreteProductWriter $concreteProductWriter,
-        AbstractProductDynamicWriter $abstractProductDynamicWriter
+        AbstractProductDynamicWriter $abstractProductDynamicWriter,
+        ConcreteBundleProductWriter $concreteBundleProductWriter
     ) {
         $this->abstractProductWriter = $abstractProductWriter;
         $this->concreteProductWriter = $concreteProductWriter;
         $this->abstractProductDynamicWriter = $abstractProductDynamicWriter;
-
+        $this->concreteBundleProductWriter = $concreteBundleProductWriter;
     }
 
     /**
@@ -31,13 +46,11 @@ class PetsDeliWriterProvider implements ProductWriterProviderInterface
      */
     public function getWriter()
     {
-
         return [
             $this->abstractProductWriter,
             $this->concreteProductWriter,
             $this->abstractProductDynamicWriter,
+            $this->concreteBundleProductWriter
         ];
-
-
     }
 }
