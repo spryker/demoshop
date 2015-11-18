5a8a2a43f2807f51fef00f7033deadd65148f90f
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-18 19:04:13 +0100

    add docblock

--- src/Pyz/Zed/Customer/Business/Model/MagentoPasswordManagerInterface.php
+++ src/Pyz/Zed/Customer/Business/Model/MagentoPasswordManagerInterface.php
@@ -6,7 +6,15 @@ use Generated\Shared\Transfer\CustomerTransfer;
 
 interface MagentoPasswordManagerInterface
 {
+    /**
+     * @param CustomerTransfer $customerTransfer
+     * @return bool
+     */
     public function checkMagentoPassword(CustomerTransfer $customerTransfer);
 
+    /**
+     * @param CustomerTransfer $customerTransfer
+     * @return bool
+     */
     public function hasMagentoPassword(CustomerTransfer $customerTransfer);
 }
