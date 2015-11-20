f366d80acdf3072d99ae3431845ed61a89a5d831
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-20 14:50:49 +0100

    add customer model to DC

--- src/Pyz/Zed/Customer/Business/CustomerDependencyContainer.php
+++ src/Pyz/Zed/Customer/Business/CustomerDependencyContainer.php
@@ -2,24 +2,36 @@
 
 namespace Pyz\Zed\Customer\Business;
 
+use SprykerFeature\Zed\Customer\Business\CustomerDependencyContainer as SpyCustomerDependencyContainer;
+use Generated\Zed\Ide\FactoryAutoCompletion\CustomerBusiness;
+use Pyz\Zed\Customer\Persistence\CustomerQueryContainerInterface;
+use Pyz\Zed\Customer\Business\Model\MagentoPasswordManagerInterface;
+use Pyz\Zed\Customer\Business\Customer\CustomerInterface;
 use Pyz\Zed\Customer\Business\Customer\Customer;
 use SprykerFeature\Zed\Customer\Business\CustomerDependencyContainer as SprykerFeatureCustomerDependencyContainer;
 
 /**
- * @method Customer createCustomer()
  * @method CustomerBusiness getFactory()
  * @method CustomerQueryContainerInterface getQueryContainer()
  */
-class CustomerDependencyContainer extends SprykerFeatureCustomerDependencyContainer
+class CustomerDependencyContainer extends SpyCustomerDependencyContainer
 {
-    
     /**
-     * @return Model\MagentoPasswordManager
+     * @return MagentoPasswordManagerInterface
      */
     public function createMagentoPasswordManager()
     {
         return $this->getFactory()->createModelMagentoPasswordManager(
+            $this->createCustomerModel(),
             $this->getQueryContainer()
         );
+    }
+
+    /**
+     * @return CustomerInterface
+     */
+    public function createCustomerModel()
+    {
+        return parent::createCustomer();
     }
 }
