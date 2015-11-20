146b5ed3217ee99db7d68f9d3e30f6174b0bf618
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-20 14:53:27 +0100

    verify password

--- src/Pyz/Zed/Customer/Business/Model/MagentoPasswordManager.php
+++ src/Pyz/Zed/Customer/Business/Model/MagentoPasswordManager.php
@@ -2,19 +2,134 @@
 
 namespace Pyz\Zed\Customer\Business\Model;
 
+use Generated\Shared\Customer\CustomerMagentoPasswordMigrationInterface;
 use Generated\Shared\Transfer\CustomerTransfer;
+use Pyz\Zed\Customer\Business\Customer\CustomerInterface;
+use Pyz\Zed\Customer\Persistence\CustomerQueryContainerInterface;
+use Orm\Zed\Customer\Persistence\SpyCustomer;
 
 class MagentoPasswordManager implements MagentoPasswordManagerInterface
 {
+    /**
+     * @var CustomerInterface
+     */
+    protected $customerInterface;
 
-    public function checkMagentoPassword(CustomerTransfer $customerTransfer)
-    {
+    /**
+     * @var CustomerQueryContainerInterface
+     */
+    protected $queryContainer;
 
+    /**
+     * @param CustomerInterface $customerInterface
+     * @param CustomerQueryContainerInterface $queryContainer
+     */
+    public function __construct(
+        CustomerInterface $customerInterface,
+        CustomerQueryContainerInterface $queryContainer
+    ){
+        $this->customerInterface = $customerInterface;
+        $this->queryContainer = $queryContainer;
     }
 
-    public function hasMagentoPassword(CustomerTransfer $customerTransfer)
-    {
+    //TODO CHECK IF EMAIL / CUSTOMER EXIST BEFORE TO CHECK MAGENTO
 
+    /**
+     * @param CustomerMagentoPasswordMigrationInterface $customerTransfer
+     * @return bool
+     */
+    public function migratePassword(CustomerMagentoPasswordMigrationInterface $customerTransfer)
+    {
+        $customerEntity = $this->findCustomerByEmail($customerTransfer->getEmail());
+
+        $password = $customerTransfer->getPassword();
+        $hash = $customerEntity->getMagentoPasswordHash();
+
+        $isPasswordValid = $this->isPasswordValid($password, $hash);
+
+        if ($isPasswordValid) {
+            $this->writeMagentoPasswordAsCustomerPassword($customerEntity, $password);
+            $this->deleteMagentoPassword($customerEntity);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param SpyCustomer $customerEntity
+     * @param string $password
+     * @throws \Propel\Runtime\Exception\PropelException
+     */
+    protected function writeMagentoPasswordAsCustomerPassword(SpyCustomer $customerEntity, $password)
+    {
+        $updatedCustomerTransfer = new CustomerTransfer();
+        $updatedCustomerTransfer->setPassword($password);
+
+        $updatedCustomerTransfer = $this->customerInterface->encryptPassword($updatedCustomerTransfer);
+        $customerEntity
+            ->setPassword($updatedCustomerTransfer->getPassword())
+            ->save();
+    }
+
+    /**
+     * @param CustomerMagentoPasswordMigrationInterface $customerTransfer
+     * @return bool
+     */
+    public function hasMagentoPassword(CustomerMagentoPasswordMigrationInterface $customerTransfer)
+    {
+        $customerEntity = $this->findCustomerByEmail($customerTransfer->getEmail());
+
+        return (bool) (!$customerEntity->getMagentoPasswordHash() == null);
+    }
+
+    /**
+     * @param SpyCustomer $customerEntity
+     * @throws \Propel\Runtime\Exception\PropelException
+     */
+    public function deleteMagentoPassword(SpyCustomer $customerEntity)
+    {
+        $customerEntity
+            ->setMagentoPasswordHash(null)
+            ->save();
+    }
+
+    /**
+     * @param string $email
+     * @return SpyCustomer
+     */
+    protected function findCustomerByEmail($email)
+    {
+        return $this->queryContainer->queryCustomerByEmail($email)
+            ->findOne();
+    }
+
+    /**
+     * @param string $password
+     * @param string $hash
+     * @return bool
+     */
+    protected function isPasswordValid($password, $hash)
+    {
+        $hashArr = explode(':', $hash);
+        switch (count($hashArr)) {
+            case 1:
+                return $this->cryptPassword($password)  === $hash;
+            case 2:
+                return $this->cryptPassword($hashArr[1] . $password) === $hashArr[0];
+        }
+        return false;
+    }
+
+
+    /**
+     * @param string $data
+     * @return string
+     */
+    protected function cryptPassword($data)
+    {
+        return md5($data);
     }
 
 }
