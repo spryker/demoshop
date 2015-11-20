74967deec28c29648b7199c04b3deebdd42c901a
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-20 14:51:33 +0100

    change parameter

--- src/Pyz/Client/Customer/Service/Zed/CustomerStub.php
+++ src/Pyz/Client/Customer/Service/Zed/CustomerStub.php
@@ -2,6 +2,9 @@
 
 namespace Pyz\Client\Customer\Service\Zed;
 
+use Generated\Shared\Customer\CustomerInterface;
+use Generated\Shared\Customer\CustomerMagentoPasswordMigrationInterface;
+use SprykerFeature\Client\Customer\Service\Zed\CustomerStub as SpyCustomerStub;
 
 use Generated\Shared\Customer\CustomerInfoInterface;
 use Generated\Shared\Customer\CustomerLoginResultInterface;
@@ -20,11 +23,11 @@ class CustomerStub extends SprykerFeatureCustomerStub implements CustomerStubInt
         return $this->zedStub->call('/customer/gateway/customer-login-result', $customerLoginResultTransfer);
     }
     /**
-     * @param ChangeTransfer $changeTransfer
+     * @param CustomerInterface $customerInterface
      * @return \SprykerEngine\Shared\Transfer\TransferInterface
      */
-    public function migrateMagentoPassword(ChangeTransfer $changeTransfer)
+    public function migrateMagentoPassword(CustomerMagentoPasswordMigrationInterface $customerInterface)
     {
-        return $this->zedStub->call('/customer/gateway/migrate-magento-password', $changeTransfer);
+        return $this->zedStub->call('/customer/gateway/migrate-magento-password', $customerInterface);
     }
 }
