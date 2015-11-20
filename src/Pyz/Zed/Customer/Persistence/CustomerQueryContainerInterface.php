6f8fb30f2f6a379833433c1adb6c15579eff4524
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-20 14:51:19 +0100

    add function to QC

--- src/Pyz/Zed/Customer/Persistence/CustomerQueryContainerInterface.php
+++ src/Pyz/Zed/Customer/Persistence/CustomerQueryContainerInterface.php
@@ -2,7 +2,13 @@
 
 namespace Pyz\Zed\Customer\Persistence;
 
+use Orm\Zed\Customer\Persistence\SpyCustomerQuery;
+
 interface CustomerQueryContainerInterface
 {
-
+    /**
+     * @param $email
+     * @return SpyCustomerQuery
+     */
+    public function queryCustomerByEmail($email);
 }
