da7ab606f0f881c78a4090b26926b6fe1f16e95a
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-23 17:31:07 +0100

    fix use after rebase merge conflicts

--- src/Pyz/Client/Customer/Service/CustomerDependencyContainer.php
+++ src/Pyz/Client/Customer/Service/CustomerDependencyContainer.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace Pyz\Client\Customer\Service;
+
 use Generated\Client\Ide\FactoryAutoCompletion\CustomerService;
 use Pyz\Client\Customer\Service\Session\CustomerSessionInterface;
 use Pyz\Client\Customer\Service\Zed\CustomerStubInterface;
+use SprykerFeature\Client\Customer\CustomerDependencyProvider;
 use SprykerFeature\Client\Customer\Service\CustomerDependencyContainer as SprykerFeatureCustomerDependencyContainer;
 
 /**
