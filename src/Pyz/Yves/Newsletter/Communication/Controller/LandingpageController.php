09797a92aedf00fd8947362d09d9dd90d949e377
rgex <124898@supinfo.com>
2015-11-24 11:53:07 +0100

    created Newsletter bundle in Yves

---
+++ src/Pyz/Yves/Newsletter/Communication/Controller/LandingpageController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pyz\Yves\Newsletter\Communication\Controller;
+
+use SprykerEngine\Yves\Application\Communication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class LandingpageController extends AbstractController
+{
+    public function indexAction(Request $request)
+    {
+
+    }
+}
---
+++ src/Pyz/Yves/Newsletter/Communication/Controller/SubscriptionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pyz\Yves\Newsletter\Communication\Controller;
+
+use SprykerEngine\Yves\Application\Communication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class SubscriptionController extends AbstractController
+{
+    public function indexAction(Request $request)
+    {
+
+    }
+}
