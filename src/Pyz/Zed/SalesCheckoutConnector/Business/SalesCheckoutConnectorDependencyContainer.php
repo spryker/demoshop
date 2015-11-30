cb96672f3bde902faa38420d4ab292bbda9366e8
tobias.rosenstock <tobias.rosenstock@project-a.com>
2015-11-30 15:38:30 +0100

    fixed dependency container

--- src/Pyz/Zed/SalesCheckoutConnector/Business/SalesCheckoutConnectorDependencyContainer.php
+++ src/Pyz/Zed/SalesCheckoutConnector/Business/SalesCheckoutConnectorDependencyContainer.php
@@ -5,12 +5,12 @@ namespace Pyz\Zed\SalesCheckoutConnector\Business;
 use Generated\Zed\Ide\FactoryAutoCompletion\SalesCheckoutConnectorBusiness;
 use Pyz\Zed\SalesCheckoutConnector\SalesCheckoutConnectorDependencyProvider;
 use Pyz\Zed\SalesCheckoutConnector\Business\Model\LinkCustomerToOrderInterface;
-use SprykerFeature\Zed\SalesCheckoutConnector\Business\SalesCheckoutConnectorDependencyContainer as SprykerSalesCheckoutConnectorDependencyContainer;
+use PavFeature\Zed\SalesCheckoutConnector\Business\SalesCheckoutConnectorDependencyContainer as PavFeatureSalesCheckoutConnectorDependencyContainer;
 
 /**
  * @method SalesCheckoutConnectorBusiness getFactory()
  */
-class SalesCheckoutConnectorDependencyContainer extends SprykerSalesCheckoutConnectorDependencyContainer
+class SalesCheckoutConnectorDependencyContainer extends PavFeatureSalesCheckoutConnectorDependencyContainer
 {
     /**
      * @return LinkCustomerToOrderInterface
