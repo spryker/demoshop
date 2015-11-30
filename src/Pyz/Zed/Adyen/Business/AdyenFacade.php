fa46ee7866dc3f98ccb35b79b6eab662bedfe4bd
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-11-30 11:58:52 +0100

    implement facade

--- src/Pyz/Zed/Adyen/Business/AdyenFacade.php
+++ src/Pyz/Zed/Adyen/Business/AdyenFacade.php
@@ -5,11 +5,12 @@ namespace Pyz\Zed\Adyen\Business;
 use PavFeature\Zed\Adyen\Business\AdyenFacade as PavAdyenFacade;
 use Pyz\Zed\Adyen\Business\AdyenDependencyContainer;
 use Orm\Zed\Payone\Persistence\SpyPaymentPayone;
+use Pyz\Zed\OrderExporter\Dependency\Facade\OrderExporterToAdyenFacade;
 
 /**
  * @method AdyenDependencyContainer getDependencyContainer()
  */
-class AdyenFacade extends PavAdyenFacade
+class AdyenFacade extends PavAdyenFacade implements OrderExporterToAdyenFacade
 {
     /**
      * @param int $salesOrderId
