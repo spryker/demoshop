8b802fa10f19f02badbb8c8615b3eb1cf8c55765
Jan Moritz <124898@supinfo.com>
2015-12-02 16:26:59 +0000

    relaced parent by

--- src/Pyz/Zed/OmsMailQueueConnector/Business/Sender/PrePaymentOrderReceivedMailQueueSender.php
+++ src/Pyz/Zed/OmsMailQueueConnector/Business/Sender/PrePaymentOrderReceivedMailQueueSender.php
@@ -37,7 +37,7 @@ class PrePaymentOrderReceivedMailQueueSender extends PavPrePaymentOrderReceivedM
      */
     public function getMailTransfer(SpySalesOrder $orderEntity)
     {
-        $mailTransfer = parent::getMailTransfer($orderEntity);
+        $mailTransfer = $this->getMailTransfer($orderEntity);
         /** @var PavPaymentAdyen $adyenPayment */
         $adyenPayment = $orderEntity->getPavPaymentAdyens()->getFirst();
         $psp = $adyenPayment->getPspReference();
