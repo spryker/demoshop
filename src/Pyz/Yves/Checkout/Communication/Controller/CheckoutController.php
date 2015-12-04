67d2d7703cb91bd8b58ad5b12822e06040c000b4
Hanna Tamoudi <hanna-eloise.tamoudi@project-a.com>
2015-12-04 19:42:17 +0100

    fix again addresses

--- src/Pyz/Yves/Checkout/Communication/Controller/CheckoutController.php
+++ src/Pyz/Yves/Checkout/Communication/Controller/CheckoutController.php
@@ -180,7 +180,7 @@ class CheckoutController extends AbstractController
     protected function setShippingAddress(CheckoutRequestTransfer $checkoutRequestTransfer)
     {
         $shippingAddressTransfer = $checkoutRequestTransfer->getShippingAddress();
-        if ($shippingAddressTransfer->getAddress1() === null) {
+        if ($shippingAddressTransfer === null) {
             $checkoutRequestTransfer->setShippingAddress($checkoutRequestTransfer->getBillingAddress());
         }
     }
