03cd3b329f06991e4c2ab44c73787ebb5a96cf10
Tamas Nyulas <tamas.nyulas@spryker.com>
2015-12-04 14:57:19 +0100

    341 Remove yves communication layer

--- refactoring/index.php
+++ refactoring/index.php
@@ -4,6 +4,7 @@ namespace Spryker\Refactor;
 
 use SprykerFeature\Zed\Development\Business\Refactor\RefactorRunner;
 use SprykerFeature\Zed\Development\Business\Refactor\Plugin\RefactorPluginInstantiation;
+use SprykerFeature\Zed\Development\Business\Refactor\Yves\RemoveCommunicationLayer;
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
@@ -14,4 +15,10 @@ $refactorer = new RefactorRunner();
 $refactorer->addRefactorer(new RefactorPluginInstantiation([
     __DIR__ . '/../src/Pyz/',
 ]));
+$refactorer->addRefactorer(new RemoveCommunicationLayer([
+    __DIR__ . '/../src/Pyz/Yves',
+    __DIR__ . '/../static/assets/Yves/',
+    __DIR__ . '/../static/public/Yves/',
+]));
+
 $refactorer->run();
