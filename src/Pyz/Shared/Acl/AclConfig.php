47d79c905078937b35f827fe1deaaa380af83d2d
Stephan Schulze <stephan.schulze@project-a.com>
2015-12-08 17:02:20 +0100

    user admin@spryker.com will only on development created

---
+++ src/Pyz/Shared/Acl/AclConfig.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Pyz\Shared\Acl;
+use SprykerFeature\Shared\Acl\AclConfig as SprykerAclConfig;
+
+interface AclConfig extends SprykerAclConfig
+{
+
+    const ACL_DEFAULT_USER = 'ACL_DEFAULT_USER';
+
+}
