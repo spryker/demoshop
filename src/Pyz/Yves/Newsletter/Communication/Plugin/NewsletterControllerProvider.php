6a42cadb8dcf8d366f7631e4c56a7c2b215ff8b6
rgex <124898@supinfo.com>
2015-12-10 11:31:05 +0100

    added newsletter unsubscription function

--- src/Pyz/Yves/Newsletter/Communication/Plugin/NewsletterControllerProvider.php
+++ src/Pyz/Yves/Newsletter/Communication/Plugin/NewsletterControllerProvider.php
@@ -10,6 +10,7 @@ class NewsletterControllerProvider extends YvesControllerProvider
 
     const NEWSLETTER_SUBSCRIPTION_ROUTE = 'NEWSLETTER_SUBSCRIPTION_ROUTE';
     const NEWSLETTER_LANDING_PAGE_ROUTE = 'NEWSLETTER_LANDING_PAGE_ROUTE';
+    const NEWSLETTER_UNSUBSCRIPTION_ROUTE = 'NEWSLETTER_UNSUBSCRIPTION_ROUTE';
 
     /**
      * @param Application $app
@@ -24,10 +25,18 @@ class NewsletterControllerProvider extends YvesControllerProvider
         )->method('POST');
 
         $this->createPostController(
-            '/newsletter/confirmation/{subscriber_key}', //please no trailing slash here
+            '/newsletter/confirmation/{subscriberKey}', //please no trailing slash here
             static::NEWSLETTER_LANDING_PAGE_ROUTE,
             'Newsletter',
             'Landingpage'
-        )->method('GET')->assert('subscriber_key', '[0-9A-Fa-f]+');
+        )->method('GET')->assert('subscriberKey', '[0-9A-Fa-f]+');
+
+        $this->createPostController(
+            '/newsletter/unsubscribe/{subscriberKey}/newsletter-type/{newsletterTypeName}', //please no trailing slash here
+            static::NEWSLETTER_UNSUBSCRIPTION_ROUTE,
+            'Newsletter',
+            'Unsubscription'
+        )->method('GET')->assert('subscriberKey', '[0-9A-Fa-f]+')
+        ->assert('newsletterTypeName','[A-Za-z0-9-_]+');
     }
 }
