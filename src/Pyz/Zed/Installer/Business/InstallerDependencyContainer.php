811d0cc54b4d138cf86c4f92e47b8109b55cbb86
mscherer <mark.scherer@spryker.com>
2015-12-15 16:08:02 +0100

    Fix namespace.

--- src/Pyz/Zed/Installer/Business/InstallerDependencyContainer.php
+++ src/Pyz/Zed/Installer/Business/InstallerDependencyContainer.php
@@ -2,7 +2,7 @@
 
 namespace Pyz\Zed\Glossary\Business;
 
-use SprykerFeature\Zed\Installer\Business\InstallerDependencyContainer as SprykerInstallerDependencyContainer;
+use Spryker\Zed\Installer\Business\InstallerDependencyContainer as SprykerInstallerDependencyContainer;
 
 class InstallerDependencyContainer extends SprykerInstallerDependencyContainer
 {
