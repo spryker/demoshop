33c6388607d184b5c622d9041a9e912f9ba5c51c
Alessandro Bellini <alessandro.bellini@spryker.com>
2016-01-04 19:01:39 +0100

    1002: yves demoshop theme new structure

---
+++ assets/demoshop-yves.spy.js
@@ -0,0 +1,48 @@
+/**
+ * 
+ * description_here
+ * @copyright: Spryker Systems GmbH
+ *
+ */
+
+'use strict';
+
+let path = require('path');
+let cwd = process.cwd();
+
+module.exports = {
+    extend: (config, options, spyRequire) => {
+    	let R = spyRequire('ramda');
+    	let webpack = spyRequire('webpack');
+    	let ExtractTextPlugin = spyRequire('extract-text-webpack-plugin');
+
+        var defaultThemeConfig = {
+        	module: {
+                loaders: [{
+                    test: /\.css(\?v=\d+\.\d+\.\d+)?$/i,
+                    loader: ExtractTextPlugin.extract('style', 'css')
+                }, {
+                    test: /\.scss$/,
+                    loader: ExtractTextPlugin.extract('style', 'css!sass')
+                }, {
+                    test: /\.(ttf|woff2?|eot)(\?v=\d+\.\d+\.\d+)?$/i,
+                    loader: 'file?name=/assets/demoshop/fonts/[name].[ext]'
+                }, {
+                    test: /\.(jpe?g|png|gif|svg)(\?v=\d+\.\d+\.\d+)?$/i,
+                    loader: 'file?name=/assets/demoshop/img/[name].[ext]',
+                }]
+            },
+            output: {
+                path: path.join(cwd, './public/Yves'),
+                filename: '/assets/demoshop/js/[name].js'
+            },
+            plugins: [
+                new ExtractTextPlugin('assets/demoshop/css/[name].css', {
+                    allChunks: true
+                })
+            ],
+        };
+
+        return R.merge(config, defaultThemeConfig);
+    }
+};
