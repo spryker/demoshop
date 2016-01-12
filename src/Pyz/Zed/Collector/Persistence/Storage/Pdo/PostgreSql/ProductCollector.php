6020d772648a24d1773de88129aa83a84315eb23
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-01-12 15:53:34 +0100

    SP-176 abstract_product -> product_abstract

--- src/Pyz/Zed/Collector/Persistence/Storage/Pdo/PostgreSql/ProductCollector.php
+++ src/Pyz/Zed/Collector/Persistence/Storage/Pdo/PostgreSql/ProductCollector.php
@@ -15,15 +15,15 @@ class ProductCollector extends AbstractPdoCollectorQuery
         $sql = '
 SELECT
   GROUP_CONCAT("spy_product"."sku") AS concrete_skus,
-  spy_abstract_product.id_abstract_product AS id_abstract_product,
-  spy_abstract_product.attributes AS abstract_attributes,
-  "spy_abstract_product_localized_attributes"."attributes" AS abstract_localized_attributes,
+  spy_product_abstract.id_product_abstract AS id_product_abstract,
+  spy_product_abstract.attributes AS abstract_attributes,
+  "spy_product_abstract_localized_attributes"."attributes" AS abstract_localized_attributes,
   GROUP_CONCAT("spy_product"."attributes") AS concrete_attributes,
   GROUP_CONCAT("spy_product_localized_attributes"."attributes") AS concrete_localized_attributes,
   GROUP_CONCAT(product_urls.url) AS product_urls,
-  "spy_abstract_product_localized_attributes"."name" AS abstract_name,
+  "spy_product_abstract_localized_attributes"."name" AS abstract_name,
   GROUP_CONCAT("spy_product_localized_attributes"."name") AS concrete_names,
-  spy_abstract_product.sku AS abstract_sku,
+  spy_product_abstract.sku AS abstract_sku,
   (SELECT SUM(v.quantity)
   FROM spy_stock_product v
   WHERE v.fk_product = spy_product.id_product) AS quantity,
@@ -52,27 +52,27 @@ SELECT
   "spy_touch"."id_touch" AS %s,
   "spy_touch"."item_id" AS %s
 FROM "spy_touch"
-  INNER JOIN spy_abstract_product ON ("spy_touch"."item_id" = spy_abstract_product.id_abstract_product)
+  INNER JOIN spy_product_abstract ON ("spy_touch"."item_id" = spy_product_abstract.id_product_abstract)
   LEFT JOIN spy_product
-    ON (spy_abstract_product.id_abstract_product = "spy_product"."fk_abstract_product" AND "spy_product"."is_active")
-  INNER JOIN spy_abstract_product_localized_attributes
-    ON (spy_abstract_product.id_abstract_product = "spy_abstract_product_localized_attributes"."fk_abstract_product")
-  INNER JOIN spy_locale ON ("spy_abstract_product_localized_attributes"."fk_locale" = spy_locale.id_locale)
+    ON (spy_product_abstract.id_product_abstract = "spy_product"."fk_product_abstract" AND "spy_product"."is_active")
+  INNER JOIN spy_product_abstract_localized_attributes
+    ON (spy_product_abstract.id_product_abstract = "spy_product_abstract_localized_attributes"."fk_product_abstract")
+  INNER JOIN spy_locale ON ("spy_product_abstract_localized_attributes"."fk_locale" = spy_locale.id_locale)
   LEFT JOIN spy_url product_urls
-    ON (spy_abstract_product.id_abstract_product = product_urls.fk_resource_abstract_product AND
+    ON (spy_product_abstract.id_product_abstract = product_urls.fk_resource_product_abstract AND
         product_urls.fk_locale = spy_locale.id_locale)
   INNER JOIN spy_product_localized_attributes
     ON ("spy_product"."id_product" = "spy_product_localized_attributes"."fk_product" AND
         "spy_product_localized_attributes"."fk_locale" = spy_locale.id_locale)
   LEFT JOIN spy_price_product abstract_price_table
-    ON (spy_abstract_product.id_abstract_product = abstract_price_table.fk_abstract_product)
+    ON (spy_product_abstract.id_product_abstract = abstract_price_table.fk_product_abstract)
   LEFT JOIN spy_price_product concrete_price_table
     ON ("spy_product"."id_product" = concrete_price_table.fk_product AND concrete_price_table.fk_price_type = 1)
   LEFT JOIN spy_price_type spy_price_type ON (concrete_price_table.fk_price_type = spy_price_type.id_price_type)
-  LEFT JOIN spy_tax_set ON (spy_abstract_product.fk_tax_set = spy_tax_set.id_tax_set)
+  LEFT JOIN spy_tax_set ON (spy_product_abstract.fk_tax_set = spy_tax_set.id_tax_set)
   LEFT JOIN spy_tax_set_tax ON (spy_tax_set.id_tax_set = spy_tax_set_tax.fk_tax_set)
   LEFT JOIN spy_tax_rate ON (spy_tax_set_tax.fk_tax_rate = spy_tax_rate.id_tax_rate)
-  LEFT JOIN spy_product_category ON ("spy_touch"."item_id" = spy_product_category.fk_abstract_product)
+  LEFT JOIN spy_product_category ON ("spy_touch"."item_id" = spy_product_category.fk_product_abstract)
   INNER JOIN spy_category_attribute ON (spy_product_category.fk_category = spy_category_attribute.fk_category)
   INNER JOIN spy_category_node ON (spy_product_category.fk_category = spy_category_node.fk_category)
   LEFT JOIN spy_url categoryUrls ON (spy_category_node.id_category_node = categoryUrls.fk_resource_categorynode AND
@@ -97,9 +97,9 @@ FROM "spy_touch"
 WHERE "spy_touch"."item_type" = :spy_touch_item_type AND "spy_touch"."item_event" = :spy_touch_item_event
     AND "spy_touch"."touched" >= :spy_touch_touched AND
       spy_locale.id_locale = :fk_locale AND spy_locale.is_active = true
-GROUP BY abstract_sku, abstract_sku, abstract_sku, spy_abstract_product.id_abstract_product,
-  spy_abstract_product.attributes, "spy_abstract_product_localized_attributes"."attributes",
-  "spy_abstract_product_localized_attributes"."name", abstract_price_table.price, spy_tax_set.name,
+GROUP BY abstract_sku, abstract_sku, abstract_sku, spy_product_abstract.id_product_abstract,
+  spy_product_abstract.attributes, "spy_product_abstract_localized_attributes"."attributes",
+  "spy_product_abstract_localized_attributes"."name", abstract_price_table.price, spy_tax_set.name,
   spy_category_attribute.name, spy_category_attribute.meta_title, spy_category_attribute.meta_description,
   spy_category_attribute.meta_keywords, spy_category_attribute.category_image_name,
   spy_category_closure_table.fk_category_node_descendant, spy_category_closure_table.depth,
