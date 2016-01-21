3929e7b62405bc3a612c8619ccd6bb7d3616be41
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-01-21 17:43:13 +0100

    SP-176 postgres migration files

---
+++ src/Orm/Propel/DE/Migration_pgsql/PropelMigration_1452090006.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * Data object containing the SQL and PHP code to migrate the database
+ * up to version 1452090006.
+ * Generated on 2016-01-06 14:20:06 by vagrant
+ */
+class PropelMigration_1452090006
+{
+    public $comment = '';
+
+    public function preUp($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postUp($manager)
+    {
+        // add the post-migration code here
+    }
+
+    public function preDown($manager)
+    {
+        // add the pre-migration code here
+    }
+
+    public function postDown($manager)
+    {
+        // add the post-migration code here
+    }
+
+    /**
+     * Get the SQL statements for the Up migration
+     *
+     * @return array list of the SQL strings to execute for the Up migration
+     *               the keys being the datasources
+     */
+    public function getUpSQL()
+    {
+        return array (
+  'zed' => '
+ALTER TABLE "spy_customer"
+
+  ADD "phone" VARCHAR(255) FIRST;
+',
+);
+    }
+
+    /**
+     * Get the SQL statements for the Down migration
+     *
+     * @return array list of the SQL strings to execute for the Down migration
+     *               the keys being the datasources
+     */
+    public function getDownSQL()
+    {
+        return array (
+  'zed' => '
+ALTER TABLE "spy_customer"
+
+  DROP "phone";
+',
+);
+    }
+
+}
