bb3142b4897721093736a7983547c43060e8eca6
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-01-25 15:55:07 +0100

    SP-176 updated navigation collector

--- src/Pyz/Zed/Collector/Business/Storage/NavigationCollector.php
+++ src/Pyz/Zed/Collector/Business/Storage/NavigationCollector.php
@@ -2,12 +2,41 @@
 
 namespace Pyz\Zed\Collector\Business\Storage;
 
+use Generated\Shared\Transfer\LocaleTransfer;
 use Spryker\Shared\Category\CategoryConstants;
 use Spryker\Zed\Collector\Business\Collector\KeyValue\AbstractKeyValuePdoCollector;
+use Spryker\Zed\Collector\Business\Exporter\Writer\KeyValue\TouchUpdaterSet;
 use Spryker\Zed\Collector\CollectorConfig;
 
 class NavigationCollector extends AbstractKeyValuePdoCollector
 {
+
+    /**
+     * @param array $collectedSet
+     * @param LocaleTransfer $locale
+     * @param TouchUpdaterSet $touchUpdaterSet
+     *
+     * @return array
+     */
+    protected function collectData(array $collectedSet, LocaleTransfer $locale, TouchUpdaterSet $touchUpdaterSet)
+    {
+        $setToExport = [];
+        $formattedCategoryNodes = [];
+        $touchKey = $this->collectKey('', $locale->getLocaleName(), []);
+        $touchKey = rtrim($touchKey, '.');
+
+        foreach ($collectedSet as $index => $collectedItemData) {
+            $formattedCategoryNodes[] = $this->formatCategoryNode($collectedItemData);
+            $touchUpdaterSet->add($touchKey, $collectedItemData[CollectorConfig::COLLECTOR_TOUCH_ID], [
+                CollectorConfig::COLLECTOR_STORAGE_KEY => $this->getCollectorStorageKeyId($collectedItemData),
+                CollectorConfig::COLLECTOR_SEARCH_KEY => $this->getCollectorSearchKeyId($collectedItemData),
+            ]);
+        }
+
+        $setToExport[$touchKey] = $formattedCategoryNodes;
+
+        return $setToExport;
+    }
 
     /**
      * @param string $touchKey
@@ -17,14 +46,7 @@ class NavigationCollector extends AbstractKeyValuePdoCollector
      */
     protected function collectItem($touchKey, array $collectItemData)
     {
-        return [
-            'node_id' => $collectItemData[CollectorConfig::COLLECTOR_RESOURCE_ID],
-            'name' => $collectItemData['name'],
-            'url' => $collectItemData['url'],
-            'image' => $collectItemData['category_image_name'],
-            'children' => [],
-            'parents' => [],
-        ];
+        return $this->formatCategoryNode($collectItemData);
     }
 
     /**
@@ -33,6 +55,62 @@ class NavigationCollector extends AbstractKeyValuePdoCollector
     protected function collectResourceType()
     {
         return CategoryConstants::RESOURCE_TYPE_NAVIGATION;
+    }
+
+    /**
+     * @param array $categoryNode
+     *
+     * @return array
+     */
+    protected function formatCategoryNode(array $categoryNode)
+    {
+        $categoryUrls = explode(',', $categoryNode['category_urls']);
+
+        return [
+            'node_id' => $categoryNode['node_id'],
+            'name' => $categoryNode['category_name'],
+            'url' => $categoryUrls[0],
+            'image' => $categoryNode['category_image_name'],
+            'children' => $this->explodeGroupedNodes(
+                $categoryNode,
+                'category_child_ids',
+                'category_child_names',
+                'category_child_urls'
+            ),
+            'parents' => $this->explodeGroupedNodes(
+                $categoryNode,
+                'category_parent_ids',
+                'category_parent_names',
+                'category_parent_urls'
+            ),
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @param string $idsField
+     * @param string $namesField
+     * @param string $urlsField
+     *
+     * @return array
+     */
+    public function explodeGroupedNodes(array $data, $idsField, $namesField, $urlsField)
+    {
+        if (!$data[$idsField]) {
+            return [];
+        }
+        $ids = explode(',', $data[$idsField]);
+        $names = explode(',', $data[$namesField]);
+        $urls = explode(',', $data[$urlsField]);
+        $nodes = [];
+
+        foreach ($ids as $key => $id) {
+            $nodes[$id]['node_id'] = $id;
+            $nodes[$id]['name'] = $names[$key];
+            $nodes[$id]['url'] = $urls[$key];
+        }
+
+        return $nodes;
     }
 
 }
