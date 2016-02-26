cc78b8d528f2e8ca70e3bb9e2896e06867dd8342
René Klatt <rene.klatt@spryker.com>
2016-02-26 10:11:31 +0000

    #1412 add file doc block block to all Pyz files

--- src/Pyz/Yves/Catalog/Business/Model/Router/CatalogDetailRouter.php
+++ src/Pyz/Yves/Catalog/Business/Model/Router/CatalogDetailRouter.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Yves\Catalog\Business\Model\Router;
 
 use Spryker\Client\Catalog\Model\Exception\ProductNotFoundException;
