f4f5c699bab255ccf8f70dbcb0f2e8827683a71b
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-02 20:57:36 +0100

    #449 collector plugins / exporter cleanups

--- src/Pyz/Zed/Collector/Communication/Plugin/TranslationCollectorStoragePlugin.php
+++ src/Pyz/Zed/Collector/Communication/Plugin/TranslationCollectorStoragePlugin.php
@@ -9,8 +9,11 @@ namespace Pyz\Zed\Collector\Communication\Plugin;
 
 use Generated\Shared\Transfer\LocaleTransfer;
 use Orm\Zed\Touch\Persistence\SpyTouchQuery;
+use Spryker\Zed\Collector\Business\Exporter\Writer\TouchUpdaterInterface;
+use Spryker\Zed\Collector\Business\Exporter\Writer\WriterInterface;
 use Spryker\Zed\Collector\Business\Model\BatchResultInterface;
 use Spryker\Zed\Collector\Communication\Plugin\AbstractCollectorPlugin;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @method \Pyz\Zed\Collector\Communication\CollectorCommunicationFactory getFactory()
@@ -23,16 +26,22 @@ class TranslationCollectorStoragePlugin extends AbstractCollectorPlugin
      * @param \Orm\Zed\Touch\Persistence\SpyTouchQuery $baseQuery
      * @param \Generated\Shared\Transfer\LocaleTransfer $locale
      * @param \Spryker\Zed\Collector\Business\Model\BatchResultInterface $result
+     * @param \Spryker\Zed\Collector\Business\Exporter\Writer\WriterInterface $dataWriter
+     * @param \Spryker\Zed\Collector\Business\Exporter\Writer\TouchUpdaterInterface $touchUpdater
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
      *
      * @return void
      */
     public function run(
         SpyTouchQuery $baseQuery,
         LocaleTransfer $locale,
-        BatchResultInterface $result
+        BatchResultInterface $result,
+        WriterInterface $dataWriter,
+        TouchUpdaterInterface $touchUpdater,
+        OutputInterface $output
     ) {
         $this->getFacade()
-            ->runStorageTranslationCollector($baseQuery, $locale, $result, $this->dataWriter, $this->touchUpdater, $this->output);
+            ->runStorageTranslationCollector($baseQuery, $locale, $result, $dataWriter, $touchUpdater, $output);
     }
 
 }
