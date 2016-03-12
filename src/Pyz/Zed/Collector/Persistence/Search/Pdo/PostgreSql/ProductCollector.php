e6816cf57d2d3a1e061e80abc884ed698cf68897
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-12 18:17:41 +0100

    added missing headers and license information

--- src/Pyz/Zed/Collector/Persistence/Search/Pdo/PostgreSql/ProductCollector.php
+++ src/Pyz/Zed/Collector/Persistence/Search/Pdo/PostgreSql/ProductCollector.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Zed\Collector\Persistence\Search\Pdo\PostgreSql;
 
 use Spryker\Zed\Collector\Persistence\Exporter\AbstractPdoCollectorQuery;
--- src/Pyz/Zed/Collector/Persistence/Storage/Propel/TranslationCollector.php
+++ src/Pyz/Zed/Collector/Persistence/Storage/Propel/TranslationCollector.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Zed\Collector\Persistence\Storage\Propel;
 
 use Orm\Zed\Glossary\Persistence\Map\SpyGlossaryKeyTableMap;
