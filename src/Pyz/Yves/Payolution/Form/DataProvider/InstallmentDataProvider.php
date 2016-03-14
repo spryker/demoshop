93d9cb7feb05750fe361091f8f34d64f041c0ccf
Auris <aurimas.lickus@spryker.com>
2016-03-14 16:54:19 +0100

    Removed old calculator factory

--- src/Pyz/Yves/Payolution/Form/DataProvider/InstallmentDataProvider.php
+++ src/Pyz/Yves/Payolution/Form/DataProvider/InstallmentDataProvider.php
@@ -24,7 +24,7 @@ class InstallmentDataProvider implements DataProviderInterface
     protected $payolutionClient;
 
     /**
-     * @param PayolutionClientInterface $payolutionClient
+     * @param \Spryker\Client\Payolution\PayolutionClientInterface $payolutionClient
      */
     public function __construct(PayolutionClientInterface $payolutionClient)
     {
