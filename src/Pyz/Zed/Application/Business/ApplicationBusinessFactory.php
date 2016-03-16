52139416068853ff00b7b62e3392d7217ee6b8ad
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-16 21:22:53 +0100

    1600: install test data

---
+++ src/Pyz/Zed/Application/Business/ApplicationBusinessFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Application\Business;
+
+use Psr\Log\LoggerInterface;
+use Spryker\Zed\Application\Business\ApplicationBusinessFactory as SprykerApplicationBusinessFactory;
+
+/**
+ * @method \Spryker\Zed\Application\ApplicationConfig getConfig()
+ */
+class ApplicationBusinessFactory extends SprykerApplicationBusinessFactory
+{
+
+    /**
+     * @param \Psr\Log\LoggerInterface|null $logger
+     *
+     * @return \Spryker\Zed\Application\Business\Model\ApplicationCheckStep\AbstractApplicationCheckStep[]
+     */
+    public function createCheckSteps(LoggerInterface $logger = null)
+    {
+        return parent::createCheckSteps($logger);
+    }
+
+}
