9468dd1cad32bf42022a71e5724006818a0eaf13
Fabian Wesner <fabian.wesner@spryker.com>
2016-03-22 11:38:26 +0100

    #1681 Added missing licenses

--- src/Pyz/Yves/Checkout/Process/Steps/EntryStep.php
+++ src/Pyz/Yves/Checkout/Process/Steps/EntryStep.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * (c) Spryker Systems GmbH copyright protected
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
 namespace Pyz\Yves\Checkout\Process\Steps;
--- src/Pyz/Yves/Customer/Plugin/Provider/CustomerAuthenticationFailureHandler.php
+++ src/Pyz/Yves/Customer/Plugin/Provider/CustomerAuthenticationFailureHandler.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
 namespace Pyz\Yves\Customer\Plugin\Provider;
 
 use Pyz\Yves\Application\Business\Model\FlashMessengerInterface;
