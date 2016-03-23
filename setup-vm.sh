e31dff0e026db8de469aaf6be05c3eabe7d0147d
Marius Rizac <marius.rizac@spryker.com>
2016-03-23 12:45:22 +0100

    1684 creted function for frontend tool instalation and removed redundand code

--- setup-vm.sh
+++ setup-vm.sh
@@ -8,18 +8,13 @@ SETUP='spryker'
 
 if [[ `echo "$@" | grep '\-\-reset'` ]] || [[ `echo "$@" | grep '\-r'` ]]; then
     resetDevelopmentState
+    installAntelope
 
     exit 0
 fi
 
 if [[ `echo "$@" | grep '\-\-install'` ]] || [[ `echo "$@" | grep '\-i'` ]]; then
     installDemoshop
-
-    exit 0
-fi
-
-if [[ `echo "$@" | grep '\-\-help'` ]] || [[ `echo "$@" | grep '\-h'` ]]; then
-    displayHelp
 
     exit 0
 fi
