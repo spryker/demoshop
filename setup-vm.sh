44d1be65ac9055367a829ecc601b73cc7eba2111
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-29 16:40:05 +0200

    1747: setup-vm cleanups, added setup yves and zed

--- setup-vm.sh
+++ setup-vm.sh
@@ -6,18 +6,36 @@ SETUP='spryker'
 
 . ./setup-functions.sh
 
-if [[ `echo "$@" | grep '\-\-reset'` ]] || [[ `echo "$@" | grep '\-r'` ]]; then
-    resetDevelopmentState
-
+if [ $# -eq 0 ]; then
+    displayHelp
     exit 0
 fi
 
-if [[ `echo "$@" | grep '\-\-install'` ]] || [[ `echo "$@" | grep '\-i'` ]]; then
-    installDemoshop
-
-    exit 0
-fi
-
-displayHelp
+for arg in "$@"
+do
+    case $arg in
+        "--install-demo-shop" | "-i" )
+           installDemoshop
+           ;;
+        "--install-yves" | "-iy" )
+           installYves
+           ;;
+        "--install-zed" | "-iz" )
+           installZed
+           ;;
+        "--reset" | "-r" )
+           resetDevelopmentState
+           ;;
+        "--help" | "-h" )
+           displayHelp
+           ;;
+        *)
+            displayHeader
+            echo ""
+            echo "Unrecognized option: $@. Use -h to display help."
+            exit 1
+        ;;
+   esac
+done
 
 exit 0
