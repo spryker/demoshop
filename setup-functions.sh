b6161e5337c934648255a7d35e4ab84de131e3f5
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-03-29 19:29:25 +0200

    better git check

--- setup-functions.sh
+++ setup-functions.sh
@@ -73,8 +73,8 @@ function createDevelopmentDatabase {
 }
 
 function installDemoshop {
-    CHANGES=`git status | grep "nothing to commit, working directory clean"`
-    if [ "x$CHANGES" == "x" ]; then
+    CHANGES=`git status | grep "nothing to commit, working directory clean" || git status | grep "no changes added to commit"`
+    if [ "x$CHANGES" != "x" ]; then
         errorText "Uncommitted changes detected. Stash or commit your changes first"
         exit 1
     fi
