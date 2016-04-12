c30a63af087cb72e6b965c2597a7f97d44a74655
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-04-12 14:27:07 +0200

    heroku/configs: heroku setup

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -6,9 +6,13 @@ SETUP='spryker'
 
 . ./setup-functions.sh
 
-ELASTIC_SEARCH_URL=$BONSAI_URL
 
-echo "ELASTIC_SEARCH_URL: $ELASTIC_SEARCH_URL"
+if [ $# -eq 0 ]; then
+    echo "Use -i to install"
+    exit 0
+fi
+
+ELASTIC_SEARCH_URL=$BONSAI_URL
 
 function installElasticsearch {
     labelText "Flushing Elasticsearch"
@@ -18,11 +22,49 @@ function installElasticsearch {
     writeErrorMessage "Elasticsearch reset failed"
 }
 
+function installAntelope {
+    if [[ -z "$SETUP" ]]; then
+        tput setab 1
+        echo "Please do not run this script individually"
+        tput sgr0
+        exit 0
+    fi
+
+    if [[ `node -v | grep -E '^v[0-4]'` ]]; then
+        labelText "Upgrade Node.js"
+        $NPM cache clean -f
+
+        $NPM install -g n
+        writeErrorMessage "NPM build failed"
+
+        n stable
+
+        successText "Node.js updated to version `node -v`"
+        successText "NPM updated to version `$NPM -v`"
+    fi
+
+    labelText "Install Antelope tool globally"
+    $NPM install -g antelope
+    writeErrorMessage "Antelope setup failed"
+
+    ANTELOPE_TOOL=`which antelope`
+
+    if [[ -f $ANTELOPE_TOOL ]]; then
+        labelText "Installing project dependencies"
+        $ANTELOPE_TOOL install
+
+        labelText "Building and optimizing assets"
+        $ANTELOPE_TOOL build
+        writeErrorMessage "Antelope build failed"
+    fi
+}
 
 for arg in "$@"
 do
     case $arg in
         "-i" )
+        installAntelope
+        exit 1
             installElasticsearch
 
             vendor/bin/console setup:install $VERBOSITY
@@ -36,6 +78,8 @@ do
             vendor/bin/console collector:search:export $VERBOSITY
             vendor/bin/console collector:storage:export $VERBOSITY
             writeErrorMessage "DataStore setup failed"
+
+            installAntelope
 
             #labelText "Setting up cronjobs"
             #vendor/bin/console setup:jenkins:generate $VERBOSITY
