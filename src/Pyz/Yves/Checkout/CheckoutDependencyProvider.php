edd9ebc88a5e6478bd078f991f2dc0b8bc497cd6
René <rene.klatt@spryker.com>
2016-04-25 12:47:01 +0000

    1977 add DependencyProvider

---
+++ src/Pyz/Yves/Checkout/CheckoutDependencyProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\Checkout;
+
+use Spryker\Yves\Kernel\AbstractBundleDependencyProvider;
+
+class CheckoutDependencyProvider extends AbstractBundleDependencyProvider
+{
+}
