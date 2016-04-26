a815c21308846b7946b2f993cfe8c1847df66af8
Alessandro Bellini <alessandro.bellini@spryker.com>
2016-04-26 16:25:16 +0200

    braintree js integration

--- assets/Yves/demoshop/js/modules/braintree.js
+++ assets/Yves/demoshop/js/modules/braintree.js
@@ -6,38 +6,90 @@
 
 require('./main');
 var braintree = require('braintree-web');
-var $braintreeContanier = $('[data-braintree-client-token]');
-var braintreeClientToken = !!$braintreeContanier.length ? $braintreeContanier.data('braintree-client-token') : null;
 
-if (!!braintreeClientToken) {
-    var braintreeSetupSettings = {
-        id: 'payment-form'
-    };
+$(document).ready(function() {
+    var $form = $('#payment-form');
+    var $braintreeContanier = $('.braintree-method:first');
+    var braintreeClientToken = !!$braintreeContanier.length ? ($braintreeContanier.data('braintree-client-token') || null) : null;
 
-    if ($('[data-braintree-credit-card-method]').length) {
-        braintreeSetupSettings.hostedFields = {
-            number: {
-                selector: "#card-number"
-            },
-            cvv: {
-                selector: "#cvv"
-            },
-            expirationDate: {
-                selector: "#expiration-date"
-            }
+    function getPaymentMethod() {
+        return $('#paymentForm_paymentSelection input:checked').val();
+    }
+
+    function getErrorTemplate(message) {
+        return '<ul class="form-errors"><li>' + message + '</li></ul>'
+    }
+
+    function errorHandler(error) {
+        var paymentMethod = getPaymentMethod();
+
+        if (paymentMethod === 'braintreePayPal') {
+            return $('.braintree-paypal-error').html(getErrorTemplate(error.message));
+        }
+
+        if (paymentMethod === 'braintreeCreditCard') {
+            return $('.braintree-credit-card-error').html(getErrorTemplate(error.message));
+        }
+
+        return $form.submit();
+    }
+
+    function paymentMethodHandler(response) {
+        var paymentMethod = getPaymentMethod();
+
+        if ((paymentMethod === 'braintreePayPal' && response.type !== 'PayPalAccount') || (paymentMethod === 'braintreeCreditCard' && response.type !== 'CreditCard')) {
+            return errorHandler({
+                message: 'User did not enter a payment method'
+            });
+        }
+
+        return $form.submit();
+    }
+
+    function readyHandler() {
+        $('.braintree-loader').removeClass('show');
+        $('.braintree-method').addClass('show');
+    }
+
+    function loadBraintree() {
+        var braintreeSetupSettings = {
+            id: 'payment-form',
+            onReady: readyHandler,
+            onPaymentMethodReceived: paymentMethodHandler,
+            onError: errorHandler
         };
+
+        if ($('.braintree-credit-card-method').length) {
+            braintreeSetupSettings.hostedFields = {
+                styles: {
+                    'input': {
+                        'font-size': '16px',
+                        'color': '#333',
+                        'font-family': 'Fira Sans, Arial, sans-serif'
+                    }
+                },
+                number: {
+                    selector: '#braintree-credit-card-number'
+                },
+                cvv: {
+                    selector: '#braintree-credit-card-cvv'
+                },
+                expirationDate: {
+                    selector: '#braintree-credit-card-expiration-date'
+                }
+            };
+        }
+
+        if ($('.braintree-paypal-method').length) {
+            braintreeSetupSettings.paypal = {
+                container: 'braintree-paypal-container'
+            };
+        }
+
+        braintree.setup(braintreeClientToken, 'custom', braintreeSetupSettings);
     }
 
-    if ($('[data-braintree-paypal-method]').length) {
-        braintreeSetupSettings.paypal = {
-            container: "paypal-container"
-        };
+    if (!!braintreeClientToken) {
+        loadBraintree();
     }
-
-    braintree.setup(braintreeClientToken, 'custom', braintreeSetupSettings);
-
-    if (!PRODUCTION) {
-        console.log('braintreeClientToken', braintreeClientToken);
-        console.log('braintreeSetupSettings', braintreeSetupSettings);
-    }
-}
+});
