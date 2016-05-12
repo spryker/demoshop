a062643a7fc75612f2a7315d31670651ed28a07d
René <rene.klatt@spryker.com>
2016-05-12 10:29:53 +0000

    16 add missing return + test, update composer.lock

---
+++ tests/YvesUnit/Pyz/Yves/Checkout/Process/StepFactoryTest.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace YvesUnit\Pyz\Yves\Checkout\Process;
+
+use Pyz\Yves\Checkout\CheckoutDependencyProvider;
+use Pyz\Yves\Checkout\Process\StepFactory;
+use Spryker\Yves\Kernel\Container;
+
+/**
+ * @group Pyz
+ * @group Yves
+ * @group Checkout
+ * @group StepFactory
+ */
+class StepFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+    const EXPECTED_DEPENDENCY = 'expected dependency';
+
+    /**
+     * @return void
+     */
+    public function testGetCalculationClient()
+    {
+        $container = new Container([CheckoutDependencyProvider::CLIENT_CALCULATION => self::EXPECTED_DEPENDENCY]);
+
+        $stepFactory = new StepFactory();
+        $stepFactory->setContainer($container);
+
+        $this->assertSame(self::EXPECTED_DEPENDENCY, $stepFactory->getCalculationClient());
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetCheckoutClient()
+    {
+        $container = new Container([CheckoutDependencyProvider::CLIENT_CHECKOUT => self::EXPECTED_DEPENDENCY]);
+
+        $stepFactory = new StepFactory();
+        $stepFactory->setContainer($container);
+
+        $this->assertSame(self::EXPECTED_DEPENDENCY, $stepFactory->getCheckoutClient());
+    }
+
+}
