fa5738c7b504fae0c925e45d45357720aa7172ac
Andriy Netseplyayev <andriy.netseplyayev@spryker.com>
2016-06-06 15:18:12 +0000

    fixed application conflicts

--- src/Pyz/Zed/Application/ApplicationDependencyProvider.php
+++ src/Pyz/Zed/Application/ApplicationDependencyProvider.php
@@ -7,19 +7,15 @@
 
 namespace Pyz\Zed\Application;
 
-use Pyz\Yves\Application\Plugin\Provider\WebProfilerServiceProvider;
 use Pyz\Yves\NewRelic\Plugin\Provider\NewRelicServiceProvider;
 use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\SessionServiceProvider;
 use Silex\Provider\TwigServiceProvider;
-<<<<<<< HEAD
-=======
 use Silex\Provider\WebProfilerServiceProvider;
 use Spryker\Shared\Application\ApplicationConstants;
 use Spryker\Shared\Config\Config;
 use Spryker\Shared\Symfony\Plugin\ServiceProvider\DoubleSubmitProtectionServiceProvider;
->>>>>>> a909647... Updated composer.lock
 use Spryker\Zed\Acl\Communication\Plugin\Bootstrap\AclBootstrapProvider;
 use Spryker\Zed\Application\ApplicationDependencyProvider as SprykerApplicationDependencyProvider;
 use Spryker\Zed\Application\Communication\Plugin\ServiceProvider\DateFormatterServiceProvider;
@@ -94,14 +90,18 @@ class ApplicationDependencyProvider extends SprykerApplicationDependencyProvider
             new DateFormatterServiceProvider(),
             new TranslationServiceProvider(),
             new SubRequestServiceProvider(),
-<<<<<<< HEAD
-            new WebProfilerServiceProvider()
-=======
+            new WebProfilerServiceProvider(),
             new DoubleSubmitProtectionServiceProvider()
->>>>>>> a909647... Updated composer.lock
         ];
 
-        return array_merge($providers, $coreProviders);
+        if (Config::get(ApplicationConstants::ENABLE_WEB_PROFILER, false)) {
+            $providers[] = new WebProfilerServiceProvider();
+        }
+
+        $providers = array_merge($providers, $coreProviders);
+        $providers[] = new DoubleSubmitProtectionServiceProvider();
+
+        return $providers;
     }
 
     /**
