6df5a5eb1d2009bc3bed1cd789f51657c06e280f
Auris <aurimas.lickus@spryker.com>
2016-06-09 17:52:12 +0200

    cr

---
+++ src/Orm/Zed/StateMachine/Persistence/SpyStateMachineItemState.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\StateMachine\Persistence;
+
+use Orm\Zed\StateMachine\Persistence\Base\SpyStateMachineItemState as BaseSpyStateMachineItemState;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_state_machine_item_state' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyStateMachineItemState extends BaseSpyStateMachineItemState
+{
+
+}
