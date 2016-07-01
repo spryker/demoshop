6aa010f1d9b14f7cbfdee7bbcf03decf07db184f
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-07-01 17:02:42 +0200

    pim: added missing entities

---
+++ src/Orm/Zed/ProductManagement/Persistence/SpyProductManagementAttributeMetadataQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductManagement\Persistence;
+
+use Orm\Zed\ProductManagement\Persistence\Base\SpyProductManagementAttributeMetadataQuery as BaseSpyProductManagementAttributeMetadataQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_product_management_attribute_metadata' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductManagementAttributeMetadataQuery extends BaseSpyProductManagementAttributeMetadataQuery
+{
+
+}
