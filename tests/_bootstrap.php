78493940bbb1d8b04170b7861ce292c2c8026ca2
René Klatt <rene.klatt@spryker.com>
2016-07-11 08:19:05 +0000

    some fixes

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,8 +1,1 @@
 <?php
-
-//$pathToComposerAutoload = __DIR__ . '/../vendor/autoload.php';
-//define('COMPOSER_AUTOLOAD', $pathToComposerAutoload);
-//
-//require_once COMPOSER_AUTOLOAD;
-//
-//define('APPLICATION_ROOT_DIR', __DIR__ . '/../');
