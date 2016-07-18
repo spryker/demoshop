81ec4a8d43dc191ce79dfb4f7cb8e00af812f0b3
René Klatt <rene.klatt@spryker.com>
2016-07-18 06:44:43 +0000

    core-297 test cleanups

--- tests/_support/ZedAcceptanceTester.php
+++ tests/_support/ZedAcceptanceTester.php
@@ -16,18 +16,4 @@ class ZedAcceptanceTester extends AcceptanceTester
         $i->amZed();
     }
 
-        /**
-         * @param string $username
-         * @param string $password
-         *
-         * @return void
-         */
-    public function iAmLoggedIn($username = 'admin@spryker.com', $password = 'change123')
-    {
-        $this->amOnPage('/auth/login');
-        $this->fillField('auth[username]', $username);
-        $this->fillField('auth[password]', $password);
-        $this->click('Login');
-    }
-
 }
