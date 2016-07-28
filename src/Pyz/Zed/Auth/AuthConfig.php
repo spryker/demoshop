453283129cd8fee3ee1332ee40ab17ff57c73162
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-07-28 10:50:41 +0200

    feature/pim: variant generation

--- src/Pyz/Zed/Auth/AuthConfig.php
+++ src/Pyz/Zed/Auth/AuthConfig.php
@@ -19,6 +19,7 @@ class AuthConfig extends SprykerAuthConfig
     {
         $this->addIgnorable('heartbeat', 'index', 'index');
         $this->addIgnorable('_profiler', 'wdt', '*');
+        $this->addIgnorable('product-management', 'matrix', '*');
 
         return parent::getIgnorable();
     }
