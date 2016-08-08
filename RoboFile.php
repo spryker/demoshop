5c292cd3b83dacbad890d7e4598632aff6dd9c44
René Klatt <rene.klatt@gmx.net>
2016-08-08 07:24:46 +0200

    add robofile

---
+++ RoboFile.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'vendor/autoload.php';
+
+class RoboFile extends \Robo\Tasks
+{
+    use Codegyre\RoboCI\Command\CI;
+    use Codegyre\RoboCI\Command\Travis\Prepare;
+
+    // define public methods as commands
+}
