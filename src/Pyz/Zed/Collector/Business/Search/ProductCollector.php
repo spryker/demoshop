0caac60d5b1b7b27b45c4b4b42fb1707791530b8
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-08-16 17:31:30 +0200

    Corrected product search collector's image handling; Providing the correct stack of catalog query expanders and result formatters on project level

--- src/Pyz/Zed/Collector/Business/Search/ProductCollector.php
+++ src/Pyz/Zed/Collector/Business/Search/ProductCollector.php
@@ -10,15 +10,11 @@ namespace Pyz\Zed\Collector\Business\Search;
 use Spryker\Shared\Product\ProductConstants;
 use Spryker\Zed\Collector\Business\Collector\Search\AbstractSearchPdoCollector;
 use Spryker\Zed\Collector\CollectorConfig;
-use Spryker\Zed\ProductImage\Persistence\ProductImageQueryContainerInterface;
 use Spryker\Zed\Search\Business\SearchFacadeInterface;
 use Spryker\Zed\Search\Dependency\Plugin\PageMapInterface;
 
 class ProductCollector extends AbstractSearchPdoCollector
 {
-
-    const ID_IMAGE_SET = 'id_image_set';
-    const PRODUCT_IMAGE_SET = 'product_image_set';
 
     /**
      * @var \Spryker\Zed\Search\Dependency\Plugin\PageMapInterface
@@ -31,23 +27,15 @@ class ProductCollector extends AbstractSearchPdoCollector
     protected $searchFacade;
 
     /**
-     * @var \Spryker\Zed\ProductImage\Persistence\ProductImageQueryContainerInterface
-     */
-    protected $productImageQueryContainer;
-
-    /**
      * @param \Spryker\Zed\Search\Dependency\Plugin\PageMapInterface $productDataPageMapPlugin
      * @param \Spryker\Zed\Search\Business\SearchFacadeInterface $searchFacade
-     * @param \Spryker\Zed\ProductImage\Persistence\ProductImageQueryContainerInterface $productImageQueryContainer
      */
     public function __construct(
         PageMapInterface $productDataPageMapPlugin,
-        SearchFacadeInterface $searchFacade,
-        ProductImageQueryContainerInterface $productImageQueryContainer
+        SearchFacadeInterface $searchFacade
     ) {
         $this->productDataPageMapPlugin = $productDataPageMapPlugin;
         $this->searchFacade = $searchFacade;
-        $this->productImageQueryContainer = $productImageQueryContainer;
     }
 
     /**
@@ -86,37 +74,8 @@ class ProductCollector extends AbstractSearchPdoCollector
         $result[CollectorConfig::COLLECTOR_TOUCH_ID] = $collectItemData[CollectorConfig::COLLECTOR_TOUCH_ID];
         $result[CollectorConfig::COLLECTOR_RESOURCE_ID] = $collectItemData[CollectorConfig::COLLECTOR_RESOURCE_ID];
         $result[CollectorConfig::COLLECTOR_SEARCH_KEY] = $collectItemData[CollectorConfig::COLLECTOR_SEARCH_KEY];
-        $result[self::PRODUCT_IMAGE_SET] = $this->generateImage($collectItemData[self::ID_IMAGE_SET]);
-
-        //->addSearchResultData($pageMapTransfer, 'image_url', $attributes['image_big']) // TODO: attributes should come from dynamic attribute mapping, e.g. database (image attributes are aliased with different name so we need to keep it)
-        //->addSearchResultData($pageMapTransfer, 'thumbnail_url', $attributes['image_small'])
 
         return $result;
-    }
-
-    /**
-     * @param int $idImageSet
-     *
-     * @return array
-     */
-    protected function generateImage($idImageSet)
-    {
-        if ($idImageSet === null) {
-            return [];
-        }
-
-        $image = $this->productImageQueryContainer
-            ->queryImagesByIdProductImageSet($idImageSet)
-            ->find()->getFirst();
-
-        if (!$image) {
-            return [];
-        }
-
-        $imageArray = $image->getSpyProductImage()->toArray();
-        $imageArray += $image->toArray();
-
-        return $imageArray;
     }
 
 }
