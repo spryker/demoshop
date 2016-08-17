f73e866796f8556481451960472671a19ecd9e07
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-08-17 09:42:22 +0200

    Simplified heroku deploy scripts

--- deploy/heroku/setup-Yves.sh
+++ deploy/heroku/setup-Yves.sh
@@ -11,53 +11,50 @@ SETUP='spryker'
 CONSOLE=vendor/bin/console
 
 . deploy/setup/functions.sh
-
-
-function installAntelope {
-    if [[ `node -v | grep -E '^v[0-4]'` ]]; then
-        labelText "Upgrade Node.js"
-        $NPM cache clean -f
-
-        $NPM install -g n
-        writeErrorMessage "NPM build failed"
-
-        n stable
-
-        successText "Node.js updated to version `node -v`"
-        successText "NPM updated to version `$NPM -v`"
-    fi
-
-    labelText "Install Antelope tool globally"
-    $NPM install -g antelope
-    writeErrorMessage "Antelope setup failed"
-
-    ANTELOPE_TOOL=`which antelope`
-
-    if [[ -f $ANTELOPE_TOOL ]]; then
-        labelText "Installing project dependencies"
-        $ANTELOPE_TOOL install
-
-        labelText "Building and optimizing assets"
-        $ANTELOPE_TOOL build
-        writeErrorMessage "Antelope build failed"
-    fi
-}
+#
+#
+#function installAntelope {
+#    if [[ `node -v | grep -E '^v[0-4]'` ]]; then
+#        labelText "Upgrade Node.js"
+#        $NPM cache clean -f
+#
+#        $NPM install -g n
+#        writeErrorMessage "NPM build failed"
+#
+#        n stable
+#
+#        successText "Node.js updated to version `node -v`"
+#        successText "NPM updated to version `$NPM -v`"
+#    fi
+#
+#    labelText "Install Antelope tool globally"
+#    $NPM install -g antelope
+#    writeErrorMessage "Antelope setup failed"
+#
+#    ANTELOPE_TOOL=`which antelope`
+#
+#    if [[ -f $ANTELOPE_TOOL ]]; then
+#        labelText "Installing project dependencies"
+#        $ANTELOPE_TOOL install
+#
+#        labelText "Building and optimizing assets"
+#        $ANTELOPE_TOOL build
+#        writeErrorMessage "Antelope build failed"
+#    fi
+#}
 
 for arg in "$@"
 do
     case $arg in
         "-i" )
-            $CONSOLE transfer:generate
-            $CONSOLE setup:search
-            $CONSOLE setup:generate-client-ide-auto-completion
-            $CONSOLE setup:generate-ide-auto-completion
-            $CONSOLE setup:generate-zed-ide-auto-completion
+            $CONSOLE setup:install $VERBOSITY
+            writeErrorMessage "Setup install failed"
 
             updateComposerBinary
 
             dumpAutoload
 
-            installAntelope
+            antelopeInstallYves
             ;;
             *)
             echo "Use -i to install"
--- deploy/heroku/setup-Zed.sh
+++ deploy/heroku/setup-Zed.sh
@@ -24,7 +24,7 @@ do
 
             dumpAutoload
 
-            ./deploy/heroku/setup-Yves.sh -i
+            antelopeInstallZed
             ;;
             *)
             echo "Use -i to install"
