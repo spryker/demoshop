8441bfa73159327570c97163e7082a221879b02b
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-08-31 12:29:03 +0200

    CORE-366 ProductSearch related propel changes

---
+++ src/Orm/Zed/ProductSearch/Persistence/SpyProductSearchAttributeArchive.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductSearch\Persistence;
+
+use Spryker\Zed\ProductSearch\Persistence\Propel\AbstractSpyProductSearchAttributeArchive as BaseSpyProductSearchAttributeArchive;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_search_attribute_archive' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductSearchAttributeArchive extends BaseSpyProductSearchAttributeArchive
+{
+
+}
