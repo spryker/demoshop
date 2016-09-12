9a3fbf2ac7ffc8b45791da646cc294b94e383c3d
René Klatt <rene.klatt@spryker.com>
2016-09-12 09:51:09 +0000

    merged master

--- .roboci/_recipes/elasticsearch/link.sh
+++
@@ -1,6 +0,0 @@
-socat TCP4-LISTEN:10005,fork,reuseaddr TCP4:elasticsearch:10005 &
-sudo mkdir /var/run/elasticsearch
-sudo chown travis /var/run/elasticsearch
-
-# socat UNIX-LISTEN:/var/run/postgresql/.s.PGSQL.5432,fork,reuseaddr TCP4:postgresql:3306 &
-# psql -c "CREATE USER postgres WITH PASSWORD ''  ;"
