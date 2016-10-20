5e4462a2f5af74c76c083d3ea817969c326a4460
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-10-20 13:30:30 +0200

    feature/pim: proper dependency

--- src/Pyz/Zed/Product/Business/ProductBusinessFactory.php
+++ src/Pyz/Zed/Product/Business/ProductBusinessFactory.php
@@ -21,7 +21,7 @@ class ProductBusinessFactory extends SprykerProductBusinessFactory
         return new ProductUrlGenerator(
             $this->createProductAbstractManager(),
             $this->getLocaleFacade(),
-            $this->getUrlFacade()
+            $this->getUtilFacade()
         );
     }
 
