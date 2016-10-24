2f72fe46471529b48a2417d22e0f13e3e5b64c5c
Tamas Nyulas <tamas.nyulas@spryker.com>
2016-10-24 14:13:52 +0200

    feature/pim: Fixed product search collector query

--- src/Pyz/Zed/Collector/Persistence/Search/Pdo/PostgreSql/ProductCollectorQuery.php
+++ src/Pyz/Zed/Collector/Persistence/Search/Pdo/PostgreSql/ProductCollectorQuery.php
@@ -35,26 +35,32 @@ class ProductCollectorQuery extends AbstractPdoCollectorQuery
                 spy_touch_search.id_touch_search                              AS %s,
                 spy_product_image_set.id_product_image_set                    AS id_image_set
             FROM spy_touch
-                INNER JOIN spy_product_abstract ON (spy_touch.item_id = spy_product_abstract.id_product_abstract)
+                INNER JOIN spy_product_abstract 
+                  ON (spy_touch.item_id = spy_product_abstract.id_product_abstract)
                 INNER JOIN spy_product
                   ON (spy_product_abstract.id_product_abstract = spy_product.fk_product_abstract AND spy_product.is_active)
                 INNER JOIN spy_product_abstract_localized_attributes
                   ON (spy_product_abstract.id_product_abstract = spy_product_abstract_localized_attributes.fk_product_abstract)
-                INNER JOIN spy_locale ON (spy_product_abstract_localized_attributes.fk_locale = spy_locale.id_locale)
+                INNER JOIN spy_locale 
+                  ON (spy_product_abstract_localized_attributes.fk_locale = spy_locale.id_locale)
                 INNER JOIN spy_url product_urls
                   ON (spy_product_abstract.id_product_abstract = product_urls.fk_resource_product_abstract AND
                       product_urls.fk_locale = spy_locale.id_locale)
                 INNER JOIN spy_product_localized_attributes
                   ON (spy_product.id_product = spy_product_localized_attributes.fk_product AND
                       spy_product_localized_attributes.fk_locale = spy_locale.id_locale)
-                INNER JOIN spy_product_search ON (spy_product.id_product = spy_product_search.fk_product AND
-                                                  spy_product_search.fk_locale = spy_locale.id_locale)
-                INNER JOIN spy_product_category ON spy_product_category.fk_product_abstract = spy_product_abstract.id_product_abstract
-                INNER JOIN spy_stock_product ON (spy_product.id_product = spy_stock_product.fk_product)
-                INNER JOIN spy_touch_search ON spy_touch_search.fk_touch = spy_touch.id_touch
+                INNER JOIN spy_product_search 
+                  ON (spy_product.id_product = spy_product_search.fk_product AND
+                      spy_product_search.fk_locale = spy_locale.id_locale)
+                INNER JOIN spy_product_category 
+                  ON spy_product_category.fk_product_abstract = spy_product_abstract.id_product_abstract
+                INNER JOIN spy_stock_product 
+                  ON (spy_product.id_product = spy_stock_product.fk_product)
+                LEFT JOIN spy_touch_search 
+                  ON spy_touch_search.fk_touch = spy_touch.id_touch
                 LEFT JOIN spy_product_image_set 
                   ON (spy_product_image_set.fk_product_abstract = spy_product_abstract.id_product_abstract OR spy_product_image_set.fk_product = spy_product.id_product) AND
-                     spy_product_image_set.fk_locale = spy_locale.id_locale
+                      spy_product_image_set.fk_locale = spy_locale.id_locale
             WHERE
                 spy_touch.item_event = :spy_touch_item_event
                 AND spy_touch.touched >= :spy_touch_touched
