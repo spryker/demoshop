d56079ee268b8e3c220a8c31b96dee222bc6cb47
Thomas Kühnel <thomas.kuehnel@spryker.com>
2016-10-24 10:31:11 +0000

    CORE-344 Adds delete-plugin stack configuration for category bundle

--- src/Pyz/Zed/Category/CategoryDependencyProvider.php
+++ src/Pyz/Zed/Category/CategoryDependencyProvider.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * This file is part of the Spryker Demoshop.
  * For full license information, please view the LICENSE file that was distributed with this source code.
@@ -8,7 +7,24 @@
 namespace Pyz\Zed\Category;
 
 use Spryker\Zed\Category\CategoryDependencyProvider as SprykerDependencyProvider;
+use Spryker\Zed\ProductCategory\Communication\Plugin\RemoveProductsAssignmentPlugin;
 
 class CategoryDependencyProvider extends SprykerDependencyProvider
 {
+
+    /**
+     * @return \Spryker\Zed\Category\Dependency\Plugin\CategoryDeletePluginInterface[]
+     */
+    protected function getDeletePluginStack()
+    {
+        $deletePlugins = array_merge(
+            [
+                new RemoveProductsAssignmentPlugin(),
+            ],
+            parent::getDeletePluginStack()
+        );
+
+        return $deletePlugins;
+    }
+
 }
