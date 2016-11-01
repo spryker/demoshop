3023fa2dd1035f68fab415879ead37f713119edb
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-11-01 15:21:27 +0100

    feature/core-635-wishlist  refactored wishlist and wishlist items, updated tests

---
+++ src/Orm/Zed/Wishlist/Persistence/SpyWishlistItem.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Wishlist\Persistence;
+
+use Orm\Zed\Wishlist\Persistence\Base\SpyWishlistItem as BaseSpyWishlistItem;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_wishlist_item' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyWishlistItem extends BaseSpyWishlistItem
+{
+
+}
---
+++ src/Orm/Zed/Wishlist/Persistence/SpyWishlistItemQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Wishlist\Persistence;
+
+use Orm\Zed\Wishlist\Persistence\Base\SpyWishlistItemQuery as BaseSpyWishlistItemQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_wishlist_item' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyWishlistItemQuery extends BaseSpyWishlistItemQuery
+{
+
+}
