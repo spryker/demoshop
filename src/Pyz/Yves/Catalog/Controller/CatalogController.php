8ff227c664d0d476512f58269ca4e5d80205ab2f
René Klatt <rene.klatt@spryker.com>
2016-11-02 13:36:58 +0000

    core-645 removed price filter, replaced price filter with money filter, add TwigMoneyServiceProvider

--- src/Pyz/Yves/Catalog/Controller/CatalogController.php
+++ src/Pyz/Yves/Catalog/Controller/CatalogController.php
@@ -8,7 +8,10 @@
 namespace Pyz\Yves\Catalog\Controller;
 
 use Generated\Shared\Search\PageIndexMap;
+use Generated\Shared\Transfer\FacetSearchResultTransfer;
+use Spryker\Client\Search\Plugin\Elasticsearch\ResultFormatter\FacetResultFormatterPlugin;
 use Spryker\Yves\Application\Controller\AbstractController;
+use Spryker\Yves\Money\Plugin\MoneyPlugin;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -65,6 +68,68 @@ class CatalogController extends AbstractController
         $searchResults['searchString'] = $searchString;
 
         return $this->viewResponse($searchResults);
+    }
+
+    /**
+     * @param array $searchResults
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    protected function formatJsonResponse(array $searchResults)
+    {
+        $searchResults['products'] = $this->formatValidProductPrices($searchResults['products']);
+
+        return $this->jsonResponse($searchResults);
+    }
+
+    /**
+     * @param array $products
+     *
+     * @return array
+     */
+    protected function formatValidProductPrices(array $products)
+    {
+        $moneyPlugin = new MoneyPlugin();
+        foreach ($products as &$product) {
+            $moneyTransfer = $moneyPlugin->fromInteger($product['price']);
+            $product['formatted_price'] = $moneyPlugin->formatWithSymbol($moneyTransfer);
+        }
+
+        return $products;
+    }
+
+    /**
+     * @param array $searchResults
+     *
+     * @return array
+     */
+    protected function convertCategoryFacetFilters($searchResults)
+    {
+        if (!isset($searchResults[FacetResultFormatterPlugin::NAME]['category'])) {
+            $searchResults[FacetResultFormatterPlugin::NAME]['category'] = [];
+
+            return $searchResults;
+        }
+
+        $searchResults[FacetResultFormatterPlugin::NAME]['category'] = $this->processCategoryFacetFilters($searchResults[FacetResultFormatterPlugin::NAME]['category']);
+
+        return $searchResults;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\FacetSearchResultTransfer $categoryFacetResultTransfer
+     *
+     * @return array
+     */
+    protected function processCategoryFacetFilters(FacetSearchResultTransfer $categoryFacetResultTransfer)
+    {
+        $result = [];
+
+        foreach ($categoryFacetResultTransfer->getValues() as $facetResultValueTransfer) {
+            $result[$facetResultValueTransfer->getValue()] = $facetResultValueTransfer->getDocCount();
+        }
+
+        return $result;
     }
 
 }
