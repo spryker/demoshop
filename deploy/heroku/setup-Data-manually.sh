e4fa2bc0427155c49d18ba4fe17eb8e3e46742ec
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-11-29 14:40:59 +0100

    feature/heroku-integration removed debug

--- deploy/heroku/setup-Data-manually.sh
+++ deploy/heroku/setup-Data-manually.sh
@@ -20,17 +20,6 @@ function resetElasticsearch {
     writeErrorMessage "Elasticsearch reset failed"
 }
 
-function setupElasticsearch {
-    for store in "${STORES[@]}"
-    do
-        labelText "Setting up Elasticsearch: ${store}"
-        curl -XPOST $ELASTIC_SEARCH_URL/${store}/_close
-        curl -XPUT $ELASTIC_SEARCH_URL/${store}/_settings -d '{"analysis": {"analyzer": {"lowercase_keyword_analyzer": {"tokenizer": "keyword","filter": ["lowercase"]}}}}'
-        curl -XPOST $ELASTIC_SEARCH_URL/${store}/_open
-        writeErrorMessage "Elasticsearch setup failed for ${store}"
-    done
-}
-
 for arg in "$@"
 do
     case $arg in
@@ -39,8 +28,6 @@ do
 
             $CONSOLE setup:install $VERBOSITY
             writeErrorMessage "Setup install failed"
-
-            setupElasticsearch
 
             labelText "Importing Demo data"
             $CONSOLE import:demo-data $VERBOSITY
