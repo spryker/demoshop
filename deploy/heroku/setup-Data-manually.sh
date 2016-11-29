a661f2bbf3d0eccbebdc35a30ba49a149aac6686
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-11-29 11:08:08 +0100

    feature/heroku-integration updated Elasticsearch setup, updated php ext

--- deploy/heroku/setup-Data-manually.sh
+++ deploy/heroku/setup-Data-manually.sh
@@ -12,12 +12,26 @@ if [ $# -eq 0 ]; then
     exit 0
 fi
 
-ELASTIC_SEARCH_URL=$BONSAI_URL
+ESSTORES=( de_search en_search )
 
 function resetElasticsearch {
-    labelText "Flushing Elasticsearch"
-    curl -XDELETE "$ELASTIC_SEARCH_URL/de_search/"
-    writeErrorMessage "Elasticsearch reset failed"
+    for store in "${ESSTORES[@]}"
+    do
+        labelText "Flushing Elasticsearch: ${store}"
+        curl -XDELETE "$ELASTIC_SEARCH_URL/${store}/"
+        writeErrorMessage "Elasticsearch reset failed for ${store}"
+    done
+}
+
+function setupElasticsearch {
+    for store in "${ESSTORES[@]}"
+    do
+        labelText "Setting up Elasticsearch: ${store}"
+        curl -XPOST $ELASTIC_SEARCH_URL/${store}/_close
+        curl -XPUT $ELASTIC_SEARCH_URL/${store}/_settings -d '{"analysis": {"analyzer": {"lowercase_keyword_analyzer": {"tokenizer": "keyword","filter": ["lowercase"]}}}}'
+        curl -XPOST $ELASTIC_SEARCH_URL/${store}/_open
+        writeErrorMessage "Elasticsearch setup failed for ${store}"
+    done
 }
 
 for arg in "$@"
@@ -28,6 +42,8 @@ do
 
             $CONSOLE setup:install $VERBOSITY
             writeErrorMessage "Setup install failed"
+
+            setupElasticsearch
 
             labelText "Importing Demo data"
             $CONSOLE import:demo-data $VERBOSITY
