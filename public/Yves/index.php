b46ea2cd6ecf6a6cf09367f3543b72640b5073af
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-12-06 12:36:29 +0100

    no more c3.php

--- public/Yves/index.php
+++ public/Yves/index.php
@@ -6,7 +6,6 @@ use Spryker\Shared\Library\Application\Environment;
 define('APPLICATION', 'YVES');
 defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR', realpath(__DIR__ . '/../..'));
 
-include APPLICATION_ROOT_DIR . '/c3.php';
 require_once APPLICATION_ROOT_DIR . '/vendor/autoload.php';
 
 Environment::initialize();
