4403038c5d8b756e7793038494ccdeb4eaf893bd
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-12-06 14:50:02 +0100

    c3.php fix

--- public/Zed/index.php
+++ public/Zed/index.php
@@ -6,6 +6,7 @@ use Spryker\Shared\Library\Application\Environment;
 define('APPLICATION', 'ZED');
 defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR', realpath(__DIR__ . '/../..'));
 
+include APPLICATION_ROOT_DIR . '/c3.php';
 require_once APPLICATION_ROOT_DIR . '/vendor/autoload.php';
 
 Environment::initialize();
