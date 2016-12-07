7990c0d5131ddedfe7cb3f06dcf9e7694b2f783d
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-12-07 11:56:17 +0100

    NewRelicServiceProvider

--- src/Pyz/Yves/NewRelic/Plugin/Provider/NewRelicServiceProvider.php
+++ src/Pyz/Yves/NewRelic/Plugin/Provider/NewRelicServiceProvider.php
@@ -9,10 +9,10 @@ namespace Pyz\Yves\NewRelic\Plugin\Provider;
 
 use Pyz\Yves\Application\Plugin\Provider\AbstractServiceProvider;
 use Silex\Application;
+use Spryker\Shared\Kernel\Store;
 use Spryker\Shared\Library\System;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Symfony\Component\HttpKernel\KernelEvents;
 
 /**
  * @method \Pyz\Yves\NewRelic\NewRelicFactory getFactory()
@@ -21,13 +21,20 @@ class NewRelicServiceProvider extends AbstractServiceProvider
 {
 
     /**
-     * @var \Spryker\Shared\NewRelic\ApiInterface
+     * @var \Spryker\Shared\NewRelic\NewRelicApiInterface
      */
     protected $newRelicApi;
 
-    public function __construct()
+    /**
+     * @return \Spryker\Shared\NewRelic\NewRelicApiInterface
+     */
+    public function getNewRelicApi()
     {
-        $this->newRelicApi = $this->getFactory()->createNewRelicApi();
+        if ($this->newRelicApi === null) {
+            $this->newRelicApi = $this->getFactory()->createNewRelicApi();
+        }
+
+        return $this->newRelicApi;
     }
 
     /**
@@ -46,7 +53,30 @@ class NewRelicServiceProvider extends AbstractServiceProvider
      */
     public function boot(Application $app)
     {
-        $app['dispatcher']->addListener(KernelEvents::CONTROLLER, [$this, 'onKernelController'], -255);
+        //$app['dispatcher']->addListener(KernelEvents::CONTROLLER, [$this, 'onKernelController'], -255);
+
+        $app->before(function (Request $request) {
+            $module = $request->attributes->get('module');
+            $controller = $request->attributes->get('controller');
+            $action = $request->attributes->get('action');
+            $transactionName = $module . '/' . $controller . '/' . $action;
+
+            $requestUri = array_key_exists('REQUEST_URI', $_SERVER) ? $_SERVER['REQUEST_URI'] : 'unknown';
+
+            $host = isset($_SERVER['COMPUTERNAME']) ? $_SERVER['COMPUTERNAME'] : System::getHostname();
+
+            $store = Store::getInstance();
+
+            $this->getNewRelicApi()->setNameOfTransaction($transactionName);
+            $this->getNewRelicApi()->addCustomParameter('request_uri', $requestUri);
+            $this->getNewRelicApi()->addCustomParameter('host', $host);
+            $this->getNewRelicApi()->addCustomParameter('store', $store->getStoreName());
+            $this->getNewRelicApi()->addCustomParameter('locale', $store->getCurrentLocale());
+
+            if (strpos($transactionName, 'system/heartbeat') !== false) {
+                $this->newRelicApi->markIgnoreTransaction();
+            }
+        });
     }
 
     /**
