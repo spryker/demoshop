6946fa81d394db430de8fe207d9e1b82f3056b49
Oliwier Ptak <oliwier.ptak@spryker.com>
2016-12-21 12:47:29 +0100

    feature/core-756-wishlist-plugins typo

--- src/Pyz/Zed/Wishlist/WishlistDependencyProvider.php
+++ src/Pyz/Zed/Wishlist/WishlistDependencyProvider.php
@@ -8,7 +8,7 @@ namespace Pyz\Zed\Wishlist;
 
 use Spryker\Zed\Wishlist\WishlistDependencyProvider as SprykerWishlistDependencyProvider;
 
-class CartDependencyProvider extends SprykerWishlistDependencyProvider
+class WishlistDependencyProvider extends SprykerWishlistDependencyProvider
 {
 
     /**
