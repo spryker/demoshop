1b78177382fb22096951976e1bb94df86d54a624
ilmente <alessandro.bellini@spryker.com>
2016-12-23 17:21:43 +0100

    add missing licence header to assets

--- assets/Yves/default/js/base/utils/keyboard-codes.js
+++ assets/Yves/default/js/base/utils/keyboard-codes.js
@@ -1,3 +1,8 @@
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 'use strict';
 
 var keyboardCodes = {
