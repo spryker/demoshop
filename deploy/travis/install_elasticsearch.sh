f6a7b83ab7ef76ecb130ce7d702def820bb7c5a1
Auris <aurimas.lickus@spryker.com>
2017-01-05 18:10:41 +0100

    travis settings

--- deploy/travis/install_elasticsearch.sh
+++ deploy/travis/install_elasticsearch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir /tmp/elasticsearch
-wget -O - https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.3/elasticsearch-2.3.3.tar.gz | tar xz --directory=/tmp/elasticsearch --strip-components=1
-/tmp/elasticsearch/bin/elasticsearch --daemonize --path.data /tmp
+mkdir /home/travis/elasticsearch
+wget -O - https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.3/elasticsearch-2.3.3.tar.gz | tar xz --directory=/home/travis/elasticsearch --strip-components=1
+/tmp/elasticsearch/bin/elasticsearch --daemonize --path.data /home/travis/elasticsearch
