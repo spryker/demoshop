6c141706591aface25eefec886f120bd8d069fe7
Auris <aurimas.lickus@spryker.com>
2017-01-06 09:43:44 +0100

    Fix elasticsearch path

--- deploy/travis/install_elasticsearch.sh
+++ deploy/travis/install_elasticsearch.sh
@@ -2,4 +2,4 @@
 
 mkdir /home/travis/elasticsearch
 wget -O - https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.3.3/elasticsearch-2.3.3.tar.gz | tar xz --directory=/home/travis/elasticsearch --strip-components=1
-/tmp/elasticsearch/bin/elasticsearch --daemonize --path.data /home/travis/elasticsearch
+/home/travis/elasticsearch/bin/elasticsearch --daemonize --path.data /home/travis/elasticsearch
