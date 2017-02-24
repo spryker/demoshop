10a7119431162ca44dcc380856e135dedf366d09
René Klatt <rene.klatt@spryker.com>
2017-02-24 08:52:46 +0000

    fixed error handler configuration

--- config/Shared/config_default-production.php
+++ config/Shared/config_default-production.php
@@ -5,13 +5,10 @@
  */
 
 use Spryker\Shared\Application\ApplicationConstants;
-use Spryker\Shared\ErrorHandler\ErrorHandlerConstants;
 use Spryker\Shared\Kernel\KernelConstants;
 
 $config[ApplicationConstants::CLOUD_ENABLED] = true;
 $config[ApplicationConstants::CLOUD_OBJECT_STORAGE_ENABLED] = true;
 $config[ApplicationConstants::CLOUD_CDN_ENABLED] = true;
 
-$config[ErrorHandlerConstants::ERROR_RENDERER] = WebExceptionErrorRenderer::class;
-$config[ErrorHandlerConstants::ERROR_RENDERER] = WebExceptionErrorRenderer::class;
 $config[KernelConstants::AUTO_LOADER_UNRESOLVABLE_CACHE_ENABLED] = true;
