54e73d7afa9347e068b27085a46aa0ed7a3e49ab
René Klatt <rene.klatt@spryker.com>
2017-03-22 07:17:10 +0000

    update for tests

---
+++ src/Pyz/Zed/Discount/Business/DiscountFacade.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Zed\Discount\Business;
+
+use Spryker\Zed\Discount\Business\DiscountFacade as SprykerDiscountFacade;
+
+class DiscountFacade extends SprykerDiscountFacade
+{
+
+}
