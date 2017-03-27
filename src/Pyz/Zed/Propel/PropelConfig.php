ce95071437d5a3a9dde7f3162cc38f9751899b4c
mscherer <mark.scherer@spryker.com>
2017-03-27 19:32:11 +0200

    Merge remote-tracking branch 'project/master' into demoshop master.

--- src/Pyz/Zed/Propel/PropelConfig.php
+++ src/Pyz/Zed/Propel/PropelConfig.php
@@ -19,19 +19,8 @@ class PropelConfig extends SprykerPropelConfig
     {
         return array_merge(
             [APPLICATION_SOURCE_DIR . '/*/Zed/*/Persistence/Propel/Schema/'],
-            parent::getPropelSchemaPathPatterns(),
-            $this->getSprykerFilePathPatterns()
+            parent::getPropelSchemaPathPatterns()
         );
-    }
-
-    /**
-     * @project Only needed in Project, not in demoshop
-     *
-     * @return array
-     */
-    protected function getSprykerFilePathPatterns()
-    {
-        return [APPLICATION_VENDOR_DIR . '/spryker/spryker/Bundles/*/src/*/Zed/*/Persistence/Propel/Schema/'];
     }
 
 }
