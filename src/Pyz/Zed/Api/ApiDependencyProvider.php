993b3ddd0219b9107a8c72020f580af1c5746f7d
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-03-29 23:52:21 +0200

    feature/core-908-rest-api [ci skip] api pre processors refactoring, added Processor, PreProcessorProvider,  ApiPostProcessorPluginInterface, ApiPreProcessorPluginInterface, cleanups

--- src/Pyz/Zed/Api/ApiDependencyProvider.php
+++ src/Pyz/Zed/Api/ApiDependencyProvider.php
@@ -7,8 +7,10 @@
 namespace Pyz\Zed\Api;
 
 use Spryker\Zed\Api\ApiDependencyProvider as SprykerApiDependencyProvider;
+use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\FilterPreProcessorPlugin;
 use Spryker\Zed\CustomerApi\Communication\Plugin\Api\CustomerApiPlugin;
 use Spryker\Zed\ProductApi\Communication\Plugin\Api\ProductApiPlugin;
+
 
 class ApiDependencyProvider extends SprykerApiDependencyProvider
 {
@@ -21,6 +23,32 @@ class ApiDependencyProvider extends SprykerApiDependencyProvider
         return [
             new CustomerApiPlugin(),
             new ProductApiPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\Api\Business\Model\Processor\Pre\PreProcessorInterface[]
+     */
+    protected function getPreProcessorPluginCollection()
+    {
+        return [
+            new PathPreProcessor(),
+            new FormatTypeByHeaderPreProcessor(),
+            new FormatTypeByExtensionPreProcessor(),
+            new ResourcePreProcessor(),
+            new ResourceActionPreProcessor(),
+            new ResourceParamsPreProcessor(),
+            new FilterPreProcessorPlugin(),
+            new PaginationPreProcessor(),
+            new FieldsByQueryPreProcessor(),
+            new SortByQueryFilterPreProcessor(),
+            new CriteriaByQueryFilterPreProcessor(),
+            new PaginationByQueryFilterPreProcessor(),
+            new PaginationByHeaderFilterPreProcessor(),
+            new AddActionPreProcessor(),
+            new UpdateActionPreProcessor(),
+            new GetActionPreProcessor(),
+            new FindActionPreProcessor(),
         ];
     }
 
