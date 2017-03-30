6d01fb5d5489b85e22bc803bb428c313c8bd4212
Oliwier Ptak <oliwier.ptak@spryker.com>
2017-03-30 13:07:07 +0200

    feature/core-908-rest-api [ci skip] removed processor plugins

--- src/Pyz/Zed/Api/ApiDependencyProvider.php
+++ src/Pyz/Zed/Api/ApiDependencyProvider.php
@@ -9,23 +9,6 @@ namespace Pyz\Zed\Api;
 use Spryker\Zed\Api\ApiDependencyProvider as SprykerApiDependencyProvider;
 use Spryker\Zed\Api\Communication\Plugin\Processor\Post\Action\AddActionPostProcessorPlugin;
 use Spryker\Zed\Api\Communication\Plugin\Processor\Post\Action\DeleteActionPostProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Action\AddActionPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Action\FindActionPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Action\GetActionPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Action\UpdateActionPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Fields\FieldsByQueryPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\FilterPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Filter\Header\PaginationByHeaderFilterPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Filter\Query\CriteriaByQueryFilterPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Filter\Query\PaginationByQueryFilterPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Filter\Query\SortByQueryFilterPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Format\FormatTypeByHeaderPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Format\FormatTypeByPathPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\PaginationPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\PathPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Resource\ResourceActionPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Resource\ResourceParametersPreProcessorPlugin;
-use Spryker\Zed\Api\Communication\Plugin\Processor\Pre\Resource\ResourcePreProcessorPlugin;
 use Spryker\Zed\CustomerApi\Communication\Plugin\Api\CustomerApiPlugin;
 use Spryker\Zed\ProductApi\Communication\Plugin\Api\ProductApiPlugin;
 
@@ -40,32 +23,6 @@ class ApiDependencyProvider extends SprykerApiDependencyProvider
         return [
             new CustomerApiPlugin(),
             new ProductApiPlugin(),
-        ];
-    }
-
-    /**
-     * @return \Spryker\Zed\Api\Business\Model\Processor\Pre\PreProcessorInterface[]
-     */
-    protected function getPreProcessorPluginCollection()
-    {
-        return [
-            new PathPreProcessorPlugin(),
-            new FormatTypeByHeaderPreProcessorPlugin(),
-            new FormatTypeByPathPreProcessorPlugin(),
-            new ResourcePreProcessorPlugin(),
-            new ResourceActionPreProcessorPlugin(),
-            new ResourceParametersPreProcessorPlugin(),
-            new FilterPreProcessorPlugin(),
-            new PaginationPreProcessorPlugin(),
-            new FieldsByQueryPreProcessorPlugin(),
-            new SortByQueryFilterPreProcessorPlugin(),
-            new CriteriaByQueryFilterPreProcessorPlugin(),
-            new PaginationByQueryFilterPreProcessorPlugin(),
-            new PaginationByHeaderFilterPreProcessorPlugin(),
-            new AddActionPreProcessorPlugin(),
-            new UpdateActionPreProcessorPlugin(),
-            new GetActionPreProcessorPlugin(),
-            new FindActionPreProcessorPlugin(),
         ];
     }
 
