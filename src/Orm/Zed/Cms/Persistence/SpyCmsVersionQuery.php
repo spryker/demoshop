f2b026ee2fe7fdcaf15703b7f762238d197ce79e
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2017-03-30 15:52:40 +0000

    CORE-771 Add cms version orm models

---
+++ src/Orm/Zed/Cms/Persistence/SpyCmsVersionQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Cms\Persistence;
+
+use Orm\Zed\Cms\Persistence\Base\SpyCmsVersionQuery as BaseSpyCmsVersionQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_cms_version' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyCmsVersionQuery extends BaseSpyCmsVersionQuery
+{
+
+}
