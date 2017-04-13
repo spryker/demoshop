9a6fc0015d6b431db618f4c82f546b64ff4a7f2d
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-04-13 08:47:09 +0200

    CORE-905 product group bundle initial commit
    Signed-off-by: Tamas Nyulas <tamas.nyulas@spryker.com>

---
+++ src/Orm/Zed/ProductGroup/Persistence/SpyProductAbstractGroupQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductGroup\Persistence;
+
+use Orm\Zed\ProductGroup\Persistence\Base\SpyProductAbstractGroupQuery as BaseSpyProductAbstractGroupQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_product_abstract_group' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductAbstractGroupQuery extends BaseSpyProductAbstractGroupQuery
+{
+
+}
