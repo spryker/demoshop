257d5bba00c2c8cc16325f70c16e87916ea42d87
Thomas Kühnel <thomas.kuehnel@spryker.com>
2017-05-03 19:00:29 +0000

    CORE-763 Add Propel models for spy_product_label table

---
+++ src/Orm/Zed/ProductLabel/Persistence/SpyProductLabel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductLabel\Persistence;
+
+use Orm\Zed\ProductLabel\Persistence\Base\SpyProductLabel as BaseSpyProductLabel;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_label' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductLabel extends BaseSpyProductLabel
+{
+
+}
