0772a5672b5a169db1c4f5696b1822ba0100d046
Denis Turkov <denis.turkov@spryker.com>
2017-05-10 13:29:25 +0000

    CORE-778: Add delete button to Yves

---
+++ src/Pyz/Yves/Customer/Controller/DeleteController.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\Customer\Controller;
+
+use Pyz\Yves\Customer\Plugin\Provider\CustomerControllerProvider;
+use Spryker\Shared\Customer\Code\Messages;
+
+class DeleteController extends AbstractCustomerController
+{
+
+    /**
+     * @return void
+     */
+    public function indexAction()
+    {
+        $loggedInCustomerTransfer = $this->getLoggedInCustomerTransfer();
+    }
+
+    public function confirmAction()
+    {
+        $loggedInCustomerTransfer = $this->getLoggedInCustomerTransfer();
+
+        $isSuccessful = $this->getClient()->anonymizeCustomer($loggedInCustomerTransfer);
+
+        if ($isSuccessful) {
+            $this->getClient()->logout();
+            $this->addSuccessMessage(Messages::CUSTOMER_ANONYMIZATION_SUCCESS);
+            return $this->redirectResponseInternal(CustomerControllerProvider::ROUTE_LOGIN);
+        }
+
+        $this->addErrorMessage(Messages::CUSTOMER_ANONYMIZATION_FAILED);
+        return $this->redirectResponseInternal(CustomerControllerProvider::ROUTE_CUSTOMER_DELETE);
+    }
+
+}
