44dbdaf1aeb5b8811b61043ed00a949ae23e275f
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-05-22 09:44:02 +0200

    CORE-1169 Added Product Set Propel classes
    Signed-off-by: Tamas Nyulas <tamas.nyulas@spryker.com>

---
+++ src/Orm/Zed/ProductSet/Persistence/SpyProductSet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductSet\Persistence;
+
+use Spryker\Zed\ProductSet\Persistence\Propel\AbstractSpyProductSet as BaseSpyProductSet;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_set' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductSet extends BaseSpyProductSet
+{
+
+}
