fd9ddb8c1fc2d3f940e705a4216b538c2a741d6b
Denis Turkov <denis.turkov@spryker.com>
2017-07-18 09:43:44 +0000

    CORE-773: Fix assoc array merge

--- src/Pyz/Zed/Category/CategoryConfig.php
+++ src/Pyz/Zed/Category/CategoryConfig.php
@@ -18,13 +18,13 @@ class CategoryConfig extends CategoryCategoryConfig
      */
     public function getTemplateList()
     {
-        return array_merge(
-            parent::getTemplateList(),
-            [
-                CmsBlockCategoryConnectorConfig::CATEGORY_TEMPLATE_ONLY_CMS_BLOCK => '@Catalog/catalog/cms-block.twig',
-                CmsBlockCategoryConnectorConfig::CATEGORY_TEMPLATE_WITH_CMS_BLOCK => '@Catalog/catalog/catalog-cms-block.twig',
-            ]
-        );
+        $templateList = [
+            CmsBlockCategoryConnectorConfig::CATEGORY_TEMPLATE_ONLY_CMS_BLOCK => '@Catalog/catalog/cms-block.twig',
+            CmsBlockCategoryConnectorConfig::CATEGORY_TEMPLATE_WITH_CMS_BLOCK => '@Catalog/catalog/catalog-cms-block.twig',
+        ];
+        $templateList += parent::getTemplateList();
+
+        return $templateList;
     }
 
 }
