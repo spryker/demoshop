c3ddcb37a14b3408328ae43b0d06ff9fb843f272
René Klatt <rene.klatt@spryker.com>
2017-07-27 13:36:22 +0000

    Make php stan work with spryker

---
+++ phpstan-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Copyright © 2017-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+define('APPLICATION_ROOT_DIR', __DIR__);
+define('APPLICATION_VENDOR_DIR', __DIR__ . '/vendor');
