1fdca05021b21d9b0a15ea4a4472699a2fa8035d
René Klatt <rene.klatt@spryker.com>
2017-08-01 05:08:25 +0000

    core-1652 update EnvironmentHelper

--- tests/PyzTest/Shared/Testify/_support/Helper/Environment.php
+++ tests/PyzTest/Shared/Testify/_support/Helper/Environment.php
@@ -21,7 +21,6 @@ class Environment extends Module
 
         defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'devtest');
         defined('APPLICATION_STORE') || define('APPLICATION_STORE', (isset($_SERVER['APPLICATION_STORE'])) ? $_SERVER['APPLICATION_STORE'] : 'DE');
-        defined('APPLICATION') || define('APPLICATION', 'ZED');
 
         defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR', $rootDir);
         defined('APPLICATION_SOURCE_DIR') || define('APPLICATION_SOURCE_DIR', APPLICATION_ROOT_DIR . '/src');
