f3fc2a283b3610c35dbab32f19454b9b312f881b
René Klatt <rene.klatt@spryker.com>
2017-08-03 05:02:58 +0000

    core-1652 debug output

--- tests/PyzTest/Yves/Availability/Presentation/CheckoutAvailabilityCest.php
+++ tests/PyzTest/Yves/Availability/Presentation/CheckoutAvailabilityCest.php
@@ -48,16 +48,25 @@ class CheckoutAvailabilityCest
         $zedTester = $i->haveFriend('zedTester', ZedAvailabilityPresentationTester::class);
 
         $zedTester->does(function (ZedAvailabilityPresentationTester $i) {
+            $i->amZed();
+            $i->amLoggedInUser();
+
             $idProductFujitsu = 118;
 
             $i->amOnPage(sprintf(AvailabilityViewPage::VIEW_PRODUCT_AVAILABILITY_URL, $idProductFujitsu));
 
+            $i->waitForElementVisible('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
+
             $reservedProductsBefore = $i->grabTextFrom('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
+
+            codecept_debug('Reserved Products before: ' . $reservedProductsBefore);
 
             $i->amOnPage(OrderListPage::ORDER_LIST_URL);
             $i->wait(2);
 
             $idSalesOrder = $i->grabTextFrom('//*[@class="dataTables_scrollBody"]/table/tbody/tr[1]/td[1]');
+
+            codecept_debug('ID Sales order: ' . $idSalesOrder);
 
             $i->amOnPage(sprintf(OrderDetailPage::ORDER_DETAIL_PAGE_URL, $idSalesOrder));
 
@@ -72,7 +81,6 @@ class CheckoutAvailabilityCest
             $reservedProductsAfter = $i->grabTextFrom('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
 
             $i->assertEquals($reservedProductsAfter, $reservedProductsBefore - 1); //Reserved item returned back
-
         });
 
         $zedTester->leave();
