8e38eb8d70b581ff8a0889772f63c43fc1665ba3
René Klatt <rene.klatt@spryker.com>
2017-08-03 10:11:06 +0000

    core-1652 fixed project tests

---
+++ test-autoload.php
@@ -0,0 +1,36 @@
+<?php
+
+$autoloader = function ($className) {
+
+    $className = ltrim($className, '\\');
+    $classNameParts = explode('\\', $className);
+
+    if ($classNameParts[0] === 'PyzTest') {
+        array_shift($classNameParts);
+        $application = array_shift($classNameParts);
+        $bundle = array_shift($classNameParts);
+        $className = implode(DIRECTORY_SEPARATOR, $classNameParts);
+        $filePathPartsSupport = [
+            __DIR__,
+            'tests',
+            'PyzTest',
+            $application,
+            $bundle,
+            '_support',
+            $className . '.php',
+        ];
+    }
+
+    if (isset($filePathPartsSupport)) {
+        $filePath = implode(DIRECTORY_SEPARATOR, $filePathPartsSupport);
+        if (file_exists($filePath)) {
+            require $filePath;
+
+            return true;
+        }
+    }
+
+    return false;
+};
+
+spl_autoload_register($autoloader);
--- tests/PyzTest/Yves/Availability/Presentation/CheckoutAvailabilityCest.php
+++ tests/PyzTest/Yves/Availability/Presentation/CheckoutAvailabilityCest.php
@@ -9,7 +9,6 @@ namespace PyzTest\Yves\Availability\Presentation;
 use PyzTest\Yves\Availability\AvailabilityPresentationTester;
 use PyzTest\Yves\Cart\PageObject\CartListPage;
 use PyzTest\Yves\Product\PageObject\ProductDetailPage;
-use PyzTest\Zed\Availability\AvailabilityPresentationTester as ZedAvailabilityPresentationTester;
 use PyzTest\Zed\Availability\PageObject\AvailabilityViewPage;
 use PyzTest\Zed\Sales\PageObject\OrderDetailPage;
 use PyzTest\Zed\Sales\PageObject\OrderListPage;
@@ -45,45 +44,39 @@ class CheckoutAvailabilityCest
 
         $i->processCheckout();
 
-        $zedTester = $i->haveFriend('zedTester', ZedAvailabilityPresentationTester::class);
+        $i->amZed();
+        $i->amLoggedInUser();
 
-        $zedTester->does(function (ZedAvailabilityPresentationTester $i) {
-            $i->amZed();
-            $i->amLoggedInUser();
+        $idProductFujitsu = 118;
 
-            $idProductFujitsu = 118;
+        $i->amOnPage(sprintf(AvailabilityViewPage::VIEW_PRODUCT_AVAILABILITY_URL, $idProductFujitsu));
 
-            $i->amOnPage(sprintf(AvailabilityViewPage::VIEW_PRODUCT_AVAILABILITY_URL, $idProductFujitsu));
+        $i->waitForElementVisible('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
 
-            $i->waitForElementVisible('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
+        $reservedProductsBefore = $i->grabTextFrom('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
 
-            $reservedProductsBefore = $i->grabTextFrom('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
+        codecept_debug('Reserved Products before: ' . $reservedProductsBefore);
 
-            codecept_debug('Reserved Products before: ' . $reservedProductsBefore);
+        $i->amOnPage(OrderListPage::ORDER_LIST_URL);
+        $i->wait(2);
 
-            $i->amOnPage(OrderListPage::ORDER_LIST_URL);
-            $i->wait(2);
+        $idSalesOrder = $i->grabTextFrom('//*[@class="dataTables_scrollBody"]/table/tbody/tr[1]/td[1]');
 
-            $idSalesOrder = $i->grabTextFrom('//*[@class="dataTables_scrollBody"]/table/tbody/tr[1]/td[1]');
+        codecept_debug('ID Sales order: ' . $idSalesOrder);
 
-            codecept_debug('ID Sales order: ' . $idSalesOrder);
+        $i->amOnPage(sprintf(OrderDetailPage::ORDER_DETAIL_PAGE_URL, $idSalesOrder));
 
-            $i->amOnPage(sprintf(OrderDetailPage::ORDER_DETAIL_PAGE_URL, $idSalesOrder));
+        $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'pay'));
+        $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'ship'));
+        $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'stock-update'));
+        $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'return'));
+        $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'refund'));
 
-            $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'pay'));
-            $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'ship'));
-            $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'stock-update'));
-            $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'return'));
-            $i->click(sprintf(OrderDetailPage::OMS_EVENT_TRIGGER_XPATH, 'refund'));
+        $i->amOnPage(sprintf(AvailabilityViewPage::VIEW_PRODUCT_AVAILABILITY_URL, $idProductFujitsu));
 
-            $i->amOnPage(sprintf(AvailabilityViewPage::VIEW_PRODUCT_AVAILABILITY_URL, $idProductFujitsu));
+        $reservedProductsAfter = $i->grabTextFrom('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
 
-            $reservedProductsAfter = $i->grabTextFrom('//*[@id="page-wrapper"]/div[3]/div[2]/div/div/div[2]/div/div[5]/p[2]');
-
-            $i->assertEquals($reservedProductsAfter, $reservedProductsBefore - 1); //Reserved item returned back
-        });
-
-        $zedTester->leave();
+        $i->assertEquals($reservedProductsAfter, $reservedProductsBefore - 1); //Reserved item returned back
     }
 
 }
