91ab8bd4d2761d799f2fa57ddd98f9a6c1530f71
René Klatt <rene.klatt@spryker.com>
2017-08-07 06:15:28 +0000

    update phpstan config

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -6,4 +6,8 @@
  */
 
 define('APPLICATION_ROOT_DIR', __DIR__);
-define('APPLICATION_VENDOR_DIR', __DIR__ . '/vendor');
+define('APPLICATION_VENDOR_DIR', APPLICATION_ROOT_DIR . '/vendor');
+define('APPLICATION_SOURCE_DIR', APPLICATION_ROOT_DIR . '/src');
+define('APPLICATION', '');
+define('APPLICATION_ENV', '');
+define('APPLICATION_STORE', '');
