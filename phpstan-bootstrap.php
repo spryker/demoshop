8b610fd37433551b20c383cec781e8dbb04d8b07
René Klatt <rene.klatt@spryker.com>
2017-08-07 11:29:21 +0000

    require for IdeAutocompletion files, update scrutinizer config

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -11,3 +11,8 @@ define('APPLICATION_SOURCE_DIR', APPLICATION_ROOT_DIR . '/src');
 define('APPLICATION', '');
 define('APPLICATION_ENV', '');
 define('APPLICATION_STORE', '');
+
+require_once(__DIR__ . '/src/Generated/Client/Ide/AutoCompletion.php');
+require_once(__DIR__ . '/src/Generated/Service/Ide/AutoCompletion.php');
+require_once(__DIR__ . '/src/Generated/Yves/Ide/AutoCompletion.php');
+require_once(__DIR__ . '/src/Generated/Zed/Ide/AutoCompletion.php');
