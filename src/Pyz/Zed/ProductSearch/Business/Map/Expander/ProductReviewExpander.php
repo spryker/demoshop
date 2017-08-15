95e24284c7480bdd8e6a32f60896f26c9973032e
Tamas Nyulas <tamas.nyulas@spryker.com>
2017-08-15 17:34:17 +0200

    CORE-903 Exported average product ratings to search

---
+++ src/Pyz/Zed/ProductSearch/Business/Map/Expander/ProductReviewExpander.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ProductSearch\Business\Map\Expander;
+
+use Generated\Shared\Transfer\LocaleTransfer;
+use Generated\Shared\Transfer\PageMapTransfer;
+use Spryker\Zed\Search\Business\Model\Elasticsearch\DataMapper\PageMapBuilderInterface;
+
+class ProductReviewExpander implements ProductPageMapExpanderInterface
+{
+
+    /**
+     * @param \Generated\Shared\Transfer\PageMapTransfer $pageMapTransfer
+     * @param \Spryker\Zed\Search\Business\Model\Elasticsearch\DataMapper\PageMapBuilderInterface $pageMapBuilder
+     * @param array $productData
+     * @param \Generated\Shared\Transfer\LocaleTransfer $localeTransfer
+     *
+     * @return \Generated\Shared\Transfer\PageMapTransfer
+     */
+    public function expandProductPageMap(
+        PageMapTransfer $pageMapTransfer,
+        PageMapBuilderInterface $pageMapBuilder,
+        array $productData,
+        LocaleTransfer $localeTransfer
+    ) {
+        $rating = $this->getRating($productData);
+
+        if (!$rating) {
+            return $pageMapTransfer;
+        }
+
+        $pageMapBuilder
+            ->addSearchResultData($pageMapTransfer, 'rating', $rating)
+            ->addIntegerFacet($pageMapTransfer, 'rating', $rating)
+            ->addIntegerSort($pageMapTransfer, 'rating', $rating);
+
+        return $pageMapTransfer;
+    }
+
+    /**
+     * @param array $productData
+     *
+     * @return float
+     */
+    protected function getRating(array $productData)
+    {
+        return round($productData['average_rating']);
+    }
+
+}
