41037a25b20ffa94c461e2115e41c6fb7aa176a7
Auris <aurimas.lickus@spryker.com>
2017-09-07 16:47:12 +0200

    core-1748: multistore propel models

---
+++ src/Pyz/Zed/DataImport/Business/Model/Store/StoreWriterStep.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\DataImport\Business\Model\Store;
+
+use Spryker\Zed\DataImport\Business\Model\DataImportStep\DataImportStepInterface;
+use Spryker\Zed\DataImport\Business\Model\DataSet\DataSetInterface;
+
+class StoreWriterStep implements DataImportStepInterface
+{
+
+    /**
+     * @param \Spryker\Zed\DataImport\Business\Model\DataSet\DataSetInterface $dataSet
+     *
+     * @return void
+     */
+    public function execute(DataSetInterface $dataSet)
+    {
+        // TODO: Implement execute() method.
+    }
+
+}
