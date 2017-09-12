6cc1114bfbd0e340fa1f7323402cb36c70df2d6a
Auris <aurimas.lickus@spryker.com>
2017-09-12 17:14:12 +0200

    core-1755: multicurrency for orders

---
+++ src/Orm/Zed/ProductReview/Persistence/SpyProductReview.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductReview\Persistence;
+
+use Orm\Zed\ProductReview\Persistence\Base\SpyProductReview as BaseSpyProductReview;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_review' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductReview extends BaseSpyProductReview
+{
+
+}
---
+++ src/Orm/Zed/ProductReview/Persistence/SpyProductReviewQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductReview\Persistence;
+
+use Orm\Zed\ProductReview\Persistence\Base\SpyProductReviewQuery as BaseSpyProductReviewQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_product_review' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductReviewQuery extends BaseSpyProductReviewQuery
+{
+
+}
