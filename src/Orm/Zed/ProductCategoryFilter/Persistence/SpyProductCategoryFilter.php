7bad31682c801b344a4bbcd98450bbc773e230b0
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2017-10-17 13:38:34 +0000

    ProductCategoryFilter feature Propel classes

---
+++ src/Orm/Zed/ProductCategoryFilter/Persistence/SpyProductCategoryFilter.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\ProductCategoryFilter\Persistence;
+
+use Spryker\Zed\ProductCategoryFilter\Persistence\Propel\AbstractSpyProductCategoryFilter as BaseSpyProductCategoryFilter;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_category_filter' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductCategoryFilter extends BaseSpyProductCategoryFilter
+{
+
+}
