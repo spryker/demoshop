d92daeac23ebe2d1305350cb2a349f3b5dfec538
René Klatt <rene.klatt@spryker.com>
2017-11-10 14:36:41 +0000

    core-2001 update config

--- config/Shared/config_default-development_DE.php
+++ config/Shared/config_default-development_DE.php
@@ -16,7 +16,7 @@ $config[MailConstants::MAILCATCHER_GUI] = sprintf('http://%s:1080', $config[Appl
 
 // ---------- Elasticsearch
 $ELASTICA_INDEX_NAME = 'de_search';
-$config[ApplicationConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
+$config[SearchConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
 $config[CollectorConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
 $config[SearchConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
 
--- config/Shared/config_default-production_DE.php
+++ config/Shared/config_default-production_DE.php
@@ -4,12 +4,11 @@
  * This is the global runtime configuration for Yves and Generated_Yves_Zed in a production environment.
  */
 
-use Spryker\Shared\Application\ApplicationConstants;
 use Spryker\Shared\Collector\CollectorConstants;
 use Spryker\Shared\Search\SearchConstants;
 
 // ---------- Elasticsearch
 $ELASTICA_INDEX_NAME = 'de_search';
-$config[ApplicationConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
+$config[SearchConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
 $config[CollectorConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
 $config[SearchConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
