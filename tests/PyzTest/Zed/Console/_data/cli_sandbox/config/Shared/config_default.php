9d78899a069cde4e8df1adfa36739eb47e41299f
Auris <aurimas.lickus@spryker.com>
2017-11-23 16:31:39 +0100

    core-1752: cleanup code

--- tests/PyzTest/Zed/Console/_data/cli_sandbox/config/Shared/config_default.php
+++ tests/PyzTest/Zed/Console/_data/cli_sandbox/config/Shared/config_default.php
@@ -17,7 +17,6 @@ use Spryker\Shared\Log\LogConstants;
 use Spryker\Shared\NewRelic\NewRelicConstants;
 use Spryker\Shared\Oms\OmsConstants;
 use Spryker\Shared\Payolution\PayolutionConstants;
-use Spryker\Shared\PriceProduct\PriceProductConstants;
 use Spryker\Shared\Propel\PropelConstants;
 use Spryker\Shared\Queue\QueueConstants;
 use Spryker\Shared\Sales\SalesConstants;
