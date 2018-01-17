685f1a72c7fd2bbd07c7ef329e96f89457c1d4ef
Karoly Gerner <karoly.gerner@spryker.com>
2018-01-17 07:35:46 +0000

    CORE-1765 Moving importer to proper namespace

--- src/Pyz/Zed/DataImport/Business/Model/ProductAbstractStore/ProductAbstractStoreWriterStep.php
+++ src/Pyz/Zed/DataImport/Business/Model/ProductAbstractStore/ProductAbstractStoreWriterStep.php
@@ -5,18 +5,17 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Pyz\Zed\DataImport\Business\Model\Product;
+namespace Pyz\Zed\DataImport\Business\Model\ProductAbstractStore;
 
 use Orm\Zed\Product\Persistence\SpyProductAbstractQuery;
 use Orm\Zed\Product\Persistence\SpyProductAbstractStoreQuery;
 use Orm\Zed\Store\Persistence\SpyStoreQuery;
 use Spryker\Zed\DataImport\Business\Model\DataImportStep\DataImportStepInterface;
-use Spryker\Zed\DataImport\Business\Model\DataImportStep\TouchAwareStep;
 use Spryker\Zed\DataImport\Business\Model\DataSet\DataSetInterface;
 
 /**
  */
-class ProductAbstractStoreWriterStep extends TouchAwareStep implements DataImportStepInterface
+class ProductAbstractStoreWriterStep implements DataImportStepInterface
 {
     const BULK_SIZE = 100;
 
