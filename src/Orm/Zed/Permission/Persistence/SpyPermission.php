dbbeec612623f9a7c9f34a9077407741e2f6cc41
dereuromark <mark.scherer@spryker.com>
2018-01-17 16:06:18 +0100

    Add missing files.

---
+++ src/Orm/Zed/Permission/Persistence/SpyPermission.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\Permission\Persistence;
+
+use Orm\Zed\Permission\Persistence\Base\SpyPermission as BaseSpyPermission;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_permission' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyPermission extends BaseSpyPermission
+{
+}
