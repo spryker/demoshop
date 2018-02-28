3eb8c9079724132d51ab98b4bd11b7ea8837cd7f
Denis Turkov <denis.turkov@spryker.com>
2018-02-28 20:32:07 +0000

    CORE-974: Update phpdoc

--- src/Orm/Zed/CompanyUnitAddress/Persistence/SpyCompanyUnitAddressToCompanyBusinessUnitQuery.php
+++ src/Orm/Zed/CompanyUnitAddress/Persistence/SpyCompanyUnitAddressToCompanyBusinessUnitQuery.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the Spryker Suite.
+ * This file is part of the Spryker Demoshop.
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
