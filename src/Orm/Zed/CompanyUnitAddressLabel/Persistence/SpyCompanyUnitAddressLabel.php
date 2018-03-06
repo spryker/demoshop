d5c878df00bb7dfb720e598fde14b9ea21c36321
Serhii Sikachov <sergey.sikachev@spryker.com>
2018-03-06 11:07:39 +0000

    CORE-3520. Add custom spryker/spryker branch.

---
+++ src/Orm/Zed/CompanyUnitAddressLabel/Persistence/SpyCompanyUnitAddressLabel.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\CompanyUnitAddressLabel\Persistence;
+
+use Orm\Zed\CompanyUnitAddressLabel\Persistence\Base\SpyCompanyUnitAddressLabel as BaseSpyCompanyUnitAddressLabel;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_company_unit_address_label' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyCompanyUnitAddressLabel extends BaseSpyCompanyUnitAddressLabel
+{
+}
---
+++ src/Orm/Zed/CompanyUnitAddressLabel/Persistence/SpyCompanyUnitAddressLabelToCompanyUnitAddress.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\CompanyUnitAddressLabel\Persistence;
+
+use Orm\Zed\CompanyUnitAddressLabel\Persistence\Base\SpyCompanyUnitAddressLabelToCompanyUnitAddress as BaseSpyCompanyUnitAddressLabelToCompanyUnitAddress;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_company_unit_address_label_to_company_unit_address' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyCompanyUnitAddressLabelToCompanyUnitAddress extends BaseSpyCompanyUnitAddressLabelToCompanyUnitAddress
+{
+}
