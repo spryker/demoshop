a6a7d3c1c826ce5f96c74e54f3e2d9b14da9d399
Oleh Hladchenko <glad.oleg@gmail.com>
2018-03-13 10:45:04 +0200

    core-3354 suppliers orm.

---
+++ src/Orm/Zed/CompanySupplier/Persistence/SpyCompanySupplierToProduct.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\CompanySupplier\Persistence;
+
+use Orm\Zed\CompanySupplier\Persistence\Base\SpyCompanySupplierToProduct as BaseSpyCompanySupplierToProduct;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_company_supplier_to_product' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyCompanySupplierToProduct extends BaseSpyCompanySupplierToProduct
+{
+
+}
