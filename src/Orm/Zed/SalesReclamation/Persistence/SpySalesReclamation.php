79ca3f902126b37f8f08be7abd8aa656c78dd6db
khatsko <kirill.khatsko@spryker.com>
2018-03-21 01:16:53 +0200

    CORE-3338 add reclamation entity

---
+++ src/Orm/Zed/SalesReclamation/Persistence/SpySalesReclamation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\SalesReclamation\Persistence;
+
+use Spryker\Zed\SalesReclamation\Persistence\SpySalesReclamation as BaseSpySalesReclamation;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_sales_reclamation' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpySalesReclamation extends BaseSpySalesReclamation
+{
+
+}
---
+++ src/Orm/Zed/SalesReclamation/Persistence/SpySalesReclamationQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\SalesReclamation\Persistence;
+
+use Spryker\Zed\SalesReclamation\Persistence\SpySalesReclamationQuery as BaseSpySalesReclamationQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_sales_reclamation' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpySalesReclamationQuery extends BaseSpySalesReclamationQuery
+{
+
+}
