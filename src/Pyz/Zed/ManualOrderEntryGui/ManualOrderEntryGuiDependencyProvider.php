961f89c05c7e48ed96eafd53af968ccc45a56fad
Artem Kuznetsov <wizardphp@gmail.com>
2018-03-21 08:10:27 +0000

    CORE-3314 payment

---
+++ src/Pyz/Zed/ManualOrderEntryGui/ManualOrderEntryGuiDependencyProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ManualOrderEntryGui;
+
+use Spryker\Zed\DummyPayment\Communication\Plugin\Checkout\DummyPaymentInvoiceSubFormPlugin;
+use Spryker\Zed\ManualOrderEntryGui\ManualOrderEntryGuiDependencyProvider as SprykerManualOrderEntryGuiDependencyProvider;
+
+class ManualOrderEntryGuiDependencyProvider extends SprykerManualOrderEntryGuiDependencyProvider
+{
+
+    /**
+     * @return \Spryker\Zed\ManualOrderEntryGui\Communication\Plugin\Payment\SubFormPluginInterface[]
+     */
+    protected function getPaymentSubFormPlugins()
+    {
+        return [
+            new DummyPaymentInvoiceSubFormPlugin(),
+        ];
+    }
+}
