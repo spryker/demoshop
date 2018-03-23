a2ec353fe605e68a2cee9d9203201f575f2fb560
Muhammad Kamran <muhammad.kamran@spryker.com>
2018-03-23 14:26:08 +0000

    codesniffer fixes

--- src/Orm/Zed/Sales/Persistence/SpyOrderSource.php
+++ src/Orm/Zed/Sales/Persistence/SpyOrderSource.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\Sales\Persistence;
 
 use Orm\Zed\Sales\Persistence\Base\SpyOrderSource as BaseSpyOrderSource;
@@ -10,11 +15,10 @@ use Orm\Zed\Sales\Persistence\Base\SpyOrderSource as BaseSpyOrderSource;
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyOrderSource extends BaseSpyOrderSource
 {
-
 }
