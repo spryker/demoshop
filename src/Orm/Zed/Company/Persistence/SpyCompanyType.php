7c41bc032427b89a6b4efedf9dbd05ef6eabd5c3
Oleh Hladchenko <glad.oleg@gmail.com>
2018-03-26 12:29:57 +0300

    core-3354 Add orm entities.

---
+++ src/Orm/Zed/Company/Persistence/SpyCompanyType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Company\Persistence;
+
+use Orm\Zed\Company\Persistence\Base\SpyCompanyType as BaseSpyCompanyType;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_company_type' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyCompanyType extends BaseSpyCompanyType
+{
+
+}
