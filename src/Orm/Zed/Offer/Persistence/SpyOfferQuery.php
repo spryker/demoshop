4b9b375ffeb3a90ee4c81782c4990c84ba92e072
Denis Turkov <denis.turkov@spryker.com>
2018-03-28 14:57:07 +0000

    CORE-3784: Add propel entities

---
+++ src/Orm/Zed/Offer/Persistence/SpyOfferQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Offer\Persistence;
+
+use Spryker\Zed\Offer\Persistence\Propel\AbstractSpyOffer as BaseSpyOfferQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_offer' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyOfferQuery extends BaseSpyOfferQuery
+{
+
+}
