447f82338ff29a526c284fa75d6a350e85463e89
Sergey Sikachev <sergey.sikachev@spryker.com>
2018-04-11 11:58:11 +0000

    CORE-3784. Fix code style.

--- src/Pyz/Zed/ManualOrderEntryGui/ManualOrderEntryGuiDependencyProvider.php
+++ src/Pyz/Zed/ManualOrderEntryGui/ManualOrderEntryGuiDependencyProvider.php
@@ -39,7 +39,7 @@ class ManualOrderEntryGuiDependencyProvider extends SprykerManualOrderEntryGuiDe
     protected function getQuoteExpanderPlugins()
     {
         return [
-            new OfferQuoteExpanderPlugin()
+            new OfferQuoteExpanderPlugin(),
         ];
     }
 
