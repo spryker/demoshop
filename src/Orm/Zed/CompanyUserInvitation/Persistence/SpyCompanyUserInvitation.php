840de8edbb702a6a65630efb1c95b8b44f0c0c22
Ahmed Sabaa <ahmed.sabaa@spryker.com>
2018-05-25 14:52:03 +0200

    core-2663 update composer lock and added database abstract classes

---
+++ src/Orm/Zed/CompanyUserInvitation/Persistence/SpyCompanyUserInvitation.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\CompanyUserInvitation\Persistence;
+
+use Spryker\Zed\CompanyUserInvitation\Persistence\Propel\AbstractSpyCompanyUserInvitation as BaseSpyCompanyUserInvitation;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_company_user_invitation' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyCompanyUserInvitation extends BaseSpyCompanyUserInvitation
+{
+}
