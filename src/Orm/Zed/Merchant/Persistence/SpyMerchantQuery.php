3ffae7c1d507d5aa82252b2e8dd6a3c624ee16da
Denis Turkov <denis.turkov@spryker.com>
2018-06-02 10:40:15 +0000

    PS-730: Propel and Compoer update

---
+++ src/Orm/Zed/Merchant/Persistence/SpyMerchantQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Merchant\Persistence;
+
+use Spryker\Zed\Merchant\Persistence\Propel\AbstractSpyMerchantQuery as BaseSpyMerchantQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_merchant' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyMerchantQuery extends BaseSpyMerchantQuery
+{
+
+}
---
+++ src/Orm/Zed/MerchantRelationship/Persistence/SpyMerchantRelationshipToCompanyBusinessUnitQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\MerchantRelationship\Persistence;
+
+use Spryker\Zed\MerchantRelationship\Persistence\Propel\AbstractSpyMerchantRelationshipToCompanyBusinessUnitQuery as BaseSpyMerchantRelationshipToCompanyBusinessUnitQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_merchant_relationship_to_company_business_unit' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyMerchantRelationshipToCompanyBusinessUnitQuery extends BaseSpyMerchantRelationshipToCompanyBusinessUnitQuery
+{
+
+}
