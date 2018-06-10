4873f99b385e25cd49e979fe4c7ae2087d3b9528
Kyrylo Khatsko <kirill.khatsko@spryker.com>
2018-06-10 22:54:50 +0300

    ps-740 BU Hierarhy: fix error from master

---
+++ src/Orm/Zed/ProductPackagingUnit/Persistence/SpyProductPackagingUnitTypeQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\ProductPackagingUnit\Persistence;
+
+use Spryker\Zed\ProductPackagingUnit\Persistence\Propel\AbstractSpyProductPackagingUnitTypeQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_product_packaging_unit_type' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductPackagingUnitTypeQuery extends AbstractSpyProductPackagingUnitTypeQuery
+{
+}
