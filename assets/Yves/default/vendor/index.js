022d893f1033940383490f8ab8dc60f330a98e9d
ilmente <alessandro.bellini@spryker.com>
2018-06-27 19:53:43 +0200

    remove unnecessary assets

--- assets/Yves/default/vendor/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * This file is part of the Spryker Demoshop.
- * For full license information, please view the LICENSE file that was distributed with this source code.
- */
-
-'use strict';
-
-require('./foundation');
