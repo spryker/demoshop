eee56b2becd4adf22d39bd09ac2aa728023de9b8
Dmitry Lymarenko <dmitry.lymarenko@spryker.com>
2018-06-29 14:54:11 +0300

    PS-1880 Added Spy classes

---
+++ src/Orm/Zed/FileManager/Persistence/SpyFileDirectoryQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\FileManager\Persistence;
+
+use Orm\Zed\FileManager\Persistence\Base\SpyFileDirectoryQuery as BaseSpyFileDirectoryQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_file_directory' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyFileDirectoryQuery extends BaseSpyFileDirectoryQuery
+{
+}
