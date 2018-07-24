c3f56d0fd969c0707bcd43b6a8d29afc3a552d38
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2018-07-24 16:16:34 +0300

    PS-974: added ORM files

---
+++ src/Orm/Zed/ProductAlternativeStorage/Persistence/SpyProductAlternativeStorageQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\ProductAlternativeStorage\Persistence;
+
+use Spryker\Zed\ProductAlternativeStorage\Persistence\Propel\AbstractSpyProductAlternativeStorageQuery as BaseSpyProductAlternativeStorageQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_product_alternative_storage' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductAlternativeStorageQuery extends BaseSpyProductAlternativeStorageQuery
+{
+}
