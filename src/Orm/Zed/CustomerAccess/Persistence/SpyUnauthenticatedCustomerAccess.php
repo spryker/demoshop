bccf53a973cd557b1ff2670309b6745a3a258a28
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-09-26 13:30:08 +0300

    PS-27: added persistence layer files with updates per phpcs

---
+++ src/Orm/Zed/CustomerAccess/Persistence/SpyUnauthenticatedCustomerAccess.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\CustomerAccess\Persistence;
+
+use Orm\Zed\CustomerAccess\Persistence\Base\SpyUnauthenticatedCustomerAccess as BaseSpyUnauthenticatedCustomerAccess;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_unauthenticated_customer_access' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyUnauthenticatedCustomerAccess extends BaseSpyUnauthenticatedCustomerAccess
+{
+}
---
+++ src/Orm/Zed/CustomerAccessStorage/Persistence/SpyUnauthenticatedCustomerAccessStorageQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Spryker Demoshop.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\CustomerAccessStorage\Persistence;
+
+use Orm\Zed\CustomerAccessStorage\Persistence\Base\SpyUnauthenticatedCustomerAccessStorageQuery as BaseSpyUnauthenticatedCustomerAccessStorageQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_unauthenticated_customer_access_storage' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyUnauthenticatedCustomerAccessStorageQuery extends BaseSpyUnauthenticatedCustomerAccessStorageQuery
+{
+}
