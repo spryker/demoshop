5323d814c78dcce2eb5c6de7458e841f8d0d0213
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-11-03 15:26:00 +0200

    PS-1892: Fix after review.
     - Change wishlist name validation logic

--- src/Pyz/Yves/Wishlist/Form/WishlistFormType.php
+++ src/Pyz/Yves/Wishlist/Form/WishlistFormType.php
@@ -13,13 +13,14 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class WishlistFormType extends AbstractType
 {
     public const FIELD_NAME = 'name';
 
-    protected const GLOSSARY_KEY_NAME_VALIDATION_ERROR = 'wishlist.validation.error.name.alnum_type';
+    protected const GLOSSARY_KEY_NAME_VALIDATION_ERROR = 'wishlist.validation.error.name.wrong_format';
+    protected const WISH_LIST_NAME_VALIDATION_REGEX = '/^[ A-Za-z0-9_-]+$/';
 
     /**
      * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
@@ -56,10 +57,13 @@ class WishlistFormType extends AbstractType
             'required' => true,
             'constraints' => [
                 new NotBlank(),
-                new Type([
-                    'type' => 'alnum',
-                    'message' => static::GLOSSARY_KEY_NAME_VALIDATION_ERROR,
-                ]),
+                new Regex(
+                    [
+                        'pattern' => static::WISH_LIST_NAME_VALIDATION_REGEX,
+                        'message' => static::GLOSSARY_KEY_NAME_VALIDATION_ERROR,
+                        'match' => true,
+                    ]
+                ),
             ],
         ]);
 
